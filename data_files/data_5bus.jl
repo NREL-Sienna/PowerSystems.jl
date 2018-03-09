17d469222d4dd6e141e7027f9f9973dbffc6141b
Jose Daniel Lara <jdlara@berkeley.edu>
2018-03-09 13:13:51 -0800

    update 5 bus data

--- data_files/data_5bus.jl
+++ data_files/data_5bus.jl
@@ -2,9 +2,10 @@ using PowerSystems
 using TimeSeries
 using Missings
 
-dates  = collect(DateTime("1/1/2024  0:00:00", "d/m/y  H:M:S"):Hour(1):DateTime("1/1/2024  23:00:00", "d/m/y  H:M:S"))
+DayAhead  = collect(DateTime("1/1/2024  0:00:00", "d/m/y  H:M:S"):Hour(1):DateTime("1/1/2024  23:00:00", "d/m/y  H:M:S"))
+#Dispatch_11am =  collect(DateTime("1/1/2024  0:11:00", "d/m/y  H:M:S"):Minute(15):DateTime("1/1/2024  12::00", "d/m/y  H:M:S"))
 
-FiveBus = SystemParam(5, 230, 100, length(dates));
+FiveBus = SystemParam(5, 230, 100, length(DayAhead));
 
 nodes5    = [Bus(1,"nodeA", "PV", 0, 1.0, (1.05, 0.9), 230),
              Bus(2,"nodeB", "PQ", 0, 1.0, (1.05, 0.9), 230),
@@ -14,14 +15,64 @@ nodes5    = [Bus(1,"nodeA", "PV", 0, 1.0, (1.05, 0.9), 230),
         ];
 
 branches5 = [Line("1", true, (nodes5[1],nodes5[2]), 0.00281, 0.0281, 0.00712, 400.0, missing),
-            Line("2", true, (nodes5[1],nodes5[4]), 0.00304, 0.0304, 0.00658, Inf, missing),
-            Line("3", true, (nodes5[1],nodes5[5]), 0.00064, 0.0064, 0.03126, Inf, missing),
-            Line("4", true, (nodes5[2],nodes5[3]), 0.00108, 0.0108, 0.01852, Inf, missing),     
-            Line("5", true, (nodes5[3],nodes5[4]), 0.00297, 0.0297, 0.00674, Inf, missing),
-            Line("6", true, (nodes5[4],nodes5[5]), 0.00297, 0.0297, 0.00674, 240, missing)
+             Line("2", true, (nodes5[1],nodes5[4]), 0.00304, 0.0304, 0.00658, Inf, missing),
+             Line("3", true, (nodes5[1],nodes5[5]), 0.00064, 0.0064, 0.03126, Inf, missing),
+             Line("4", true, (nodes5[2],nodes5[3]), 0.00108, 0.0108, 0.01852, Inf, missing),     
+             Line("5", true, (nodes5[3],nodes5[4]), 0.00297, 0.0297, 0.00674, Inf, missing),
+             Line("6", true, (nodes5[4],nodes5[5]), 0.00297, 0.0297, 0.00674, 240, missing)
 ];     
 
 Net5 = Network(FiveBus, branches5, nodes5); 
+
+solar_ts_DA = [0
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            0.351105684
+            0.632536266
+            0.99463925
+            1
+            0.944237283
+            0.396681234
+            0.366511428
+            0.155125829
+            0.040872694
+            0
+            0
+            0
+            0
+            0
+            0]
+
+wind_ts_DA = [0.985205412
+           0.991791369
+           0.997654144
+           1
+           0.998663733
+           0.995497149
+           0.992414567
+           0.98252418
+           0.957203427
+           0.927650911
+           0.907181989
+           0.889095913
+           0.848186718
+           0.766813846
+           0.654052531
+           0.525336131
+           0.396098004
+           0.281771509
+           0.197790004
+           0.153241012
+           0.131355854
+           0.113688144
+           0.099302656
+           0.069569628]
 
 generators5 = [  ThermalGen("Alta", true, nodes5[1],
                     TechGen(40, (0, 40), 10, (-30, 30), missing, missing),
@@ -42,14 +93,10 @@ generators5 = [  ThermalGen("Alta", true, nodes5[1],
                 ThermalGen("Brighton", true, nodes5[5],
                     TechGen(600, (0, 600), 150, (-450, 450), missing, missing),
                     EconGen(600, 10.0, 0.0, 0.0, 0.0, missing)
-                ), 
-                ReFix("SolarC", true, nodes5[3], 
-                    TechRE()
-
                 )
             ];
 
-loadbus2_ts = [ 0.792729978
+loadbus2_ts_DA = [ 0.792729978
               0.723201574
               0.710952098
               0.677672816
@@ -74,7 +121,7 @@ loadbus2_ts = [ 0.792729978
               0.921465115
               0.837001437 ] 
 
-loadbus3_ts = [ 0.831093782
+loadbus3_ts_DA = [ 0.831093782
                 0.689863228
                 0.666058513
                 0.627033103
@@ -99,7 +146,7 @@ loadbus3_ts = [ 0.831093782
                 0.906611479
                 0.824307054]     
 
-loadbus4_ts = [ 0.871297342
+loadbus4_ts_DA = [ 0.871297342
                 0.670489749
                 0.642812243
                 0.630092987
@@ -124,8 +171,7 @@ loadbus4_ts = [ 0.871297342
                 0.771004923
                 0.717847996]            
 
-loads5 = [  StaticLoad("Bus2", true, nodes5[2], "P", 300, 98.61, TimeArray(dates, loadbus2_ts)),
-            StaticLoad("Bus3", true, nodes5[3], "P", 300, 98.61, TimeArray(dates, loadbus3_ts)),
-            StaticLoad("Bus4", true, nodes5[4], "P", 400, 131.47, TimeArray(dates, loadbus4_ts)),
+loads5_DA = [  StaticLoad("Bus2", true, nodes5[2], "P", 300, 98.61, TimeArray(DayAhead, loadbus2_ts_DA)),
+            StaticLoad("Bus3", true, nodes5[3], "P", 300, 98.61, TimeArray(DayAhead, loadbus3_ts_DA)),
+            StaticLoad("Bus4", true, nodes5[4], "P", 400, 131.47, TimeArray(DayAhead, loadbus4_ts_DA)),
         ]
-            
