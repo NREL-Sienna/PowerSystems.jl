cae84e8a76223cecb5f0082a92059464aa06797f
Jose Daniel Lara <jdlara@berkeley.edu>
2018-03-08 17:23:04 -0800

    update system data for 5bus and 14bus

--- data_files/data_14bus.jl
+++ data_files/data_14bus.jl
@@ -2,9 +2,11 @@ using PowerSystems
 using TimeSeries
 using Missings
 
-FourteenBus = SystemParam(14, 230, 100, 1);
+dates  = collect(DateTime("1/1/2024  0:00:00", "d/m/y  H:M:S"):Hour(1):DateTime("1/1/2024  23:00:00", "d/m/y  H:M:S"))
 
-nodes_ac = [
+FourteenBus = SystemParam(14, 230, 100, length(dates));
+
+nodes14= [
                 Bus(1 , "Bus 1"  , "SF" ,      0 , 1.06  , (1.06,0.94), 69),
                 Bus(2 , "Bus 2"  , "PV" ,  -4.98 , 1.045 , (1.06,0.94), 69),
                 Bus(3 , "Bus 3"  , "PV" , -12.72 , 1.01  , (1.06,0.94), 69),
@@ -21,62 +23,138 @@ nodes_ac = [
                 Bus(14, "Bus 14" , "PQ" ,  -16.04, 1.036 , (1.06,0.94), 13.8)
             ]
 
-Branches = [
-                Line("Line1",  true, (nodes_ac[1],nodes_ac[2]),   0.01938, 0.05917,  0.0528, Inf, missing),
-                Line("Line2",  true, (nodes_ac[1],nodes_ac[5]),   0.05403, 0.22304,  0.0492, Inf, missing),
-                Line("Line3",  true, (nodes_ac[2],nodes_ac[3]),   0.04699, 0.19797,  0.0438, Inf, missing),
-                Line("Line4",  true, (nodes_ac[2],nodes_ac[4]),   0.05811, 0.17632,  0.0340, Inf, missing),
-                Line("Line5",  true, (nodes_ac[2],nodes_ac[5]),   0.05695, 0.17388,  0.0346, Inf, missing),
-                Line("Line6",  true, (nodes_ac[3],nodes_ac[4]),   0.06701, 0.17103,  0.0128, Inf, missing),
-                Line("Line7",  true, (nodes_ac[4],nodes_ac[5]),   0.01335, 0.04211,  0.0   , Inf, missing),
-                Transformer2W("Trans3", true, (nodes_ac[4],nodes_ac[7]),  0.0    , 0.20912,  0.0    , 0.978, 0.0, Inf),
-                Transformer2W("Trans1", true, (nodes_ac[4],nodes_ac[9]),  0.0    , 0.55618,  0.0     , 0.969, 0.0, Inf),
-                Transformer2W("Trans2", true, (nodes_ac[5],nodes_ac[6]),  0.0    , 0.25202,  0.0     , 0.932, 0.0, Inf),
-                Line("Line8",  true, (nodes_ac[6],nodes_ac[11]),  0.09498, 0.19890,  0.0   , Inf, missing),   
-                Line("Line9",  true, (nodes_ac[6],nodes_ac[12]),  0.12291, 0.25581,  0.0   , Inf, missing),    
-                Line("Line10", true, (nodes_ac[6],nodes_ac[13]),  0.06615, 0.13027,  0.0   , Inf, missing), 
-                Transformer2W("Trans4", true, (nodes_ac[7],nodes_ac[8]),  0.0      , 0.17615,  0.0     , 1.0,   0.0, Inf),
-                Line("Line16", true, (nodes_ac[7],nodes_ac[9]),   0.0,     0.11001,  0.0   , Inf, missing),   
-                Line("Line11", true, (nodes_ac[9],nodes_ac[10]),  0.03181, 0.08450,  0.0   , Inf, missing),    
-                Line("Line12", true, (nodes_ac[9],nodes_ac[14]),  0.12711, 0.27038,  0.0   , Inf, missing),    
-                Line("Line13", true, (nodes_ac[10],nodes_ac[11]), 0.08205, 0.19207,  0.0   , Inf, missing),    
-                Line("Line14", true, (nodes_ac[12],nodes_ac[13]), 0.22092, 0.19988,  0.0   , Inf, missing),    
-                Line("Line15", true, (nodes_ac[13],nodes_ac[14]), 0.17093, 0.34802,  0.0   , Inf, missing)
+branches14 = [
+                Line("Line1",  true, (nodes14[1],nodes14[2]),   0.01938, 0.05917,  0.0528, Inf, missing),
+                Line("Line2",  true, (nodes14[1],nodes14[5]),   0.05403, 0.22304,  0.0492, Inf, missing),
+                Line("Line3",  true, (nodes14[2],nodes14[3]),   0.04699, 0.19797,  0.0438, Inf, missing),
+                Line("Line4",  true, (nodes14[2],nodes14[4]),   0.05811, 0.17632,  0.0340, Inf, missing),
+                Line("Line5",  true, (nodes14[2],nodes14[5]),   0.05695, 0.17388,  0.0346, Inf, missing),
+                Line("Line6",  true, (nodes14[3],nodes14[4]),   0.06701, 0.17103,  0.0128, Inf, missing),
+                Line("Line7",  true, (nodes14[4],nodes14[5]),   0.01335, 0.04211,  0.0   , Inf, missing),
+                Transformer2W("Trans3", true, (nodes14[4],nodes14[7]),  0.0    , 0.20912,  0.0    , 0.978, 0.0, Inf),
+                Transformer2W("Trans1", true, (nodes14[4],nodes14[9]),  0.0    , 0.55618,  0.0     , 0.969, 0.0, Inf),
+                Transformer2W("Trans2", true, (nodes14[5],nodes14[6]),  0.0    , 0.25202,  0.0     , 0.932, 0.0, Inf),
+                Line("Line8",  true, (nodes14[6],nodes14[11]),  0.09498, 0.19890,  0.0   , Inf, missing),   
+                Line("Line9",  true, (nodes14[6],nodes14[12]),  0.12291, 0.25581,  0.0   , Inf, missing),    
+                Line("Line10", true, (nodes14[6],nodes14[13]),  0.06615, 0.13027,  0.0   , Inf, missing), 
+                Transformer2W("Trans4", true, (nodes14[7],nodes14[8]),  0.0      , 0.17615,  0.0     , 1.0,   0.0, Inf),
+                Line("Line16", true, (nodes14[7],nodes14[9]),   0.0,     0.11001,  0.0   , Inf, missing),   
+                Line("Line11", true, (nodes14[9],nodes14[10]),  0.03181, 0.08450,  0.0   , Inf, missing),    
+                Line("Line12", true, (nodes14[9],nodes14[14]),  0.12711, 0.27038,  0.0   , Inf, missing),    
+                Line("Line13", true, (nodes14[10],nodes14[11]), 0.08205, 0.19207,  0.0   , Inf, missing),    
+                Line("Line14", true, (nodes14[12],nodes14[13]), 0.22092, 0.19988,  0.0   , Inf, missing),    
+                Line("Line15", true, (nodes14[13],nodes14[14]), 0.17093, 0.34802,  0.0   , Inf, missing)
             ]   
 
-Net = Network(FourteenBus, Branches, nodes_ac) 
+Net14 = Network(FourteenBus, branches14, nodes14) 
 
-generators = [  ThermalGen("Bus1", true, nodes_ac[1],
+generators14 = [ThermalGen("Bus1", true, nodes14[1],
                 TechGen(200, (0, 200), -16.9, (-990, 990), missing, missing),
                 EconGen(40, x -> 0.04303*x^2 + 20*x, 0.0, 0.0, 0.0, missing)
                 ), 
-                ThermalGen("Bus2", true, nodes_ac[2],
+                ThermalGen("Bus2", true, nodes14[2],
                 TechGen(40, (0, 140), 42.4, (-40, 50), missing, missing),
                 EconGen(140, x -> 0.25*x^2 + 20*x, 0.0, 0.0, 0.0, missing)
                 ), 
-                ThermalGen("Bus3", true, nodes_ac[3],
+                ThermalGen("Bus3", true, nodes14[3],
                 TechGen(50, (0, 100), 23.4, (0, 40), missing, missing),
                 EconGen(100, x -> 0.01*x^2 + 40*x, 0.0, 0.0, 0.0, missing)
                 ),                
-                ThermalGen("Bus6", true, nodes_ac[6],
+                ThermalGen("Bus6", true, nodes14[6],
                 TechGen(0, (0, 100), 12.2, (-6, 24), missing, missing),
                 (EconGen(100, x -> 0.01*x^2 + 40*x, 0.0, 0.0, 0.0, missing))
                 ),    
-                ThermalGen("Bus8", true, nodes_ac[8],
+                ThermalGen("Bus8", true, nodes14[8],
                 TechGen(0, (0, 100), 17.4, (-6, 24), missing, missing),
                 EconGen(100, x -> 0.01*x^2 + 40*x, 0.0, 0.0, 0.0, missing)
                 )
             ];
 
-loads = [ StaticLoad("Bus2", nodes_ac[2], "P", 21.7, 12.7, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus3", nodes_ac[3], "P", 94.2, 19, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus4", nodes_ac[4], "P", 47.8, -3.9, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus5", nodes_ac[5], "P", 7.6, 1.6, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus6", nodes_ac[6], "P", 11.2, 7.5, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus9", nodes_ac[9], "P", 29.5, 16.6, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus10", nodes_ac[10], "P", 9, 5.8, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus11", nodes_ac[11], "P", 3.5, 1.8, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus12", nodes_ac[12], "P", 6.1, 1.6, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus13", nodes_ac[13], "P", 13.5, 5.8, TimeArray(DateTime(today()), [1.0])),
-          StaticLoad("Bus14", nodes_ac[14], "P", 14.9, 5, TimeArray(DateTime(today()), [1.0]))
+
+loadz1_ts = [ 0.792729978
+            0.723201574
+            0.710952098
+            0.677672816
+            0.668249175
+            0.67166919
+            0.687608809
+            0.711821241
+            0.756320618
+            0.7984057
+            0.827836527
+            0.840362459
+            0.84511032
+            0.834592803
+            0.822949221
+            0.816941743
+            0.824079963
+            0.905735139
+            0.989967048
+            1
+            0.991227765
+            0.960842114
+            0.921465115
+            0.837001437 ] 
+
+loadz2_ts = [ 0.831093782
+              0.689863228
+              0.666058513
+              0.627033103
+              0.624901388
+              0.62858924
+              0.650734211
+              0.683424321
+              0.750876413
+              0.828347191
+              0.884248576
+              0.888523615
+              0.87752169
+              0.847534405
+              0.8227661
+              0.803809323
+              0.813282799
+              0.907575962
+              0.98679848
+              1
+              0.990489904
+              0.952520972
+              0.906611479
+              0.824307054]     
+
+loadz3_ts = [ 0.871297342
+              0.670489749
+              0.642812243
+              0.630092987
+              0.652991383
+              0.671971681
+              0.716278493
+              0.770885833
+              0.810075243
+              0.85562361
+              0.892440566
+              0.910660449
+              0.922135467
+              0.898416969
+              0.879816542
+              0.896390855
+              0.978598576
+              0.96523761
+              1
+              0.969626503
+              0.901212601
+              0.81894251
+              0.771004923
+              0.717847996]   
+            
+loads14 = [ StaticLoad("Bus2", true, nodes14[2], "P", 21.7, 12.7, TimeArray(dates, loadz1_ts)),
+          StaticLoad("Bus3", true, nodes14[3], "P", 94.2, 19, TimeArray(dates, loadz1_ts)),
+          StaticLoad("Bus4", true, nodes14[4], "P", 47.8, -3.9, TimeArray(dates, loadz3_ts)),
+          StaticLoad("Bus5", true, nodes14[5], "P", 7.6, 1.6, TimeArray(dates, loadz1_ts)),
+          StaticLoad("Bus6", true, nodes14[6], "P", 11.2, 7.5, TimeArray(dates, loadz2_ts)),
+          StaticLoad("Bus9", true, nodes14[9], "P", 29.5, 16.6, TimeArray(dates, loadz3_ts)),
+          StaticLoad("Bus10", true, nodes14[10], "P", 9, 5.8, TimeArray(dates, loadz2_ts)),
+          StaticLoad("Bus11", true, nodes14[11], "P", 3.5, 1.8, TimeArray(dates, loadz2_ts)),
+          StaticLoad("Bus12", true, nodes14[12], "P", 6.1, 1.6, TimeArray(dates, loadz2_ts)),
+          StaticLoad("Bus13", true, nodes14[13], "P", 13.5, 5.8, TimeArray(dates, loadz2_ts)),
+          StaticLoad("Bus14", true, nodes14[14], "P", 14.9, 5, TimeArray(dates, loadz2_ts))
           ]
--- data_files/data_5bus.jl
+++ data_files/data_5bus.jl
@@ -2,51 +2,130 @@ using PowerSystems
 using TimeSeries
 using Missings
 
-dates  = DateTime(today())
+dates  = collect(DateTime("1/1/2024  0:00:00", "d/m/y  H:M:S"):Hour(1):DateTime("1/1/2024  23:00:00", "d/m/y  H:M:S"))
 
-FiveBus = SystemParam(5, 230, 100, 1);
+FiveBus = SystemParam(5, 230, 100, length(dates));
 
-nodes    = [Bus(1,"nodeA", "PV", 0, 1.0, (1.05, 0.9), 230),
-            Bus(2,"nodeB", "PQ", 0, 1.0, (1.05, 0.9), 230),
-            Bus(3,"nodeC", "PV", 0, 1.0, (1.05, 0.9), 230),
-            Bus(4,"nodeD", "PV", 0, 1.0, (1.05, 0.9), 230),
-            Bus(5,"nodeE", "SF", 0, 1.0, (1.05, 0.9), 230),
+nodes5    = [Bus(1,"nodeA", "PV", 0, 1.0, (1.05, 0.9), 230),
+             Bus(2,"nodeB", "PQ", 0, 1.0, (1.05, 0.9), 230),
+             Bus(3,"nodeC", "PV", 0, 1.0, (1.05, 0.9), 230),
+             Bus(4,"nodeD", "PV", 0, 1.0, (1.05, 0.9), 230),
+             Bus(5,"nodeE", "SF", 0, 1.0, (1.05, 0.9), 230),
         ];
 
-Branches = [Line("1", true, (nodes[1],nodes[2]), 0.00281, 0.0281, 0.00712, 400.0, missing),
-            Line("2", true, (nodes[1],nodes[4]), 0.00304, 0.0304, 0.00658, Inf, missing),
-            Line("3", true, (nodes[1],nodes[5]), 0.00064, 0.0064, 0.03126, Inf, missing),
-            Line("4", true, (nodes[2],nodes[3]), 0.00108, 0.0108, 0.01852, Inf, missing),     
-            Line("5", true, (nodes[3],nodes[4]), 0.00297, 0.0297, 0.00674, Inf, missing),
-            Line("6", true, (nodes[4],nodes[5]), 0.00297, 0.0297, 0.00674, 240, missing)
+branches5 = [Line("1", true, (nodes5[1],nodes5[2]), 0.00281, 0.0281, 0.00712, 400.0, missing),
+            Line("2", true, (nodes5[1],nodes5[4]), 0.00304, 0.0304, 0.00658, Inf, missing),
+            Line("3", true, (nodes5[1],nodes5[5]), 0.00064, 0.0064, 0.03126, Inf, missing),
+            Line("4", true, (nodes5[2],nodes5[3]), 0.00108, 0.0108, 0.01852, Inf, missing),     
+            Line("5", true, (nodes5[3],nodes5[4]), 0.00297, 0.0297, 0.00674, Inf, missing),
+            Line("6", true, (nodes5[4],nodes5[5]), 0.00297, 0.0297, 0.00674, 240, missing)
 ];     
 
-Net = Network(FiveBus, Branches, nodes); 
+Net5 = Network(FiveBus, branches5, nodes5); 
 
-Generators = [  ThermalGen("Alta", true, nodes[1],
-                TechGen(40, (0, 40), 10, (-30, 30), missing, missing),
-                EconGen(40, 14.0, 0.0, 0.0, 0.0, missing)
+generators5 = [  ThermalGen("Alta", true, nodes5[1],
+                    TechGen(40, (0, 40), 10, (-30, 30), missing, missing),
+                    EconGen(40, 14.0, 0.0, 0.0, 0.0, missing)
                 ), 
-                ThermalGen("Park City", true, nodes[1],
-                TechGen(170, (0, 170), 20, (-127.5, 127.5), missing, missing),
-                EconGen(170, 15.0, 0.0, 0.0, 0.0, missing)
+                ThermalGen("Park City", true, nodes5[1],
+                    TechGen(170, (0, 170), 20, (-127.5, 127.5), missing, missing),
+                    EconGen(170, 15.0, 0.0, 0.0, 0.0, missing)
                 ), 
-                ThermalGen("Solitude", true, nodes[3],
-                TechGen(520, (0, 520), 100, (-390, 390), missing, missing),
-                EconGen(520, 30.0, 0.0, 0.0, 0.0, missing)
+                ThermalGen("Solitude", true, nodes5[3],
+                    TechGen(520, (0, 520), 100, (-390, 390), missing, missing),
+                    EconGen(520, 30.0, 0.0, 0.0, 0.0, missing)
                 ),                
-                ThermalGen("Sundance", true, nodes[4],
-                TechGen(200, (0, 200), 40, (-150, 150), missing, missing),
-                (EconGen(200, 40.0, 0.0, 0.0, 0.0, missing))
+                ThermalGen("Sundance", true, nodes5[4],
+                    TechGen(200, (0, 200), 40, (-150, 150), missing, missing),
+                    EconGen(200, 40.0, 0.0, 0.0, 0.0, missing)
                 ),    
-                ThermalGen("Brighton", true, nodes[5],
-                TechGen(600, (0, 600), 150, (-450, 450), missing, missing),
-                EconGen(600, 10.0, 0.0, 0.0, 0.0, missing)
+                ThermalGen("Brighton", true, nodes5[5],
+                    TechGen(600, (0, 600), 150, (-450, 450), missing, missing),
+                    EconGen(600, 10.0, 0.0, 0.0, 0.0, missing)
+                ), 
+                ReFix("SolarC", true, nodes5[3], 
+                    TechRE()
+
                 )
             ];
-            
-Loads = [   StaticLoad("Bus2", nodes[2], "P", 300, 98.61, TimeArray(DateTime(today()), [1.0])),
-            StaticLoad("Bus3", nodes[3], "P", 300, 98.61, TimeArray(DateTime(today()), [1.0])),
-            StaticLoad("Bus4", nodes[4], "P", 400, 131.47, TimeArray(DateTime(today()), [1.0])),
+
+loadbus2_ts = [ 0.792729978
+              0.723201574
+              0.710952098
+              0.677672816
+              0.668249175
+              0.67166919
+              0.687608809
+              0.711821241
+              0.756320618
+              0.7984057
+              0.827836527
+              0.840362459
+              0.84511032
+              0.834592803
+              0.822949221
+              0.816941743
+              0.824079963
+              0.905735139
+              0.989967048
+              1
+              0.991227765
+              0.960842114
+              0.921465115
+              0.837001437 ] 
+
+loadbus3_ts = [ 0.831093782
+                0.689863228
+                0.666058513
+                0.627033103
+                0.624901388
+                0.62858924
+                0.650734211
+                0.683424321
+                0.750876413
+                0.828347191
+                0.884248576
+                0.888523615
+                0.87752169
+                0.847534405
+                0.8227661
+                0.803809323
+                0.813282799
+                0.907575962
+                0.98679848
+                1
+                0.990489904
+                0.952520972
+                0.906611479
+                0.824307054]     
+
+loadbus4_ts = [ 0.871297342
+                0.670489749
+                0.642812243
+                0.630092987
+                0.652991383
+                0.671971681
+                0.716278493
+                0.770885833
+                0.810075243
+                0.85562361
+                0.892440566
+                0.910660449
+                0.922135467
+                0.898416969
+                0.879816542
+                0.896390855
+                0.978598576
+                0.96523761
+                1
+                0.969626503
+                0.901212601
+                0.81894251
+                0.771004923
+                0.717847996]            
+
+loads5 = [  StaticLoad("Bus2", true, nodes5[2], "P", 300, 98.61, TimeArray(dates, loadbus2_ts)),
+            StaticLoad("Bus3", true, nodes5[3], "P", 300, 98.61, TimeArray(dates, loadbus3_ts)),
+            StaticLoad("Bus4", true, nodes5[4], "P", 400, 131.47, TimeArray(dates, loadbus4_ts)),
         ]
             
