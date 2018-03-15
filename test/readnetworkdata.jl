407a616f3ca69ad99b7eeb4dcc640c548c32a19f
Jose Daniel Lara <jdlara@berkeley.edu>
2018-03-14 22:32:13 -0700

    update for windows testing

--- test/readnetworkdata.jl
+++ test/readnetworkdata.jl
@@ -1,4 +1,13 @@
-include("../data_files/data_5bus.jl")
-include("../data_files/data_14bus.jl")
+
+if is_linux() || is_apple()
+    include("../data_files/data_5bus.jl")
+    include("../data_files/data_14bus.jl")
+end
+
+if is_windows()
+    include("..\\data_files\\data_5bus.jl")
+    include("..\\data_files\\data_14bus.jl")
+end
+
 true
 
