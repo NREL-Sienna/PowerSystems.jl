d55b189d0e092c9bf6e030b8b509b4692180d9d6
Jose Daniel Lara <jdlara@berkeley.edu>
2018-04-20 14:59:50 -0700

    Eliminate SystemParam

--- src/models/topological_elements.jl
+++ src/models/topological_elements.jl
@@ -1,18 +1,4 @@
-export SystemParam
 export Bus
-
-struct SystemParam
-    busquantity::Int
-    basevoltage::Real # [kV]
-    basepower::Real # [MVA]
-    timesteps::Int
-end
-
-SystemParam(; busquantity = 0,
-        basevoltage = 0.0,
-        basepower = 1000,
-        timesteps = 1
-        ) = SystemParam(busquantity, basevoltage, basepower, timesteps)
 
 # Update to named tuples when Julia 0.7 becomes available 
 
