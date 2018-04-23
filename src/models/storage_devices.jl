89ffa844117f8e30c59a27a99713e715acc5ef6a
Jose Daniel Lara <jdlara@berkeley.edu>
2018-04-23 11:18:27 -0700

    add support for storage devices

--- src/models/storage_devices.jl
+++ src/models/storage_devices.jl
@@ -9,10 +9,22 @@ struct GenericBattery <: Storage
     name::String
     status::Bool
     bus::Bus
-    realpower::Real # [MW]
+    realpower::Float64 # [MW]
     inputrealpowerlimits::NamedTuple
     outputrealpowerlimits::NamedTuple
     efficiency::NamedTuple
-    reactivepower::Union{Real,Nothing} # [MVAr]
+    reactivepower::Union{Float64,Nothing} # [MVAr]
     reactivepowerlimits::Union{NamedTuple,Nothing}
 end
+
+GenericBattery(; name = "init",
+                status = false,
+                bus = Bus(),
+                realpower = 0.0,
+                inputrealpowerlimits = @NT(max = 0.0, min = 0.0),
+                outputrealpowerlimits = @NT(max = 0.0, min = 0.0),
+                efficiency = @NT(in = 0.0, out = 0.0), 
+                reactivepower = 0.0, 
+                reactivepowerlimits = @NT(max = 0.0, min = 0.0)
+                ) = GenericBattery(name, status, bus, realpower, inputrealpowerlimits, 
+                                    outputrealpowerlimits, efficiency, reactivepower, reactivepowerlimits)
