a7575706d29428b443512a238cc5c1456b14b333
Jose Daniel Lara <jdlara@berkeley.edu>
2018-04-23 11:24:19 -0700

    update tests to support new devices

--- test/constructors.jl
+++ test/constructors.jl
@@ -1,6 +1,16 @@
+# SystemConstructor
+tPowerSystem = PowerSystem()
+
+#Bus Constructor
 tBus = Bus()
+
+#Generation Constructors
 tEcongen = EconGen()
 tTechGen = TechGen() 
 tThermalGen = ThermalGen()
+tTecHydro = TecHydro()
+
+#Storage Constructots 
+tStorage = GenericBattery()
 
 true
