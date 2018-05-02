e053d579ed3c5d3a130b049f02795f03b26e5d1c
Jose Daniel Lara <jdlara@berkeley.edu>
2018-05-01 23:04:37 -0700

    update network.jl

--- src/models/network/network.jl
+++ src/models/network/network.jl
@@ -9,22 +9,17 @@ export Network
 
 struct Network
     branches::Array{Branch}
-    linecount::Int
     ybus::SparseMatrixCSC{Complex{Float64},Int64}
     ptdf::Union{Array{Float64},Nothing}
     incidence::Array{Int}
+end
 
-    function Network(branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
+function Network(branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
+    buscount = length(nodes)
+    ybus = PowerSystems.build_ybus(buscount,branches);
+    ptdf, A = PowerSystems.build_ptdf(buscount, branches, nodes)
 
-        buscount = length(nodes)
-        ybus = PowerSystems.build_ybus(buscount,branches);
-        ptdf, A = PowerSystems.build_ptdf(buscount, branches, nodes)
-        new(branches,
-            length(branches),
-            ybus,
-            ptdf,
-            A)
-    end
-
+    return Network(branches, ybus, ptdf, A) 
+    
 end
 
