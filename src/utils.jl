0859cf010df6459a1397ad003d069b8705f999d9
Jose Daniel Lara <jdlara@berkeley.edu>
2018-05-15 12:25:16 -0600

    Remove unused code

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,5 @@
 export PlotTimeSeries
 
-orderedlimits(limits::Tuple) = limits[2] < limits[1] ? error("Limits not in ascending order") :limits
-
 orderedlimits(limits::@NT(min::Float64, max::Float64)) = limits.max < limits.min ? error("Limits not in ascending order") : limits
 
 orderedlimits(limits::Nothing) = warn("Limits defined as nothing")
