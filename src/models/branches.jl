ba5922e8c55d26d234553bcadf1d20dfe14316b0
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-06-07 16:05:05 -0600

    Make include function use relative path

--- src/models/branches.jl
+++ src/models/branches.jl
@@ -2,8 +2,8 @@ abstract type
     Branch <: PowerSystemType
 end
 
-include("branches/lines.jl")
-include("branches/transformers.jl")
+include("./branches/lines.jl")
+include("./branches/transformers.jl")
 include("../utils/ybus_calculations.jl")
 include("../utils/ptdf_calculations.jl")
 
