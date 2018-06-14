64d2bc416d635acf867bc84eeca9f227378e4938
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-14 10:56:06 -0600

    Expand and complete hydro modeling

--- src/models/generation/hydro_generation.jl
+++ src/models/generation/hydro_generation.jl
@@ -25,6 +25,14 @@ TechHydro(; realpower = 0.0,
           timelimits = nothing
         ) = TechHydro(realpower, realpowerlimits, reactivepower, reactivepowerlimits, ramplimits, timelimits)
 
+
+struct EconHydro
+    curtailcost::Float64 # [$/MWh]
+    interruptioncost::Union{Float64,Nothing} # [$]
+end
+
+EconHydro(; curtailcost = 0.0, interruptioncost = nothing) = EconHydro(curtailcost, interruptioncost)
+
 struct HydroFix <: HydroGen
     name::String
     status::Bool
@@ -33,11 +41,48 @@ struct HydroFix <: HydroGen
     scalingfactor::TimeSeries.TimeArray
 end
 
+HydroFix(; name="init",
+        status = false,
+        bus = Bus(),
+        tech = TechHydro(),
+        scalingfactor = TimeSeries.TimeArray(today(), [1.0])) = HydroFix(name, status, bus, tech, scalingfactor)
+
+
+struct HydroCurtailment <: HydroGen
+    name::String
+    status::Bool
+    bus::Bus
+    tech::TechHydro
+    econ::Union{EconHydro,Nothing}
+    scalingfactor::TimeSeries.TimeArray
+    function HydroCurtailment(name, status, bus, tech, curtailcost::Float64, scalingfactor)
+        econ = EconHydro(curtailcost, nothing)
+        new(name, status, bus, tech, econ, scalingfactor)
+    end
+end
+
+HydroCurtailment(; name = "init",
+                status = false,
+                bus= Bus(),
+                tech = TechHydro(),
+                curtailcost = 0.0,
+                scalingfactor = TimeSeries.TimeArray(today(), [1.0])) = HydroCurtailment(name, status, bus, tech, curtailcost, scalingfactor)
+
+
 struct HydroStorage <: HydroGen
     name::String
     status::Bool
     bus::Bus
     tech::TechHydro
+    econ::Union{EconHydro,Nothing}
     storagecapacity::Float64
     scalingfactor::TimeSeries.TimeArray
 end
+
+HydroStorage(; name = "init",
+                status = false,
+                bus= Bus(),
+                tech = TechHydro(),
+                econ = EconHydro(),
+                storagecapacity = 0.0,
+                scalingfactor = TimeSeries.TimeArray(today(), [1.0])) = HydroStorage(name, status, bus, tech, econ, storagecapacity, scalingfactor)
