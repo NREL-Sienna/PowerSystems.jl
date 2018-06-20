2862307b9d279ece07812a7f309edd7b1ac0c25a
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-20 14:12:50 -0600

    Remove breaking condition in printing after update to generator classifier

--- src/utils/print.jl
+++ src/utils/print.jl
@@ -1,15 +1,15 @@
-function printPST(io::IO, pst::PowerSystems.PowerSystemType)
+function printPST(io::IO, pst::PowerSystems.PowerSystemDevice)
     pst_type = typeof(pst)
     print(io, "$pst_type(name=\"$(pst.name)\")")
 end
 
-Base.show(io::IO, pst::PowerSystems.PowerSystemType) = printPST(io, pst)
+Base.show(io::IO, pst::PowerSystems.PowerSystemDevice) = printPST(io, pst)
 
 function printPowerSystem(io::IO, system::PowerSystems.PowerSystem)
     number_of_buses = length(system.buses)
     number_of_branches = length(system.network.branches)
-    number_of_generators = length(collect(Iterators.flatten(values(system.generators))))
-    print(io, "$(typeof(system))(buses=$number_of_buses, generators=$number_of_generators, branches=$number_of_branches)")
+    #number_of_generators = length(collect(Iterators.flatten(values(system.generators))))
+    print(io, "$(typeof(system))(buses=$number_of_buses, branches=$number_of_branches)")
 end
 
 Base.show(io::IO, system::PowerSystems.PowerSystem) = printPowerSystem(io, system)
