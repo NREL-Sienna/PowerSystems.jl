01c474ce868e6d5b83f33b351542404ebde24328
SourabhDalvi <sdalvi@cbarrow-22573s.nrel.gov>
2018-06-22 15:00:13 -0600

    Changed transformer  susceptance to a Named Tuple(from,to)

--- src/models/branches/transformers.jl
+++ src/models/branches/transformers.jl
@@ -9,7 +9,7 @@ struct PhaseShiftingTransformer <: Branch
     connectionpoints::@NT(from::Bus, to::Bus)
     r::Float64 #[pu]
     x::Float64 #[pu]
-    zb::Float64 #[pu]
+    zb::@NT(from::Float64, to::Float64) #[pu]
     tap::Float64 # [0 - 2]
     α::Float64 # [radians]
     rate::Union{Float64,Nothing} #[MVA]
@@ -20,7 +20,7 @@ PhaseShiftingTransformer(; name = "init",
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
                 x = 0.0,
-                zb = 0.0,
+                zb = @NT(from::0.0, to::0.0),
                 tap = 1.0,
                 α = 0.0,
                 rate = nothing
@@ -32,7 +32,7 @@ struct Transformer2W <: Branch
     connectionpoints::@NT(from::Bus, to::Bus)
     r::Float64 #[pu]
     x::Float64 #[pu]
-    zb::Float64 #[pu]
+    zb::@NT(from::0.0, to::0.0) #[pu]
     tap::Float64 # [0 - 2]
     rate::Union{Float64,Nothing} #[MVA]
 end
@@ -42,7 +42,7 @@ Transformer2W(; name = "init",
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
                 x = 0.0,
-                zb = 0.0,
+                zb = @NT(from::0.0, to::0.0),
                 tap = 1.0,
                 rate = nothing
             ) = Transformer2W(name, status, connectionpoints, r, x, zb, tap, α, rate)
