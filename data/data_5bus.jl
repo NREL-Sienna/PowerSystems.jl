8edce59b93a97aad547db9c1106c9d0122612219
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-25 15:12:59 -0600

    Correct data to new line model

--- data/data_5bus.jl
+++ data/data_5bus.jl
@@ -12,12 +12,12 @@ nodes5    = [Bus(1,"nodeA", "PV", 0, 1.0, @NT(min = 0.9, max=1.05), 230),
              Bus(5,"nodeE", "SF", 0, 1.0, @NT(min = 0.9, max=1.05), 230),
         ];
 
-branches5 = [Line("1", true, @NT(from=nodes5[1],to=nodes5[2]), 0.00281, 0.0281, 0.00712, 400.0, @NT(max = 60.0, min = -60.0)),
-             Line("2", true, @NT(from=nodes5[1],to=nodes5[4]), 0.00304, 0.0304, 0.00658, 3960.0, @NT(max = 60.0, min = -60.0)),
-             Line("3", true, @NT(from=nodes5[1],to=nodes5[5]), 0.00064, 0.0064, 0.03126, 18812.0, @NT(max = 60.0, min = -60.0)),
-             Line("4", true, @NT(from=nodes5[2],to=nodes5[3]), 0.00108, 0.0108, 0.01852, 11148.0, @NT(max = 60.0, min = -60.0)),
-             Line("5", true, @NT(from=nodes5[3],to=nodes5[4]), 0.00297, 0.0297, 0.00674, 4053.0, @NT(max = 60.0, min = -60.0)),
-             Line("6", true, @NT(from=nodes5[4],to=nodes5[5]), 0.00297, 0.0297, 0.00674, 240.0, @NT(max = 60.0, min = -60.0))
+branches5 = [Line("1", true, @NT(from=nodes5[1],to=nodes5[2]), 0.00281, 0.0281, @NT(from::0.00356, to::0.00356), 400.0, @NT(max = 60.0, min = -60.0)),
+             Line("2", true, @NT(from=nodes5[1],to=nodes5[4]), 0.00304, 0.0304, @NT(from::0.00329, to::0.00329), 3960.0, @NT(max = 60.0, min = -60.0)),
+             Line("3", true, @NT(from=nodes5[1],to=nodes5[5]), 0.00064, 0.0064, @NT(from::0.01563, to::0.01563), 0.03126, 18812.0, @NT(max = 60.0, min = -60.0)),
+             Line("4", true, @NT(from=nodes5[2],to=nodes5[3]), 0.00108, 0.0108, @NT(from::0.00926, to::0.00926), 11148.0, @NT(max = 60.0, min = -60.0)),
+             Line("5", true, @NT(from=nodes5[3],to=nodes5[4]), 0.00297, 0.0297, @NT(from::0.00337, to::0.00337), 4053.0, @NT(max = 60.0, min = -60.0)),
+             Line("6", true, @NT(from=nodes5[4],to=nodes5[5]), 0.00297, 0.0297, @NT(from::0.00337, to::00.00337), 240.0, @NT(max = 60.0, min = -60.0))
 ];
 
 solar_ts_DA = [0
