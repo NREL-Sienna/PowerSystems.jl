257553645c53a735253ebf419bd728c1baca04f2
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-25 10:50:03 -0600

    transformer model update

--- src/models/branches/transformers.jl
+++ src/models/branches/transformers.jl
@@ -9,38 +9,26 @@ The 2-W transformer model uses an equivalent circuit assuming the impedance is o
 The model allocates the iron losses and magnetezing suceptance to the primary side
 """
 
-struct PhaseShiftingTransformer <: Branch
+struct Transformer2W <: Branch
     name::String
     available::Bool
     connectionpoints::@NT(from::Bus, to::Bus)
     r::Float64 #[pu]
     x::Float64 #[pu]
-    zb::@NT(primary::Complex64, secondary::Complex64) #[pu]
-    tap::Float64 # [0 - 2]
-    α::Float64 # [radians]
+    primaryshunt::Float64
     rate::Union{Float64,Nothing} #[MVA]
-
-    function PhaseShiftingTransformer(name, available, connectionpoints, r, x, zb, tap, α, rate)
-
-        TapCheck(tap, alpha, x, zb)
-
-        new(name, available, connectionpoints, r, x, zb, tap, α, rate)
-
-    end
 end
 
-PhaseShiftingTransformer(; name = "init",
+Transformer2W(; name = "init",
                 status = false,
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
                 x = 0.0,
-                zb = @NT(from::0.0, to::0.0),
-                tap = 1.0,
-                α = 0.0,
+                primaryshunt = 0.0,
                 rate = nothing
             ) = Transformer2W(name, status, connectionpoints, r, x, zb, tap, α, rate)
 
-struct Transformer2W <: Branch
+struct TapTransformer <: Branch
     name::String
     available::Bool
     connectionpoints::@NT(from::Bus, to::Bus)
@@ -52,7 +40,7 @@ struct Transformer2W <: Branch
     rate::Union{Float64,Nothing} #[MVA]
 end
 
-Transformer2W(; name = "init",
+TapTransformer(; name = "init",
                 status = false,
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
@@ -75,3 +63,33 @@ Transformer3W(; name = "init",
                 line = Line()
             ) = Transformer3W(name, status, transformer, line)
 
+            struct PhaseShiftingTransformer <: Branch
+                name::String
+                available::Bool
+                connectionpoints::@NT(from::Bus, to::Bus)
+                r::Float64 #[pu]
+                x::Float64 #[pu]
+                zb::@NT(primary::Complex64, secondary::Complex64) #[pu]
+                tap::Float64 # [0 - 2]
+                α::Float64 # [radians]
+                rate::Union{Float64,Nothing} #[MVA]
+
+                function PhaseShiftingTransformer(name, available, connectionpoints, r, x, zb, tap, α, rate)
+
+                    TapCheck(tap, alpha, x, zb)
+
+                    new(name, available, connectionpoints, r, x, zb, tap, α, rate)
+
+                end
+            end
+
+PhaseShiftingTransformer(; name = "init",
+                status = false,
+                connectionpoints = @NT(from::Bus(), to::Bus()),
+                r = 0.0,
+                x = 0.0,
+                zb = @NT(from::0.0, to::0.0),
+                tap = 1.0,
+                α = 0.0,
+                rate = nothing
+            ) = Transformer2W(name, status, connectionpoints, r, x, zb, tap, α, rate)
