ed45c5aecbf82c35d53a22d22b621403e8e92d1b
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-26 10:31:12 -0600

    minor fix in PTDF calculations

--- src/utils/ptdf_calculations.jl
+++ src/utils/ptdf_calculations.jl
@@ -1,5 +1,7 @@
-function build_ptdf(buscount, branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
+# TODO: Enable distributed slack buses
+function build_ptdf(branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
 
+    buscount = length(nodes)
     linecount = length(branches)
 
     for b in nodes
@@ -21,7 +23,12 @@ function build_ptdf(buscount, branches::Array{T}, nodes::Array{Bus}) where {T<:B
 
         A[b.connectionpoints.to.number, ix] = -1;
 
-        if typeof(b) == PowerSystems.Transformer2W
+        if isa(b,PowerSystems.Transformer2W)
+
+            Y11 = 1/b.x;
+            X[ix,ix] = b.x;
+
+        elseif isa(b,PowerSystems.TapTransformer)
 
             Y11 = (1/(b.tap*b.x));
             X[ix,ix] = b.x*b.tap;
@@ -65,7 +72,7 @@ function build_ptdf(buscount, branches::Array{T}, nodes::Array{Bus}) where {T<:B
 
         S = inv(full(X))*A[setdiff(1:end, slack_position), :]'*inv(full(B));
 
-        S = hcat(S[:,1:slack_position-1],zeros(linecount,),S[:,slack_position:end-1])
+        S = hcat(S[:,1:slack_position-1],zeros(linecount,),S[:,slack_position:end])
 
     elseif slack_position == -9
 
