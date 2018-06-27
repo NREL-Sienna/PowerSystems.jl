4a1bec04772e83f649c928d7978af5b74edb06e1
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-27 15:44:09 -0600

    Move the location of the check codes for PowerSystems

--- src/utils/base_checks.jl
+++ src/utils/base_checks.jl
@@ -1,7 +1,7 @@
-function orderedlimits(limits::@NT(min::Float64, max::Float64), limsname::String) 
+function orderedlimits(limits::@NT(min::Float64, max::Float64), limsname::String)
     limits.max < limits.min ? error("'$limsname' limits not in ascending order") : limits
 end
 
-function orderedlimits(limits::Nothing,limsname::String) 
+function orderedlimits(limits::Nothing,limsname::String)
     info("'$limsname' limits defined as nothing")
 end
