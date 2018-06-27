c326a48f7672cfe1554f992bad07443e90a76a3f
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-27 15:44:44 -0600

    Remove the use of Named Tuples and create a struct to classify the generators

--- src/models/generation.jl
+++ src/models/generation.jl
@@ -5,3 +5,26 @@ end
 include("generation/renewable_generation.jl")
 include("generation/thermal_generation.jl")
 include("generation/hydro_generation.jl")
+
+struct Sources{T <: Union{Nothing,Array{<:ThermalGen,1}}, R <: Union{Nothing,Array{<:RenewableGen,1}}, H <: Union{Nothing,Array{<:HydroGen,1}}}
+    thermal::T
+    renewable::R
+    hydro::H
+end
+
+# Generator Classifier
+function GenClassifier(gen::Array{T}) where T <: PowerSystems.Generator
+
+    t = [d for d in gen if isa(d, PowerSystems.ThermalGen)]
+    r = [d for d in gen if isa(d, PowerSystems.RenewableGen)]
+    h = [d for d in gen if isa(d, PowerSystems.HydroGen)]
+
+    #Check for type stability
+    isempty(t) ? t = nothing: t
+    isempty(r) ? r = nothing: r
+    isempty(h) ? h = nothing: h
+
+    generators = Sources(t,r,h)
+
+    return generators
+end
