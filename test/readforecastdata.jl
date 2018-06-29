02ed3a1f44c019ac1d7fcbe1bbc215444c04da8d
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-06-29 08:22:10 -0600

    update forecast parser test

--- test/readforecastdata.jl
+++ test/readforecastdata.jl
@@ -9,7 +9,7 @@ ps_dict = PowerSystems.add_realtime_ts(ps_dict,time_series)
 
 Buses, Generators, Branches, Loads  = PowerSystems.ps_dict2ps_struct(ps_dict)
 
-forecast = PowerSystems.make_forecast_dict(time_series,Day(1),24,Generators)
+forecast = PowerSystems.make_forecast_dict(time_series,Day(1),24,Array{PowerSystems.PowerSystemDevice,1}(Generators))
 
 Forecast_Struct = PowerSystems.make_forecast_array(forecast)
 
