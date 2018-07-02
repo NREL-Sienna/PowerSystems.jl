f1eb2802e71b0b1fad82177606fc178475a1d702
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-02 16:26:16 -0600

    Updated phase shifting transformers

--- src/models/branches/transformers.jl
+++ src/models/branches/transformers.jl
@@ -56,33 +56,25 @@ Transformer3W(; name = "init",
                 line = Line()
             ) = Transformer3W(name, status, transformer, line)
 
-            struct PhaseShiftingTransformer <: Branch
-                name::String
-                available::Bool
-                connectionpoints::@NT(from::Bus, to::Bus)
-                r::Float64 #[pu]
-                x::Float64 #[pu]
-                zb::@NT(primary::Complex64, secondary::Complex64) #[pu]
-                tap::Float64 # [0 - 2]
-                α::Float64 # [radians]
-                rate::Union{Float64,Nothing} #[MVA]
-
-                function PhaseShiftingTransformer(name, available, connectionpoints, r, x, zb, tap, α, rate)
-
-                    TapCheck(tap, alpha, x, zb)
-
-                    new(name, available, connectionpoints, r, x, zb, tap, α, rate)
-
-                end
-            end
+struct PhaseShiftingTransformer <: Branch
+    name::String
+    available::Bool
+    connectionpoints::@NT(from::Bus, to::Bus)
+    r::Float64 #[pu]
+    x::Float64 #[pu]
+    primaryshunt::Float64
+    tap::Float64 # [0 - 2]
+    α::Float64 # [radians]
+    rate::Union{Float64,Nothing} #[MVA]
+end
 
 PhaseShiftingTransformer(; name = "init",
                 status = false,
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
                 x = 0.0,
-                zb = @NT(from::0.0, to::0.0),
+                primaryshunt::0.0,
                 tap = 1.0,
                 α = 0.0,
                 rate = nothing
-            ) = Transformer2W(name, status, connectionpoints, r, x, zb, tap, α, rate)
+            ) = Transformer2W(name, status, connectionpoints, r, x, primaryshunt, tap, α, rate)
