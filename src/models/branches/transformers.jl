ba2e71c6c0d6a93409cef8cedbb87f03ca8bf5b2
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-03 08:28:51 -0600

    conflict resolved

--- src/models/branches/transformers.jl
+++ src/models/branches/transformers.jl
@@ -56,6 +56,7 @@ Transformer3W(; name = "init",
                 line = Line()
             ) = Transformer3W(name, status, transformer, line)
 
+<<<<<<< HEAD
             struct PhaseShiftingTransformer <: Branch
                 name::String
                 available::Bool
@@ -67,13 +68,30 @@ Transformer3W(; name = "init",
                 α::Float64 # [radians]
                 rate::Union{Float64,Nothing} #[MVA]
             end
+=======
+struct PhaseShiftingTransformer <: Branch
+    name::String
+    available::Bool
+    connectionpoints::@NT(from::Bus, to::Bus)
+    r::Float64 #[pu]
+    x::Float64 #[pu]
+    primaryshunt::Float64
+    tap::Float64 # [0 - 2]
+    α::Float64 # [radians]
+    rate::Union{Float64,Nothing} #[MVA]
+end
+>>>>>>> Updated phase shifting transformers
 
 PhaseShiftingTransformer(; name = "init",
                 status = false,
                 connectionpoints = @NT(from::Bus(), to::Bus()),
                 r = 0.0,
                 x = 0.0,
+<<<<<<< HEAD
+=======
+                primaryshunt::0.0,
+>>>>>>> Updated phase shifting transformers
                 tap = 1.0,
                 α = 0.0,
                 rate = nothing
-            ) = Transformer2W(name, status, connectionpoints, r, x, zb, tap, α, rate)
+            ) = Transformer2W(name, status, connectionpoints, r, x, primaryshunt, tap, α, rate)
