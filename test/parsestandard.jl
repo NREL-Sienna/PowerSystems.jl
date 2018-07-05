a915ccedf42f4520b6747bd4c5f36d3bcb8117d0
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-05 11:31:02 -0600

    Added Contruction of PowerSystems Struct in Parsing Test

--- test/parsestandard.jl
+++ test/parsestandard.jl
@@ -9,8 +9,14 @@ for f in files
     try
         ext = match(file_ext, f)
         print("Parsing $f ...\n")
-        ParseStandardFiles(joinpath(Pkg.dir(),"PowerSystems/data/matpower",f))
-        println("Successfully parsed $f")
+        pm_dict = ParseStandardFiles(joinpath(Pkg.dir(),"PowerSystems/data/matpower",f))
+        println("Successfully parsed $f to PowerModels dict")
+        ps_dict = PowerSystems.pm2ps_dict(pm_dict)
+        println("Successfully parsed $f to PowerSystems dict")
+        Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
+        println("Successfully parsed $f to PowerSystems devices")
+        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,230.0,1000.0) # TODO: Add DClines, Shunts , LoadZones
+        println("Successfully parsed $f to PowerSystem struct")
     catch
         warn("Error while parsing $f")
         catch_stacktrace()
