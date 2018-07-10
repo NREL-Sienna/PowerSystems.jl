5f65ed319867952223bf1fdb7b54fdda2ce2aac1
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-10 10:02:03 -0600

    Updated thermal limit check

--- src/models/branches/lines.jl
+++ src/models/branches/lines.jl
@@ -12,19 +12,23 @@ function check_angle_limits!(anglelimits::@NT(max::Float64, min::Float64))
 end
 
 
-function calculate_thermal_limits!(r::Float64, x::Float64, rate::@NT(from_to::Float64, to_from::Float64), anglelimits::@NT(max::Float64, min::Float64))
+function calculate_thermal_limits!(r::Float64, x::Float64, rate::@NT(from_to::Float64, to_from::Float64), anglelimits::@NT(max::Float64, min::Float64), connectionpoints::@NT(from::Bus, to::Bus))
     theta_max = max(abs(anglelimits.min), abs(anglelimits.max))
     g =  r / (r^2 + x^2)
     b = -x / (r^2 + x^2)
     y_mag = sqrt(g^2 + b^2)
     fr_vmax = connectionpoints.from.voltagelimits.max
     to_vmax =  connectionpoints.to.voltagelimits.max
+    if isa(fr_vmax,Nothing) || isa(to_vmax,Nothing)
+        fr_vmax = 1.0
+        to_vmax = 0.9
+        diff_angle = abs(connectionpoints.from.angle -connectionpoints.to.angle)
+        new_rate = y_mag*fr_vmax*to_vmax*cos(theta_max)
+    else
     m_vmax = max(fr_vmax, to_vmax)
     c_max = sqrt(fr_vmax^2 + to_vmax^2 - 2*fr_vmax*to_vmax*cos(theta_max))
-    rating_from_to = Float64[]
-    rating_to_from  = Float64[]
     new_rate = y_mag*m_vmax*c_max
-
+    end
     rate.from_to <= 0.0 ? rating_from_to = new_rate : rate.from_to > new_rate ? rating_from_to = new_rate : rating_from_to = rate.from_to
     rate.to_from <= 0.0 ? rating_to_from = new_rate : rate.to_from > new_rate ? rating_to_from = new_rate : rating_to_from = rate.to_from
 
@@ -33,22 +37,23 @@ end
 
 struct Line <: Branch
     name::String
-    available
+    available::Bool
     connectionpoints::@NT(from::Bus, to::Bus)
     r::Float64 #[pu]
-    x::Float64 #[pu]
+    x::Float64 #[pu]Co
     b::@NT(from::Float64, to::Float64) #[pu]
     rate::@NT(from_to::Float64, to_from::Float64) #MW
     anglelimits::@NT(max::Float64, min::Float64) #Degrees
 
-    function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64) rate, anglelimits)
+    function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64), rate, anglelimits)
 
         anglelimits = check_angle_limits!(anglelimits)
 
-        rating =  calculate_thermal_limits(r, x, connectionpoints @NT(from_to = rate, to_from = rate), anglelimits)
+        rating =  calculate_thermal_limits!(r, x,  @NT(from_to = rate, to_from = rate), anglelimits, connectionpoints)
 
         new(name, available, connectionpoints, r, x, b, rating, anglelimits)
     end
+
 end
 
 Line(;  name = "init",
