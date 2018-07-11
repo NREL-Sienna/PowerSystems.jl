20b8dd5c75f0050e69b6cfe1b6ec26268b67cdc1
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-11 15:01:47 -0600

    Added distributed bus

--- src/utils/ptdf_calculations.jl
+++ src/utils/ptdf_calculations.jl
@@ -1,4 +1,4 @@
-function build_ptdf(branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
+function build_ptdf(branches::Array{T}, nodes::Array{Bus}, dist_slack::Array{Float64}=[0.1] ) where {T<:Branch}
 
     buscount = length(nodes)
     linecount = length(branches)
@@ -44,21 +44,24 @@ function build_ptdf(branches::Array{T}, nodes::Array{Bus}) where {T<:Branch}
             error("3W Transformer not implemented about PTDF")
         end
     end
-    slack_position = [num_bus[n.number] for n in nodes if n.bustype == "SF"]
-                
-    B = gemm('N','T', gemm('N','N',A[setdiff(1:end, slack_position),1:end] ,inv_X), A[setdiff(1:end, slack_position),1:end])
+    slacks = [num_bus[n.number] for n in nodes if n.bustype == "SF"]
+    slack_position = slacks[1]            
+    B = gemm('N','T', gemm('N','N',A[setdiff(1:end, slacks[1]),1:end] ,inv_X), A[setdiff(1:end, slacks[1]),1:end])
 
-    if length(slack_position) == 1
-        slack_position = slack_position[1]
+    if dist_slack[1] == 0.1 && length(dist_slack) ==1
         (B, bipiv, binfo) = getrf!(B)
         S_ = gemm('N','N', gemm('N','T', inv_X, A[setdiff(1:end, slack_position), :]), getri!(B, bipiv) )
         S = hcat(S_[:,1:slack_position-1],zeros(linecount,),S_[:,slack_position:end])
-    elseif length(slack_position) > 1
+                    
+    elseif dist_slack[1] != 0.1 && length(dist_slack)  == buscount
+        println("Distributed bus")
         (B, bipiv, binfo) = getrf!(B)
         S_ = gemm('N','N', gemm('N','T', inv_X, A[setdiff(1:end, slack_position), :]), getri!(B, bipiv) )
-        slack_array = [x in slack_position ? 1 : 0 for x in 1:buscount]
-        slack_array =slack_array/sum(slack_array)
-        S = S_*ones(buscount,buscount) - slack_array*ones(1,buscount)
+        S = hcat(S_[:,1:slack_position-1],zeros(linecount,),S_[:,slack_position:end])
+        slack_array =dist_slack/sum(dist_slack)
+        slack_array = reshape(slack_array,buscount,1)
+        S = S - gemm('N','N',gemm('N','N',S,slack_array),ones(1,buscount))
+                    
     elseif length(slack_position) == 0
         warn("Slack bus not identified in the Bus/Nodes list, can't build PTLDF")
         S = nothing
