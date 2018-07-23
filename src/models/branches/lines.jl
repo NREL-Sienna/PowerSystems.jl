0c2d7775162e12bcd88299928928d957201c04df
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-23 12:12:39 -0600

    Remove the ifs based on type.

--- src/models/branches/lines.jl
+++ src/models/branches/lines.jl
@@ -9,13 +9,8 @@ struct Line <: Branch
     anglelimits::@NT(max::Float64, min::Float64) #Degrees
 end
 
-function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64), rate, anglelimits)
-    if isa(rate,Float64)
-        rating =  @NT(from_to=rate, to_from=rate)
-        Line(name, available, connectionpoints, r, x, b, rating, anglelimits)
-    else
-        Line(name, available, connectionpoints, r, x, b, rate, anglelimits)
-    end
+function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64), rate::Float64,anglelimits::Float64)
+    return Line(name, available, connectionpoints, r, x, b, @NT(from_to=rate, to_from=rate), @NT(max=anglelimits, min=-1*anglelimits))
 end
 
 
