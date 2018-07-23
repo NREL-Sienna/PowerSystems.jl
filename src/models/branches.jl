6fdb81825e60c253a42f334f89b7badaec62fc2c
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-23 10:32:45 -0600

    Add new dc_line configuration to file structure

--- src/models/branches.jl
+++ src/models/branches.jl
@@ -4,6 +4,7 @@ end
 
 include("./branches/lines.jl")
 include("./branches/transformers.jl")
+include("./branches/dc_lines.jl")
 include("../utils/ybus_calculations.jl")
 include("../utils/ptdf_calculations.jl")
 
