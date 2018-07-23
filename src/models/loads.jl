4944fe542d8831c26ce9c4886a463dad38f232f0
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-23 10:00:57 -0600

    Remove the shunt device and related to the ElectricLoad

--- src/models/loads.jl
+++ src/models/loads.jl
@@ -1,8 +1,4 @@
 abstract type ElectricLoad <: PowerSystemDevice end
 
-abstract type
-    ShuntElement <: PowerSystemDevice
-end
-
 include("loads/electric_loads.jl")
 include("loads/shunt_elements.jl")
