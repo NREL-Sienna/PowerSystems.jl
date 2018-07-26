d19fbda003a87f248abec41815dfb3c787c60d7f
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-26 14:21:55 -0600

    Fix lines syntax mistake

--- src/models/branches/lines.jl
+++ src/models/branches/lines.jl
@@ -3,20 +3,16 @@ struct Line <: Branch
     available::Bool
     connectionpoints::@NT(from::Bus, to::Bus)
     r::Float64 #[pu]
-    x::Float64 #[pu]Co
+    x::Float64 #[pu]
     b::@NT(from::Float64, to::Float64) #[pu]
     rate::@NT(from_to::Float64, to_from::Float64) #MW
     anglelimits::@NT(max::Float64, min::Float64) #Degrees
-
 end
 
-function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64), rate, anglelimits)
-    if isa(rate,Float64)
+function Line(name::String, available::Bool, connectionpoints::@NT(from::Bus, to::Bus), r::Float64, x::Float64, b::@NT(from::Float64, to::Float64), rate::Float64, anglelimits::Float64)
         rating =  @NT(from_to=rate, to_from=rate)
-        Line(name, available, connectionpoints, r, x, b, rating, anglelimits)
-    else
-        Line(name, available, connectionpoints, r, x, b, rate, anglelimits)
-    end
+        anglelimits = @NT(max=anglelimits, min=anglelimits)
+        return Line(name, available, connectionpoints, r, x, b, rating, anglelimits)
 end
 
 
