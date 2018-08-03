7029df4f1e533485145d7f5ec0b638f8f3be7662
Jose Daniel Lara <jdlara@berkeley.edu>
2018-08-03 11:51:58 -0600

    update to build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -3,11 +3,11 @@ import InfoZIP
 
 Pkg.checkout("PowerModels", "master")
 
-const GITHUB_TAG = "v0.1-alpha.2"
+const GITHUB_TAG = "data"
 
 const POWERSYSTEMS_GITHUB_URL = "https://github.com/NREL/PowerSystems.jl"
 
-const ZIP_DATA_URL = joinpath(POWERSYSTEMS_GITHUB_URL, "releases/download/" , GITHUB_TAG, "data.zip")
+const ZIP_DATA_URL = joinpath(POWERSYSTEMS_GITHUB_URL, "releases/download/" , GITHUB_TAG, "data-v0.1.0.zip")
 
 const DATA_FOLDER = Pkg.dir("PowerSystems/data")
 
