48d5474dd265c6accf74f12b214234b326bdac01
Dalvi <sdalvi@cbarrows-27611s.nrel.gov>
2018-08-03 09:18:48 -0600

    Replaced element-wise division with BLAS/LAPACK functions

--- src/utils/lodf_calculations.jl
+++ src/utils/lodf_calculations.jl
@@ -3,7 +3,8 @@ function buildlodf(branches::Array{T}, nodes::Array{Bus}, dist_slack::Array{Floa
     ptdf , a = PowerSystems.buildptdf(branches,nodes,dist_slack)
     H = gemm('N','N',ptdf,a)
     h = reshape(diag(gemm('N','N',ptdf,a)),(linecount,1))
-    LODF = gemm('N','N',ptdf,a) ./ (ones(Float64,linecount,linecount) - gemm('N','T',ones(Float64,linecount,1),h))
+    (Dem, dipiv, dinfo) = getrf!(eye(Float64,linecount,linecount) - Array(Diagonal(H)))
+    LODF = gemm('N','N',H,getri!(Dem,dipiv))
     LODF = LODF - Array(Diagonal(LODF)) - eye(linecount,linecount) 
     return LODF
 end
