ce2845296561ee5fd1bcd114420f4e3723f94ad1
Dalvi <sdalvi@cbarrows-27611s.nrel.gov>
2018-08-03 08:24:34 -0600

    optimized code

--- src/utils/lodf_calculations.jl
+++ src/utils/lodf_calculations.jl
@@ -1,11 +1,9 @@
 function buildlodf(branches::Array{T}, nodes::Array{Bus}, dist_slack::Array{Float64}=[0.1] ) where {T<:Branch}
     linecount = length(branches)
     ptdf , a = PowerSystems.buildptdf(branches,nodes,dist_slack)
-    H = zeros(Float64,linecount,linecount)
     H = gemm('N','N',ptdf,a)
-    h = diag(H)
-    h = reshape(h,(linecount,1))
-    LODF = H ./ (ones(Float64,linecount,linecount) - gemm('N','T',ones(Float64,linecount,1),h))
+    h = reshape(diag(gemm('N','N',ptdf,a)),(linecount,1))
+    LODF = gemm('N','N',ptdf,a) ./ (ones(Float64,linecount,linecount) - gemm('N','T',ones(Float64,linecount,1),h))
     LODF = LODF - Array(Diagonal(LODF)) - eye(linecount,linecount) 
     return LODF
 end
