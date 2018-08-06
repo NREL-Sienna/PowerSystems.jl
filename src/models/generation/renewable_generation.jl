1d149e464b47ea5dd802a8f23dcb5e0e22bc452c
SourabhDalvi <sourabh.p.dalvi@gmail.com>
2018-08-06 14:59:54 -0600

    Added new Scalingfactor

--- src/models/generation/renewable_generation.jl
+++ src/models/generation/renewable_generation.jl
@@ -33,7 +33,7 @@ RenewableFix(; name="init",
         status = false,
         bus = Bus(),
         installedcapacity = 0.0,
-        scalingfactor = TimeSeries.TimeArray(today(), [1.0])) = RenewableFix(name, status, bus, installedcapacity, scalingfactor)
+        scalingfactor = TimeArray(collect(DateTime(today()):Hour(1):DateTime(today()+Day(1))), ones(25))) = RenewableFix(name, status, bus, installedcapacity, scalingfactor)
 
 struct RenewableCurtailment <: RenewableGen
     name::String
@@ -54,7 +54,7 @@ RenewableCurtailment(; name = "init",
                 bus= Bus(),
                 installedcapacity = 0.0,
                 econ = EconRenewable(),
-                scalingfactor = TimeSeries.TimeArray(today(), [1.0])) = RenewableCurtailment(name, status, bus, installedcapacity, econ, scalingfactor)
+                scalingfactor = TimeArray(collect(DateTime(today()):Hour(1):DateTime(today()+Day(1))), ones(25))) = RenewableCurtailment(name, status, bus, installedcapacity, econ, scalingfactor)
 
 struct RenewableFullDispatch <: RenewableGen
     name::String
