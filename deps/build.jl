0431c0b6cb3b6acfdc0e1954b372bdf2b81bbdb4
SourabhDalvi <sourabh.p.dalvi@gmail.com>
2018-08-06 15:56:23 -0600

    Updated release to data

--- deps/build.jl
+++ deps/build.jl
@@ -3,11 +3,11 @@ import InfoZIP
 
 Pkg.checkout("PowerModels", "master")
 
-const GITHUB_TAG = "v0.1-alpha.2"
+const GITHUB_TAG = "data"
 
 const POWERSYSTEMS_GITHUB_URL = "https://github.com/NREL/PowerSystems.jl"
 
-const ZIP_DATA_URL = joinpath(POWERSYSTEMS_GITHUB_URL, "releases/download/" , GITHUB_TAG, "data.zip")
+const ZIP_DATA_URL = joinpath(POWERSYSTEMS_GITHUB_URL, "releases/download/" , GITHUB_TAG, "data-v0.1.0.zip")
 
 const DATA_FOLDER = Pkg.dir("PowerSystems/data")
 
@@ -17,7 +17,7 @@ function download_data()
         mkpath(DATA_FOLDER)
         temp_folder = tempname()
         mkpath(temp_folder)
-        temp_data_zip = joinpath(temp_folder, "data.zip")
+        temp_data_zip = joinpath(temp_folder, "data-v0.1.0.zip")
         download(ZIP_DATA_URL, temp_data_zip)
         InfoZIP.unzip(temp_data_zip, DATA_FOLDER)
     end
