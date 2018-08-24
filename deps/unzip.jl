4306a9751246b54a8b22e5dcf177fb85cbabf599
Jose Daniel Lara <jdlara@berkeley.edu>
2018-08-24 15:49:23 -0700

    Fix usage of 7z in Appveyor.yml

--- deps/unzip.jl
+++ deps/unzip.jl
@@ -1,19 +1,26 @@
 @static if Sys.iswindows()
-
+    global has_7z
+    global has_zip
+    
     function unpack_cmd(file,directory)
-        global has_7z
-        global has_zip
 
         if has_7z === nothing
             has_7z  = success(`where 7z`)
             has_zip = success(`where unzip`)
         end
 
-        try
-            rm(directory,recursive=true)
-            return (`powershell -file $(joinpath(@__FILE__,"winunzip.ps1")) $file $directory`)
-        catch
-            @error("Can't unpack $file")
+        if has_7z
+            try
+                return (`7z x $file -y -o$directory`)
+            catch
+                @error("Can't unpack $file")
+            end
+        elseif has_zip
+            try
+                return (`powershell -file $(joinpath(@__FILE__,"winunzip.ps1")) $file $directory`)
+            catch
+                @error("Can't unpack $file")
+            end
         end
     end
 
