2353cd871fba9e3970f5679b4c49025732e95c10
Jose Daniel Lara <jdlara@berkeley.edu>
2018-08-24 17:12:05 -0700

    update code to v0.7

--- src/models/branches/dc_lines.jl
+++ src/models/branches/dc_lines.jl
@@ -1,24 +1,25 @@
-abstract type DCLine <: Branch end
+# TODO: Implement more complex DC Line models including control angle and transformers
+#abstract type DCLine <: Branch end
 
-struct SimpleDCLine <: DCLine
+struct DCLine <: Branch
     name::String
     available::Bool
-    connectionpoints::@NT(from::Bus, to::Bus)
-    realpowerlimits_from::@NT(min::Float64, max::Float64) #MW
-    realpowerlimits_to::@NT(min::Float64, max::Float64) #MW
-    reactivepowerlimits_from::@NT(min::Float64, max::Float64) #MVar
-    reactivepowerlimits_to::@NT(min::Float64, max::Float64) #MVar
-    loss::@NT(l0::Float64, l1::Float64)
+    connectionpoints::From_To_Bus
+    realpowerlimits_from::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #MW
+    realpowerlimits_to::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #MW
+    reactivepowerlimits_from::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #MVar
+    reactivepowerlimits_to::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #MVar
+    loss::NamedTuple{(:l0, :l1),Tuple{Float64,Float64}}
 end
 
 DCLine(; name ="init",
         available = true,
-        connectionpoints = @NT(from = Bus(), to = Bus()),
-        realpowerlimits_from = @NT(min=0.0, max=0.0),
-        realpowerlimits_to = @NT(min=0.0, max=0.0),
-        reactivepowerlimits_from = @NT(min=0.0, max=0.0),
-        reactivepowerlimits_to = @NT(min=0.0, max=0.0),
-        loss = @NT(l0=0.0, l1=0.0)
+        connectionpoints = (from = Bus(), to = Bus()),
+        realpowerlimits_from = (min=0.0, max=0.0),
+        realpowerlimits_to = (min=0.0, max=0.0),
+        reactivepowerlimits_from = (min=0.0, max=0.0),
+        reactivepowerlimits_to = (min=0.0, max=0.0),
+        loss = (l0=0.0, l1=0.0)
     ) = DCLine(name, available, connectionpoints, realpowerlimits_from, realpowerlimits_to, reactivepowerlimits_from, reactivepowerlimits_to,loss )
 
 
@@ -28,11 +29,11 @@ As implemented in Milano's Book Page 397
 struct VSCDCLine <: DCLine
     name::String
     available::Bool
-    connectionpoints::@NT(from::Bus, to::Bus)
-    rectifier_taplimits::@NT(min::Float64, max::Float64) #pu
+    connectionpoints::From_To_Bus
+    rectifier_taplimits::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #pu
     rectifier_xrc::Float64
-    rectifier_firingangle::@NT(min::Float64, max::Float64) #radians
-    inverter_taplimits::@NT(min::Float64, max::Float64) #pu
+    rectifier_firingangle::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #radians
+    inverter_taplimits::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #pu
     inverter_xrc::Float64
-    inverter_firingangle::@NT(min::Float64, max::Float64) #radians
+    inverter_firingangle::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #radians
 end
