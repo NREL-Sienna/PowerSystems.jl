ea1336eb76bf30dc281616a1be7ca6fd7d2b45fc
Jose Daniel Lara <jdlara@berkeley.edu>
2018-08-24 17:12:05 -0700

    update code to v0.7

--- src/models/branches/dc_lines.jl
+++ src/models/branches/dc_lines.jl
@@ -21,3 +21,19 @@ DCLine(; name ="init",
         reactivepowerlimits_to = (min=0.0, max=0.0),
         loss = (l0=0.0, l1=0.0)
     ) = DCLine(name, available, connectionpoints, realpowerlimits_from, realpowerlimits_to, reactivepowerlimits_from, reactivepowerlimits_to,loss )
+
+
+"""
+As implemented in Milano's Book Page 397
+"""
+struct VSCDCLine <: DCLine
+    name::String
+    available::Bool
+    connectionpoints::From_To_Bus
+    rectifier_taplimits::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #pu
+    rectifier_xrc::Float64
+    rectifier_firingangle::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #radians
+    inverter_taplimits::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #pu
+    inverter_xrc::Float64
+    inverter_firingangle::NamedTuple{(:min, :max),Tuple{Float64,Float64}} #radians
+end
