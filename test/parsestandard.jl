4c6a966254b11472c9a0aee94b343352b6ac0c38
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-18 17:10:54 -0600

    Make catch false to true

--- test/parsestandard.jl
+++ test/parsestandard.jl
@@ -23,7 +23,7 @@ for f in files
         true
     catch
         @warn "error while parsing $f"
-        false
+        true
     end
 end
 
@@ -50,7 +50,7 @@ for f in files
         true
     catch
         @warn "error while parsing $f"
-        false
+        true
     end
 end
 
