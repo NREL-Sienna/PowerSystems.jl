0680209dd4af2e059ca55bc6d0cbce6b00a0704e
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-18 15:19:38 -0600

    Change println to info, warn and debug

--- test/parsestandard.jl
+++ test/parsestandard.jl
@@ -11,15 +11,15 @@ end
 for f in files
     @test @time  try
         ext = match(file_ext, f)
-        print("Parsing $f ...\n")
+        @info "Parsing $f ..."
         pm_dict = parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/matpower",f)))
-        println("Successfully parsed $f to PowerModels dict")
+        @info "Successfully parsed $f to PowerModels dict"
         ps_dict = PowerSystems.pm2ps_dict(pm_dict)
-        println("Successfully parsed $f to PowerSystems dict")
+        @info "Successfully parsed $f to PowerSystems dict"
         Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
-        println("Successfully parsed $f to PowerSystems devices")
-        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,float(ps_dict["baseMVA"])) 
-        println("Successfully parsed $f to PowerSystem struct")
+        @info "Successfully parsed $f to PowerSystems devices"
+        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,float(ps_dict["baseMVA"]))
+        @info "Successfully parsed $f to PowerSystem struct"
         true
     catch
         @warn("error while parsing $f")
@@ -39,16 +39,16 @@ for f in files
         ext = match(file_ext, f)
         print("Parsing $f ...\n")
         pm_dict = parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/psse_raw",f)))
-        println("Successfully parsed $f to PowerModels dict")
+        @info "Successfully parsed $f to PowerModels dict"
         ps_dict = PowerSystems.pm2ps_dict(pm_dict)
-        println("Successfully parsed $f to PowerSystems dict")
+        @info "Successfully parsed $f to PowerSystems dict"
         Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
-        println("Successfully parsed $f to PowerSystems devices")
-        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,float(ps_dict["baseMVA"])) # TODO: Add DClines, Shunts 
-        println("Successfully parsed $f to PowerSystem struct")
+        @info "Successfully parsed $f to PowerSystems devices"
+        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,float(ps_dict["baseMVA"])) # TODO: Add DClines, Shunts
+        @info "Successfully parsed $f to PowerSystem struct"
         true
     catch
-        @warn("error while parsing $f")
+        @warn "error while parsing $f"
     end
 end
 
