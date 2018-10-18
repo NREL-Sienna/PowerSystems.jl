c83c076ef85bc4140c24c6236fd8635bdfb82929
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-18 17:06:06 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,13 @@
 using PowerSystems
 using Test
+using Logging
+
+import Memento
+Memento.config!(Memento.getlogger("PowerModels"), "error")
 
 # Testing Topological components of the schema
+gl = global_logger()
+global_logger(ConsoleLogger(gl.stream, Logging.Error))
 
 
 @testset "Read PowerSystems data" begin
