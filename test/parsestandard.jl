c8a740214f652b050f73795ffbb013dfa39f52d9
Barrows <clayton.barrows@nrel.gov>
2018-10-22 10:56:39 -0600

    fixing error assigning storage struct in parserstandard test

--- test/parsestandard.jl
+++ test/parsestandard.jl
@@ -16,9 +16,9 @@ for f in files
         @info "Successfully parsed $f to PowerModels dict"
         ps_dict = PowerSystems.pm2ps_dict(pm_dict)
         @info "Successfully parsed $f to PowerSystems dict"
-        Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
+        Buses, Generators, Storages, Branches, Loads, LoadZones ,Shunts = PowerSystems.ps_dict2ps_struct(ps_dict)
         @info "Successfully parsed $f to PowerSystems devices"
-        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storage,float(ps_dict["baseMVA"]))
+        sys_test = PowerSystem(Buses, Generators,Loads,Branches,Storages,float(ps_dict["baseMVA"]))
         @info "Successfully parsed $f to PowerSystem struct"
         true
     catch
