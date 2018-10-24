636dea91f48974f04b7e9ef2da796cbf73a7064a
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-24 10:36:53 -0600

    Update readforecastdata.jl

--- test/readforecastdata.jl
+++ test/readforecastdata.jl
@@ -1,31 +1,37 @@
 using Dates
 @test try
-    @test ps_dict = PowerSystems.parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/matpower/case5_re.m")));
-    @test da_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/5bus_ts")); REGEX_FILE = r"da_(.*?)\.csv");
-    @test rt_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/5bus_ts")); REGEX_FILE = r"rt_(.*?)\.csv");
-    @test ps_dict = PowerSystems.assign_ts_data(ps_dict,rt_time_series);
+        ps_dict = PowerSystems.parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/matpower/case5_re.m")));
+        da_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/5bus_ts")); REGEX_FILE = r"da_(.*?)\.csv");
+        rt_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/5bus_ts")); REGEX_FILE = r"rt_(.*?)\.csv");
+        ps_dict = PowerSystems.assign_ts_data(ps_dict,rt_time_series);
 
-    @test Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts  = PowerSystems.ps_dict2ps_struct(ps_dict);
-    @test sys_5 = PowerSystems.PowerSystem(Buses, Generators,Loads,Branches,Storage,ps_dict["baseMVA"]);
+        buses, generators, storage, branches, loads, loadZones, shunts  = PowerSystems.ps_dict2ps_struct(ps_dict);
+        sys_5 = PowerSystems.PowerSystem(buses, generators, loads, branches, storage, ps_dict["baseMVA"]);
 
-    @test forecast_gen = PowerSystems.make_forecast_dict("DA",da_time_series["gen"],Day(1),24,Generators);
-    @test forecast_load = PowerSystems.make_forecast_dict("DA",da_time_series,Day(1),24,Loads);
+        forecast_gen = PowerSystems.make_forecast_dict("DA", da_time_series["gen"], Day(1), 24, generators);
+        forecast_load = PowerSystems.make_forecast_dict("DA", da_time_series, Day(1), 24, loads);
 
-    @test Forecast_Struct = PowerSystems.make_forecast_array(forecast_gen);
-true finally end
+        forecast_struct = PowerSystems.make_forecast_array(forecast_gen);
+        true
+    finally
+        false
+    end
 
 @test try
-    @test ps_dict = PowerSystems.parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/matpower/RTS_GMLC.m")));
-    @test da_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/RTS_GMLC_forecasts")); REGEX_FILE = r"DAY_AHEAD(.*?)\.csv");
-    @test rt_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/RTS_GMLC_forecasts")); REGEX_FILE = r"REAL_TIME(.*?)\.csv");
-    @test ps_dict = PowerSystems.assign_ts_data(ps_dict,rt_time_series);
+        ps_dict = PowerSystems.parsestandardfiles(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/matpower/RTS_GMLC.m")));
+        da_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/RTS_GMLC_forecasts")); REGEX_FILE = r"DAY_AHEAD(.*?)\.csv");
+        rt_time_series = PowerSystems.read_data_files(abspath(joinpath(dirname(Base.find_package("PowerSystems")), "../data/forecasts/RTS_GMLC_forecasts")); REGEX_FILE = r"REAL_TIME(.*?)\.csv");
+        ps_dict = PowerSystems.assign_ts_data(ps_dict,rt_time_series);
 
-    @test Buses, Generators, Storage, Branches, Loads, LoadZones ,Shunts  = PowerSystems.ps_dict2ps_struct(ps_dict);
-    @test sys_RTS = PowerSystems.PowerSystem(Buses, Generators,Loads,Branches,Storage,ps_dict["baseMVA"]);
+        buses, generators, storage, branches, loads, loadZones, shunts  = PowerSystems.ps_dict2ps_struct(ps_dict);
+        sys_5 = PowerSystems.PowerSystem(buses, generators, loads, branches, storage, ps_dict["baseMVA"]);
 
-    @test forecast_gen = PowerSystems.make_forecast_dict("DA",da_time_series["gen"],Day(1),24,Generators);
-    @test forecast_load = PowerSystems.make_forecast_dict("RT",rt_time_series,Day(1),288,Loads,LoadZones);
+        forecast_gen = PowerSystems.make_forecast_dict("DA", da_time_series["gen"], Day(1), 24, generators);
+        forecast_load = PowerSystems.make_forecast_dict("RT", rt_time_series, Day(1), 288, loads, loadZones);
 
-    @test Forecast_Struct = PowerSystems.make_forecast_array(forecast_load);
-true finally end
+        forecast_struct = PowerSystems.make_forecast_array(forecast_load);
+        true
+    finally
+        false
+    end
 true
