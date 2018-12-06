21dbf679aed2c9c4f6a4d7ca18ded9227ed630bd
Barrows <clayton.barrows@nrel.gov>
2018-12-06 07:58:46 -0700

    adding negative test

--- test/cdmparse.jl
+++ test/cdmparse.jl
@@ -2,25 +2,63 @@ using Logging
 using PowerSystems
 
 
+
+basedir = abspath(joinpath(dirname(dirname(Base.find_package("PowerSystems"))), "data/RTS_GMLC"))
+cdm_dict = nothing
+sys_rts_da = nothing
+sys_rts_rt = nothing
+
 @test try
-
-    basedir = abspath(joinpath(dirname(dirname(Base.find_package("PowerSystems"))), "data/RTS_GMLC"))
-
     @info "parsing data from $basedir"
-    cdm_dict = PowerSystems.csv2ps_dict(basedir)
-
-    @info "assigning time series data for DA"
-    cdm_dict = PowerSystems.assign_ts_data(cdm_dict,cdm_dict["forecast"]["DA"])
-    
-    @info "making DA System"
-    sys_rts_da = PowerSystem(cdm_dict)
-
-    @info "assigning time series data for RT"
-    cdm_dict = PowerSystems.assign_ts_data(cdm_dict,cdm_dict["forecast"]["RT"])
-    
-    @info "making RT System"
-    sys_rts_rt = PowerSystem(cdm_dict)
-
+    global cdm_dict = PowerSystems.csv2ps_dict(basedir)
     true
 finally
 end
+
+@test (cdm_dict isa Dict) & haskey(cdm_dict,"load_zone") == true
+
+@test try
+    @info "assigning time series data for DA"
+    global cdm_dict = PowerSystems.assign_ts_data(cdm_dict,cdm_dict["forecast"]["DA"])
+    true
+finally
+end
+
+@test length(cdm_dict["gen"]["Renewable"]["PV"]["102_PV_1"]["scalingfactor"])==24
+
+@test try
+    @info "making DA System"
+    global sys_rts_da = PowerSystem(cdm_dict)
+    true
+finally
+end
+
+@test sys_rts_da isa PowerSystem
+
+@test try
+    @info "assigning time series data for RT"
+    global cdm_dict = PowerSystems.assign_ts_data(cdm_dict,cdm_dict["forecast"]["RT"])
+    true
+finally
+end 
+
+@test length(cdm_dict["gen"]["Renewable"]["PV"]["102_PV_1"]["scalingfactor"])==288
+
+@test try
+    @info "making RT System"
+    global sys_rts_rt = PowerSystem(cdm_dict)
+    true
+finally
+end
+
+@test (sys_rts_rt isa PowerSystem) == true
+
+@test try
+    basedir = "."
+    @info "testing bad directory"
+    global cdm_dict = PowerSystems.csv2ps_dict(basedir);
+    true
+finally
+end
+
+@test cdm_dict == Dict{String,Any}("baseMVA"=>100.0,"gen"=>nothing,"branch"=>nothing,"load"=>nothing)
