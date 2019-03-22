9561e4ecd8bfcc3a386b8a0ac5c85d416ae32964
Barrows <clayton.barrows@nrel.gov>
2019-03-22 16:12:42 -0600

    starting the time-series refactor, but updating the Deterministic struct and adding constructors

--- src/models/forecasts.jl
+++ src/models/forecasts.jl
@@ -2,13 +2,44 @@ abstract type
     Forecast
 end
 
+"""
+    Deterministic
+        A deterministic forecast for a particular data field in a PowerSystemDevice.
+
+"""
 struct Deterministic <: Forecast
-    device::PowerSystemDevice
-    horizon::Int
-    resolution::Dates.Period
-    interval::Dates.Period
-    initialtime::Dates.DateTime
-    data::Dict{Any,Dict{Int,TimeSeries.TimeArray}}
+    device::PowerSystemDevice       # device
+    field::Symbol                   # field of device
+    resolution::Dates.Period        # resolution
+    initialtime::Dates.DateTime     # forecast availability time
+    data::TimeSeries.TimeArray      # TimeStamp - scalingfactor
+
+    function Deterministic(device::PowerSystemDevice,
+                            field::Symbol,
+                            resolution::Dates.Period,
+                            initialtime::Dates.DateTime,
+                            data::TimeSeries.TimeArray; kwargs...)
+        runchecks = in(:runchecks, keys(kwargs)) ? kwargs[:runchecks] : true
+        if runchecks
+            if !isafield(device,field)
+                @error "Device $device has no field: $field"
+            end
+        end
+        new(device, field, resolution, initialtime, data)
+    end
+end
+
+
+function Deterministic(device::PowerSystemDevice, field::Symbol, resolution::Dates.Period, initialtime::Dates.DateTime, time_steps::Int; kwargs...)
+    data = TimeSeries.TimeArray(initialtime:Dates.Hour(1):initialtime+resolution*(time_steps-1), ones(time_steps))
+    Deterministic(device, field, resolution, initialtime, data; kwargs...)
+end
+
+function Deterministic(device::PowerSystemDevice, field::Symbol, data::TimeSeries.TimeArray; kwargs...)
+    resolution = getresolution(data)
+    initialtime = TimeSeries.timestamp(data)[1]
+    time_steps = length(data)
+    Deterministic(device, field, resolution, initialtime, data; kwargs...)
 end
 
 struct Scenarios <: Forecast
