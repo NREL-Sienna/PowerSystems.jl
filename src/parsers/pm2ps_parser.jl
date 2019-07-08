f075315a19e72c2d56ed076a916d7d4791255d84
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-08 15:37:47 -0600

    remove cap factor from ps_parser

--- src/parsers/pm2ps_parser.jl
+++ src/parsers/pm2ps_parser.jl
@@ -275,7 +275,6 @@ function make_thermal_gen(gen_name::AbstractString, d::Dict, bus::Bus)
         fixedcost=fixedcost,
         startupcost=startupcost,
         shutdncost=shutdncost,
-        annualcapacityfactor=nothing,
     )
 
     thermal_gen = ThermalStandard(
