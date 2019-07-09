d2a939b8b95afa1732ad506fd054589a0caaabf9
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-09 15:34:40 -0600

    change to correct branch in test data

--- deps/build.jl
+++ deps/build.jl
@@ -2,4 +2,4 @@
 include(joinpath(@__DIR__, "../src/utils/data.jl"))
 import .UtilsData: TestData
 
-download(TestData; branch = "cost_function_updates")
+download(TestData; branch = "improve-timeseries")
