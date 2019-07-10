d01e8ac010e0fff8b20605c9e8bad264e0cfcb89
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-10 13:07:29 -0600

    fix typo in TwoPartCost

--- src/models/generated/TwoPartCost.jl
+++ src/models/generated/TwoPartCost.jl
@@ -5,16 +5,16 @@ This file is auto-generated. Do not edit.
 """Data Structure Operational Cost Data in two parts fixed and variable cost."""
 mutable struct TwoPartCost <: OperationalCost
     variable::VariableCost
-    fixed_cost::Float64
+    fixed::Float64
     internal::PowerSystems.PowerSystemInternal
 end
 
-function TwoPartCost(variable, fixed_cost, )
-    TwoPartCost(variable, fixed_cost, PowerSystemInternal())
+function TwoPartCost(variable, fixed, )
+    TwoPartCost(variable, fixed, PowerSystemInternal())
 end
 
-function TwoPartCost(; variable, fixed_cost, )
-    TwoPartCost(variable, fixed_cost, )
+function TwoPartCost(; variable, fixed, )
+    TwoPartCost(variable, fixed, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -22,13 +22,13 @@ end
 function TwoPartCost(::Nothing)
     TwoPartCost(;
         variable=VariableCost((0.0, 0.0)),
-        fixed_cost=0.0,
+        fixed=0.0,
     )
 end
 
 """Get TwoPartCost variable."""
 get_variable(value::TwoPartCost) = value.variable
-"""Get TwoPartCost fixed_cost."""
-get_fixed_cost(value::TwoPartCost) = value.fixed_cost
+"""Get TwoPartCost fixed."""
+get_fixed(value::TwoPartCost) = value.fixed
 """Get TwoPartCost internal."""
 get_internal(value::TwoPartCost) = value.internal
