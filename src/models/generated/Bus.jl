ab7663c6de7801c5b022356f70b3ac9390f7fe09
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-19 15:39:33 -0600

    update structs

--- src/models/generated/Bus.jl
+++ src/models/generated/Bus.jl
@@ -9,7 +9,7 @@ mutable struct Bus <: Topology
     bustype::Union{Nothing, BusType}  # bus type
     angle::Union{Nothing, Float64}  # angle of the bus in radians
     voltage::Union{Nothing, Float64}  # voltage as a multiple of basevoltage
-    voltagelimits::Union{Nothing, NamedTuple{(:min, :max), Tuple{Float64, Float64}}}  # limits on the voltage variation as multiples of basevoltage
+    voltagelimits::Union{Nothing, Min_Max}  # limits on the voltage variation as multiples of basevoltage
     basevoltage::Union{Nothing, Float64}  # the base voltage in kV
     internal::PowerSystems.PowerSystemInternal
 
--- src/models/generated/MonitoredLine.jl
+++ src/models/generated/MonitoredLine.jl
@@ -6,22 +6,24 @@ This file is auto-generated. Do not edit.
 mutable struct MonitoredLine <: ACBranch
     name::String
     available::Bool
+    activepower_flow::Float64
+    reactivepower_flow::Float64
     arch::Arch
     r::Float64
     x::Float64
     b::NamedTuple{(:from, :to), Tuple{Float64, Float64}}
     flowlimits::NamedTuple{(:from_to, :to_from), Tuple{Float64, Float64}}
     rate::Float64
-    anglelimits::NamedTuple{(:min, :max), Tuple{Float64, Float64}}
+    anglelimits::Min_Max
     internal::PowerSystems.PowerSystemInternal
 end
 
-function MonitoredLine(name, available, arch, r, x, b, flowlimits, rate, anglelimits, )
-    MonitoredLine(name, available, arch, r, x, b, flowlimits, rate, anglelimits, PowerSystemInternal())
+function MonitoredLine(name, available, activepower_flow, reactivepower_flow, arch, r, x, b, flowlimits, rate, anglelimits, )
+    MonitoredLine(name, available, activepower_flow, reactivepower_flow, arch, r, x, b, flowlimits, rate, anglelimits, PowerSystemInternal())
 end
 
-function MonitoredLine(; name, available, arch, r, x, b, flowlimits, rate, anglelimits, )
-    MonitoredLine(name, available, arch, r, x, b, flowlimits, rate, anglelimits, )
+function MonitoredLine(; name, available, activepower_flow, reactivepower_flow, arch, r, x, b, flowlimits, rate, anglelimits, )
+    MonitoredLine(name, available, activepower_flow, reactivepower_flow, arch, r, x, b, flowlimits, rate, anglelimits, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -30,6 +32,8 @@ function MonitoredLine(::Nothing)
     MonitoredLine(;
         name="init",
         available=false,
+        activepower_flow=0.0,
+        reactivepower_flow=0.0,
         arch=Arch(Bus(nothing), Bus(nothing)),
         r=0.0,
         x=0.0,
@@ -44,6 +48,10 @@ end
 get_name(value::MonitoredLine) = value.name
 """Get MonitoredLine available."""
 get_available(value::MonitoredLine) = value.available
+"""Get MonitoredLine activepower_flow."""
+get_activepower_flow(value::MonitoredLine) = value.activepower_flow
+"""Get MonitoredLine reactivepower_flow."""
+get_reactivepower_flow(value::MonitoredLine) = value.reactivepower_flow
 """Get MonitoredLine arch."""
 get_arch(value::MonitoredLine) = value.arch
 """Get MonitoredLine r."""
