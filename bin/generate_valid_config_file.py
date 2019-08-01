b4f778d3c4603aa0e0d30c42cf0bf9ced40216a9
KyraDiF <kyradifrancesco@hotmail.com>
2019-08-01 15:35:46 -0600

    move setup_yaml

--- bin/generate_valid_config_file.py
+++ bin/generate_valid_config_file.py
@@ -21,12 +21,6 @@ POWER_SYSTEM_DESCRIPTOR_FILE = os.path.join(
     "power_system_structs.json"
 )
 
-def setup_yaml():
-  """ https://stackoverflow.com/a/8661021 """
-  represent_dict_order = lambda self, data:  self.represent_mapping('tag:yaml.org,2002:map', data.items())
-  yaml.add_representer(OrderedDict, represent_dict_order)
-setup_yaml()
-
 def read_json_data(filename):
     """Return the JSON data from a file."""
     with open(filename) as fp_in:
@@ -37,14 +31,13 @@ def generate_config(input_file):
     """Generate validation descriptors from the PowerSystems struct data file."""
     config = {}
     data = read_json_data(input_file)
-    items=[]
-    #structName = str()
-    for struct in data:
-        # new_struct = {"struct_name":struct["struct_name"], "fields":[]}
+    items = []
+
+    for ps_struct in data:
         new_struct = OrderedDict()
-        new_struct["struct_name"] = struct["struct_name"]
+        new_struct["struct_name"] = ps_struct["struct_name"]
         new_struct["fields"] = []
-        for field in struct["fields"]:
+        for field in ps_struct["fields"]:
             new_field = OrderedDict()
             new_field["name"] = field["name"]
             if "data_type" in field:
@@ -74,6 +67,11 @@ def main():
 
     generate_file(sys.argv[1])
 
+def setup_yaml():
+  """ https://stackoverflow.com/a/8661021 """
+  represent_dict_order = lambda self, data:  self.represent_mapping('tag:yaml.org,2002:map', data.items())
+  yaml.add_representer(OrderedDict, represent_dict_order)
 
 if __name__ == "__main__":
+    setup_yaml()
     main()
