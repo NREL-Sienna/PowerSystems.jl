919e99ad19ef953397d00614c7818fb039b5000a
KyraDiF <kyradifrancesco@hotmail.com>
2019-08-02 10:15:31 -0600

    Revert "add docstring"
    This reverts commit c19ed3ab3ada44bb9924dc63c44428f8c7c2911f.

--- src/base.jl
+++ src/base.jl
@@ -125,6 +125,8 @@ function System(buses::Vector{Bus},
         @debug "components: $(string(key)): count=$(string(length(value)))"
     end
 
+    configpath = get(kwargs, :configpath, VALID_CONFIG_FILE)
+
     runchecks = get(kwargs, :runchecks, true)
 
     if error_detected
