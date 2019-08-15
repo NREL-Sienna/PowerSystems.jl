81c786be2def478a31b4319563d0b41ada6e961a
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-15 15:30:47 -0700

    run build

--- src/models/generated/ThermalStandard.jl
+++ src/models/generated/ThermalStandard.jl
@@ -7,19 +7,19 @@ mutable struct ThermalStandard <: ThermalGen
     name::String
     available::Bool
     bus::Bus
-    reactivepower::Float64
     activepower::Float64
+    reactivepower::Float64
     tech::Union{Nothing, TechThermal}
     op_cost::ThreePartCost
     internal::PowerSystemInternal
 end
 
-function ThermalStandard(name, available, bus, reactivepower, activepower, tech, op_cost, )
-    ThermalStandard(name, available, bus, reactivepower, activepower, tech, op_cost, PowerSystemInternal())
+function ThermalStandard(name, available, bus, activepower, reactivepower, tech, op_cost, )
+    ThermalStandard(name, available, bus, activepower, reactivepower, tech, op_cost, PowerSystemInternal())
 end
 
-function ThermalStandard(; name, available, bus, reactivepower, activepower, tech, op_cost, )
-    ThermalStandard(name, available, bus, reactivepower, activepower, tech, op_cost, )
+function ThermalStandard(; name, available, bus, activepower, reactivepower, tech, op_cost, )
+    ThermalStandard(name, available, bus, activepower, reactivepower, tech, op_cost, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -29,8 +29,8 @@ function ThermalStandard(::Nothing)
         name="init",
         available=false,
         bus=Bus(nothing),
-        reactivepower=0.0,
         activepower=0.0,
+        reactivepower=0.0,
         tech=TechThermal(nothing),
         op_cost=ThreePartCost(nothing),
     )
@@ -42,10 +42,10 @@ get_name(value::ThermalStandard) = value.name
 get_available(value::ThermalStandard) = value.available
 """Get ThermalStandard bus."""
 get_bus(value::ThermalStandard) = value.bus
-"""Get ThermalStandard reactivepower."""
-get_reactivepower(value::ThermalStandard) = value.reactivepower
 """Get ThermalStandard activepower."""
 get_activepower(value::ThermalStandard) = value.activepower
+"""Get ThermalStandard reactivepower."""
+get_reactivepower(value::ThermalStandard) = value.reactivepower
 """Get ThermalStandard tech."""
 get_tech(value::ThermalStandard) = value.tech
 """Get ThermalStandard op_cost."""
