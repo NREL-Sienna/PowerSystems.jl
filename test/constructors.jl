8608d7556005dfad39eb2648b3fc9f30516ced22
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-15 12:12:54 -0700

    add tests

--- test/constructors.jl
+++ test/constructors.jl
@@ -83,13 +83,19 @@ end
     tg = RenewableFix(nothing)
     forecast_data = PowerSystems.TimeSeries.TimeArray([DateTime("01-01-01"), DateTime("01-01-01")+Hour(1)], [1.0, 1.0])
     #Deterministic Tests
-    tDeterministicForecast = Deterministic(tg,"scalingfactor",Hour(1),DateTime("01-01-01"),24)
+    tDeterministicForecast = Deterministic(tg,"scalingfactor", Hour(1),DateTime("01-01-01"),24)
     @test tDeterministicForecast isa PowerSystems.Forecast
-    tDeterministicForecast = Deterministic(tg,"scalingfactor",forecast_data)
+    tDeterministicForecast = Deterministic(tg,"scalingfactor", forecast_data)
     @test tDeterministicForecast isa PowerSystems.Forecast
     #Probabilistic Tests
-    tProbabilisticForecast = Probabilistic(tg,"scalingfactor",Hour(1), DateTime("01-01-01"),[0.5, 0.5], 24)
+    tProbabilisticForecast = Probabilistic(tg,"scalingfactor", Hour(1), DateTime("01-01-01"),[0.5, 0.5], 24)
     @test  tProbabilisticForecast isa PowerSystems.Forecast
-    tProbabilisticForecast = Probabilistic(tg,"scalingfactor",[1.0], forecast_data)
+    tProbabilisticForecast = Probabilistic(tg,"scalingfactor", [1.0], forecast_data)
     @test  tProbabilisticForecast isa PowerSystems.Forecast
+    #Scenario Tests
+    tScenarioForecast = ScenarioBased(tg, "scalingfactor", Hour(1), DateTime("01-01-01"), 2, 24)
+    @test  tScenarioForecast isa PowerSystems.Forecast
+    tScenarioForecast = ScenarioBased(tg,"scalingfactor",forecast_data)
+    @test  tScenarioForecast isa PowerSystems.Forecast
 end
+
