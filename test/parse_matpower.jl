1727829e41f75d003860d2e15f189626ec3fea5a
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-27 13:17:06 -0700

    add parse tests in pm_folder

--- test/parse_matpower.jl
+++ test/parse_matpower.jl
@@ -37,3 +37,27 @@ POWER_MODELS_KEYS = [
         @info "Successfully converted $path to mixed_units"
     end
 end
+
+@testset "Parse PowerModel Matpower data files" begin
+    files = [x for x in readdir(joinpath(DATA_DIR, "pm_data","matpower")) if splitext(x)[2] == ".m"]
+    if length(files) == 0
+        @error "No test files in the folder"
+    end
+
+    for f in files
+        @info "Parsing $f..."
+        path = joinpath(joinpath(DATA_DIR, "pm_data","matpower"), f)
+
+        pm_dict = PowerSystems.parse_file(path)
+        for key in POWER_MODELS_KEYS
+            @test haskey(pm_dict, key)
+        end
+        @info "Successfully parsed $path to PowerModels dict"
+
+        sys = PowerSystems.pm2ps_dict(pm_dict)
+        @info "Successfully parsed $path to System struct"
+
+        PowerSystems.make_mixed_units(pm_dict)
+        @info "Successfully converted $path to mixed_units"
+    end
+end
