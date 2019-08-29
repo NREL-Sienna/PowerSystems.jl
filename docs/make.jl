7b9489c4cf40e81a3b78de9f2c75f0c15c079883
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-28 23:15:50 -0700

    add validation file generation code to make.

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,9 @@
 using Documenter, PowerSystems
 
+script_name = joinpath(@__DIR__, "../bin", "generate_valid_config_file.py")		
+config_name = joinpath(@__DIR__, "../src", "descriptors", "validation_config.json")		
+descriptor_name = joinpath(@__DIR__, "../src", "descriptors", "power_system_structs.json")		
+read(`python3 $script_name $config_name $descriptor_name`)		
 include("src/generate_validation_table.jl")
 
 makedocs(
