1f3a169b2af2c2db43fb70228522a1ba8c2f1ad1
Daniel Thom <daniel.thom@nrel.gov>
2019-09-12 14:53:00 -0600

    Suppressed unit test output in generate_structs.

--- test/test_generate_structs.jl
+++ test/test_generate_structs.jl
@@ -2,10 +2,13 @@
     descriptor_file = joinpath(@__DIR__, "..", "src", "descriptors",
                                "power_system_structs.json")
     existing_dir = joinpath(@__DIR__, "..", "src", "models", "generated")
-    output_dir = "tmp"
+    output_dir = "tmp-test-generated-structs"
+    if isdir(output_dir)
+        rm(output_dir; recursive=true)
+    end
     mkdir(output_dir)
 
-    IS.generate_structs(descriptor_file, output_dir)
+    IS.generate_structs(descriptor_file, output_dir; print_results=false)
 
     matched = true
     try
