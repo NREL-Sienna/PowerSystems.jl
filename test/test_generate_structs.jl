86a7fc054824c9eecb9e72fcdc98553c3119f2cd
Daniel Thom <daniel.thom@nrel.gov>
2019-09-12 13:09:29 -0600

    Run struct generation check in a unit test.

---
+++ test/test_generate_structs.jl
@@ -0,0 +1,22 @@
+@testset "Test generated structs" begin
+    descriptor_file = joinpath(@__DIR__, "..", "src", "descriptors",
+                               "power_system_structs.json")
+    existing_dir = joinpath(@__DIR__, "..", "src", "generated")
+    output_dir = "tmp"
+    mkdir(output_dir)
+
+    IS.generate_structs(descriptor_file, output_dir)
+
+    matched = true
+    try
+        run(`diff $output_dir $existing_dir`)
+    catch(err)
+        @error "Generated structs do not match the descriptor file."
+        matched = false
+    finally
+        rm(output_dir; recursive=true)
+    end
+
+    @test matched
+end
+
