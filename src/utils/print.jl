bca4b8464c74eb6bafc0ce3d8c02621acd731fb7
Daniel Thom <daniel.thom@nrel.gov>
2019-09-14 09:45:18 -0600

    Simplify printing for PowerSystemTableData

--- src/utils/print.jl
+++ src/utils/print.jl
@@ -29,3 +29,21 @@ end
 function Base.summary(arc::Arc)
     return "$(get_name(get_from(arc))) -> $(get_name(get_to(arc))): ($(typeof(arc)))"
 end
+
+function Base.show(io::IO, ::MIME"text/plain", data::PowerSystemTableData)
+    println(io, "$(typeof(data)):")
+    println(io, "  directory:  $(data.directory)")
+    if !isnothing(data.timeseries_metadata_file)
+        println(io, "  timeseries_metadata_file:  $(data.timeseries_metadata_file)")
+    end
+    println(io, "  basepower:  $(data.basepower)")
+    for field in ("branch", "bus", "dcline", "gen", "load", "services")
+        print(io, "  $field:  ")
+        val = getfield(data, Symbol(field))
+        if isnothing(val)
+            println(io, "no data")
+        else
+            println(io, "$(summary(val))")
+        end
+    end
+end
