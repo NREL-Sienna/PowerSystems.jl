3a824f64e1892c6079ec519d923fd85a6a857fd4
Daniel Thom <daniel.thom@nrel.gov>
2019-10-31 18:49:28 -0600

    Fix generate_initial_times docstring

--- src/base.jl
+++ src/base.jl
@@ -568,9 +568,10 @@ end
 """
     generate_initial_times(sys::System, interval::Dates.Period, horizon::Int)
 
-Generates all possible initial times for the stored forecasts. This should be used when
-contiguous forecasts have been stored in chunks, such as a one-year forecast broken up into
-365 one-day forecasts.
+Generates all possible initial times for the stored forecasts. This should return the same
+result regardless of whether the forecasts have been stored as one contiguous array or
+chunks of contiguous arrays, such as one 365-day forecast vs 365 one-day forecasts.
+
 
 Throws ArgumentError if there are no forecasts stored, interval is not a multiple of the
 system's forecast resolution, or if the stored forecasts have overlapping timestamps.
