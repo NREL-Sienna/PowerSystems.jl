f56e15055313a3b0b4a41f6d73d2427b824c66f0
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-01-06 12:56:12 -0700

    Adding ne HydroDispatch file

--- src/models/generated/HydroDispatch.jl
+++ src/models/generated/HydroDispatch.jl
@@ -10,6 +10,8 @@ This file is auto-generated. Do not edit.
         reactivepower::Float64
         tech::TechHydro
         op_cost::TwoPartCost
+        storagecapacity::Float64
+        initial_storage::Float64
         services::Vector{Service}
         ext::Dict{String, Any}
         _forecasts::InfrastructureSystems.Forecasts
@@ -26,6 +28,8 @@ This file is auto-generated. Do not edit.
 - `reactivepower::Float64`
 - `tech::TechHydro`
 - `op_cost::TwoPartCost`
+- `storagecapacity::Float64`
+- `initial_storage::Float64`
 - `services::Vector{Service}`: Services that this device contributes to
 - `ext::Dict{String, Any}`
 - `_forecasts::InfrastructureSystems.Forecasts`
@@ -39,6 +43,8 @@ mutable struct HydroDispatch <: HydroGen
     reactivepower::Float64
     tech::TechHydro
     op_cost::TwoPartCost
+    storagecapacity::Float64
+    initial_storage::Float64
     "Services that this device contributes to"
     services::Vector{Service}
     ext::Dict{String, Any}
@@ -47,12 +53,12 @@ mutable struct HydroDispatch <: HydroGen
     internal::InfrastructureSystemsInternal
 end
 
-function HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, services=Device[], ext=Dict{String, Any}(), _forecasts=InfrastructureSystems.Forecasts(), )
-    HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, services, ext, _forecasts, InfrastructureSystemsInternal(), )
+function HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, storagecapacity, initial_storage, services=Device[], ext=Dict{String, Any}(), _forecasts=InfrastructureSystems.Forecasts(), )
+    HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, storagecapacity, initial_storage, services, ext, _forecasts, InfrastructureSystemsInternal(), )
 end
 
-function HydroDispatch(; name, available, bus, activepower, reactivepower, tech, op_cost, services=Device[], ext=Dict{String, Any}(), _forecasts=InfrastructureSystems.Forecasts(), )
-    HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, services, ext, _forecasts, )
+function HydroDispatch(; name, available, bus, activepower, reactivepower, tech, op_cost, storagecapacity, initial_storage, services=Device[], ext=Dict{String, Any}(), _forecasts=InfrastructureSystems.Forecasts(), )
+    HydroDispatch(name, available, bus, activepower, reactivepower, tech, op_cost, storagecapacity, initial_storage, services, ext, _forecasts, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -65,6 +71,8 @@ function HydroDispatch(::Nothing)
         reactivepower=0.0,
         tech=TechHydro(nothing),
         op_cost=TwoPartCost(nothing),
+        storagecapacity=0.0,
+        initial_storage=0.0,
         services=Device[],
         ext=Dict{String, Any}(),
         _forecasts=InfrastructureSystems.Forecasts(),
@@ -85,6 +93,10 @@ get_reactivepower(value::HydroDispatch) = value.reactivepower
 get_tech(value::HydroDispatch) = value.tech
 """Get HydroDispatch op_cost."""
 get_op_cost(value::HydroDispatch) = value.op_cost
+"""Get HydroDispatch storagecapacity."""
+get_storagecapacity(value::HydroDispatch) = value.storagecapacity
+"""Get HydroDispatch initial_storage."""
+get_initial_storage(value::HydroDispatch) = value.initial_storage
 """Get HydroDispatch services."""
 get_services(value::HydroDispatch) = value.services
 """Get HydroDispatch ext."""
