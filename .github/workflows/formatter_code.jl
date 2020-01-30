d4a9b3c9a865bbe08d5d7227ef88fc02a3c4b4aa
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 13:55:56 -0800

    update formatter code

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -2,12 +2,10 @@ main_paths = ["./src", "./test"]
 for main_path in main_paths
     for (root, dir, files) in walkdir(main_path)
         for f in files
-            @show file_path = abspath(root, f)
-            occursin("generated", file_path) && continue
-            occursin("descriptors", file_path) && continue
-            if isfile(file_path)
-                !occursin(".jl", file_path) && continue
-            end
+        @show file_path = abspath(root, f)
+        occursin("generated", file_path) && continue
+        occursin("descriptors", file_path) && continue
+        !occursin(".jl", file_path) && continue
         format(file_path;
             whitespace_ops_in_indices = true,
             remove_extra_newlines = true,
