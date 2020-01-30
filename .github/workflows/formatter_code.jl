4ada25aaa8d7b8c9501461191c18443e37fd40d7
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 13:58:31 -0800

    change skip condition

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -3,9 +3,8 @@ for main_path in main_paths
     for (root, dir, files) in walkdir(main_path)
         for f in files
         @show file_path = abspath(root, f)
+        !occursin(".jl", f) && continue
         occursin("generated", file_path) && continue
-        occursin("descriptors", file_path) && continue
-        !occursin(".jl", file_path) && continue
         format(file_path;
             whitespace_ops_in_indices = true,
             remove_extra_newlines = true,
