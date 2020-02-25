9068807019e3f86eecd5bbdeb1623cfb6406effc
Daniel Thom <daniel.thom@nrel.gov>
2020-02-25 13:22:03 -0700

    Enable error log check for unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -100,9 +100,7 @@ function run_tests()
             @includetests ARGS
         end
 
-        # TODO: once all known error logs are fixed, add this test:
-        #@test length(get_log_events(multi_logger.tracker, Logging.Error)) == 0
-
+        @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
         @info IS.report_log_summary(multi_logger)
     end
 end
