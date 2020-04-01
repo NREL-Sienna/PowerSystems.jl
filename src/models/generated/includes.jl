836273150623a9489f907ef0ccfcd416e8663e66
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-01 15:51:21 -0700

    add AGC structs

--- src/models/generated/includes.jl
+++ src/models/generated/includes.jl
@@ -25,6 +25,7 @@ include("ThermalStandard.jl")
 include("GenericBattery.jl")
 include("StaticReserve.jl")
 include("VariableReserve.jl")
+include("AGC.jl")
 include("Transfer.jl")
 include("AVRFixed.jl")
 include("AVRSimple.jl")
@@ -73,8 +74,10 @@ export get_H_hp
 export get_H_ip
 export get_H_lp
 export get_K0
+export get_K_d
 export get_K_ex
 export get_K_hp
+export get_K_i
 export get_K_ip
 export get_K_lp
 export get_K_p
@@ -139,10 +142,12 @@ export get_area
 export get_available
 export get_b
 export get_basevoltage
+export get_bias
 export get_bus
 export get_bustype
 export get_capacity
 export get_cf
+export get_delta_t
 export get_efficiency
 export get_energy
 export get_eq_p
