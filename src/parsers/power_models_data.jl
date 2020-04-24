549c4604991764a57f8609441907ab587518a03e
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-24 10:08:52 -0700

    enable runchecks kwarg in PowerModels parser

--- src/parsers/power_models_data.jl
+++ src/parsers/power_models_data.jl
@@ -30,6 +30,7 @@ sys = System(
 ```
 """
 function System(pm_data::PowerModelsData; kwargs...)
+    runchecks = get(kwargs, :runchecks, true)
     data = pm_data.data
     if length(data["bus"]) < 1
         throw(DataFormatError("There are no buses in this file."))
@@ -47,7 +48,11 @@ function System(pm_data::PowerModelsData; kwargs...)
     read_shunt!(sys, data, bus_number_to_bus; kwargs...)
     read_dcline!(sys, data, bus_number_to_bus; kwargs...)
 
-    check!(sys)
+
+    if runchecks
+        check!(sys)
+    end
+
     return sys
 end
 
