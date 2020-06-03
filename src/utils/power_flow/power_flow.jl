a16b47f6711f10725dd007e3a63ed4fdedeaf1a7
Jose Daniel Lara <jdlara@berkeley.edu>
2020-06-02 22:12:36 -0700

    modify location of pf call

--- src/utils/power_flow/power_flow.jl
+++ src/utils/power_flow/power_flow.jl
@@ -99,8 +99,26 @@ function _write_pf_sol!(sys::System, nl_result)
     return
 end
 
+# TODO: Apply actions according to load type
+function _get_load_data(sys::System, b::Bus)
+    activepower = 0.0
+    reactivepower = 0.0
+    for l in get_components(ElectricLoad, sys)
+        if !isa(l, FixedAdmittance) && (l.bus == b)
+            activepower += get_activepower(l)
+            reactivepower += get_reactivepower(l)
+        elseif isa(l, FixedAdmittance) && (l.bus == b)
+            # Assume v rated = 1.0
+            activepower += real(get_Y(l))
+            reactivepower += imag(get_Y(l))
+        end
+    end
+    return activepower, reactivepower
+end
+
+
 """
-    solve_powerflow!(sys, solve_function, args...)
+    solve_powerflow!(system, solve_function, args...)
 
 Solves a the power flow into the system and writes the solution into the relevant structs.
 Updates generators active and reactive power setpoints and branches active and reactive
@@ -139,15 +157,131 @@ solve_powerflow!(sys, nlsolve, method = :newton)
 ```
 
 """
-function solve_powerflow!(sys, nlsolve; args...)
-    pf!, x0 = PowerSystems.make_pf(sys)
-    res = nlsolve(pf!, x0; args...)
+function solve_powerflow!(system::System; args...)
+    to = TimerOutput()
+    @timeit to "Startup" begin
+    buses = sort(collect(get_components(Bus, system)), by = x -> get_number(x))
+    N_BUS = length(buses)
+
+    # assumes the ordering in YBus is the same as in the buses.
+    tempYb = Ybus(get_components(ACBranch, system), buses)
+    a = collect(1:N_BUS)
+    Yb = Ybus(tempYb.data, (a, a), (_make_ax_ref(a), _make_ax_ref(a))).data
+    I, J, V = SparseArrays.findnz(Yb)
+    neighbors = [Set{Int}([i]) for i in 1:N_BUS]
+    for nz in eachindex(V)
+        push!(neighbors[I[nz]], J[nz])
+        push!(neighbors[J[nz]], I[nz])
+    end
+    x0 = zeros(N_BUS * 2)
+
+    # Use vectors to cache data for closure
+    # These should be read only
+    P_GEN_BUS = fill(0.0, N_BUS)
+    Q_GEN_BUS = fill(0.0, N_BUS)
+    P_LOAD_BUS = fill(0.0, N_BUS)
+    Q_LOAD_BUS = fill(0.0, N_BUS)
+    P_net = fill(0.0, N_BUS)
+    Q_net = fill(0.0, N_BUS)
+    Vm = fill(0.0, N_BUS)
+    θ = fill(0.0, N_BUS)
+
+
+    state_variable_count = 1
+
+    for (ix, b) in enumerate(buses)
+        bus_number = get_number(b)::Int
+        bus_angle = get_angle(b)::Float64
+        bus_voltage = get_voltage(b)::Float64
+        P_GEN_BUS[ix] = 0.0
+        Q_GEN_BUS[ix] = 0.0
+        get_ext(b)["neighbors"] = neighbors[ix]
+        for gen in get_components(StaticInjection, system, d -> !isa(d, ElectricLoad))
+            if gen.bus == b
+                P_GEN_BUS[ix] += get_activepower(gen)
+                Q_GEN_BUS[ix] += get_reactivepower(gen)
+            end
+        end
+
+        P_LOAD_BUS[ix], Q_LOAD_BUS[ix] = _get_load_data(system, b)
+
+        if b.bustype == BusTypes.REF
+            injection_components = get_components(StaticInjection, system, d -> d.bus == b)
+            isempty(injection_components) &&
+                throw(IS.ConflictingInputsError("The slack bus does not have any injection component. Power Flow can not proceed"))
+            Vm[ix] = get_voltage(b)::Float64
+            θ[ix] = get_angle(b)::Float64
+            x0[state_variable_count] = P_GEN_BUS[ix]
+            x0[state_variable_count + 1] = Q_GEN_BUS[ix]
+            state_variable_count += 2
+        elseif b.bustype == BusTypes.PV
+            Vm[ix] = get_voltage(b)::Float64
+            x0[state_variable_count] = Q_GEN_BUS[ix]
+            x0[state_variable_count + 1] = bus_angle
+            state_variable_count += 2
+        elseif b.bustype == BusTypes.PQ
+            x0[state_variable_count] = bus_voltage
+            x0[state_variable_count + 1] = bus_angle
+            state_variable_count += 2
+        end
+    end
+
+    @assert state_variable_count - 1 == N_BUS * 2
+    bus_types = get_bustype.(buses)
+    end
+
+    function pf!(F::Vector{Float64}, X::Vector{Float64})
+        @timeit to "Update X" begin
+        @inbounds for (ix, b) in enumerate(bus_types)
+            if b == BusTypes.REF
+                # When bustype == REFERENCE Bus, state variables are Active and Reactive Power Generated
+                P_net[ix] = X[2 * ix - 1] - P_LOAD_BUS[ix]
+                Q_net[ix] = X[2 * ix] - Q_LOAD_BUS[ix]
+            elseif b == BusTypes.PV
+                # When bustype == PV Bus, state variables are Reactive Power Generated and Voltage Angle
+                P_net[ix] = P_GEN_BUS[ix] - P_LOAD_BUS[ix]
+                Q_net[ix] = X[2 * ix - 1] - Q_LOAD_BUS[ix]
+                θ[ix] = X[2 * ix]
+            elseif b == BusTypes.PQ
+                # When bustype == PQ Bus, state variables are Voltage Magnitude and Voltage Angle
+                P_net[ix] = P_GEN_BUS[ix] - P_LOAD_BUS[ix]
+                Q_net[ix] = Q_GEN_BUS[ix] - Q_LOAD_BUS[ix]
+                Vm[ix] = X[2 * ix - 1]
+                θ[ix] = X[2 * ix]
+            end
+        end
+    end
+
+    @timeit to "Update F" begin
+        # F is active and reactive power balance equations at all buses
+        @inbounds for ix_f in a
+            S_re = -P_net[ix_f]
+            S_im = -Q_net[ix_f]
+            @inbounds for ix_t in neighbors[ix_f]
+                gb = real(Yb[ix_f, ix_t])
+                bb = imag(Yb[ix_f, ix_t])
+                S_re +=
+                    Vm[ix_f] *
+                    Vm[ix_t] *
+                    (gb * cos(θ[ix_f] - θ[ix_t]) + bb * sin(θ[ix_f] - θ[ix_t]))
+                S_im +=
+                    Vm[ix_f] *
+                    Vm[ix_t] *
+                    (gb * sin(θ[ix_f] - θ[ix_t]) - bb * cos(θ[ix_f] - θ[ix_t]))
+            end
+            F[2 * ix_f - 1] = S_re
+            F[2 * ix_f] = S_im
+        end
+    end
+    end
+
+    res = NLsolve.nlsolve(pf!, x0, show_trace = true)
     @info(res)
     if res.f_converged
-        PowerSystems._write_pf_sol!(sys, res)
+        PowerSystems._write_pf_sol!(system, res)
         @info("PowerFlow solve converged, the results have been stored in the system")
-        return res.f_converged
+        return res.f_converged, to
     end
     @error("The powerflow solver returned convergence = $(res.f_converged)")
-    return res.f_converged
+    return res.f_converged, to
 end
