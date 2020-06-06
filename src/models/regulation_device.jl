583cd679e075709db6cb3f70e837e46bc7731edb
Jose Daniel Lara <jdlara@berkeley.edu>
2020-06-06 15:50:50 -0700

    add reserve direction limit

--- src/models/regulation_device.jl
+++ src/models/regulation_device.jl
@@ -2,18 +2,20 @@ mutable struct RegulationDevice{T <: StaticInjection} <: Device
     device::T
     droop::Float64
     participation_factor::Float64
-    reserve_limit::Float64
+    reserve_limit_up::Float64
+    reserve_limit_dn::Float64
     inertia::Float64
 
     function RegulationDevice{T}(
         device::T,
         droop::Float64,
         participation_factor::Float64,
-        reserve_limit::Float64,
+        reserve_limit_up::Float64,
+        reserve_limit_dn::Float64,
         inertia::Float64,
     ) where {T <: StaticInjection}
         IS.@forward((RegulationDevice{T}, :device), T)
-        new{T}(device, droop, participation_factor, reserve_limit, inertia)
+        new{T}(device, droop, participation_factor, reserve_limit_up, reserve_limit_dn, inertia)
     end
 end
 
@@ -21,21 +23,24 @@ function RegulationDevice(
     device::T;
     droop::Float64 = Inf,
     participation_factor::Float64 = 0.0,
-    reserve_limit::Float64 = 0.0,
+    reserve_limit_up::Float64 = 0.0,
+    reserve_limit_dn::Float64 = 0.0,
     inertia::Float64 = 0.0,
 ) where {T <: StaticInjection}
-    return RegulationDevice{T}(device, droop, participation_factor, reserve_limit, inertia)
+    return RegulationDevice{T}(device, droop, participation_factor, reserve_limit_up, reserve_limit_dn, inertia)
 end
 
 IS.get_name(value::RegulationDevice) = IS.get_name(value.device)
 IS.get_uuid(value::RegulationDevice) = IS.get_uuid(value.device)
 get_droop(value::RegulationDevice) = value.droop
 get_participation_factor(value::RegulationDevice) = value.participation_factor
-get_reserve_limit(value::RegulationDevice) = value.reserve_limit
+get_reserve_limit_up(value::RegulationDevice) = value.reserve_limit_up
+get_reserve_limit_dn(value::RegulationDevice) = value.reserve_limit_dn
 get_inertia(value::RegulationDevice) = value.inertia
 
 set_droop!(value::RegulationDevice, val::Float64) = value.droop = val
 set_participation_factor!(value::RegulationDevice, val::Float64) =
     value.participation_factor = val
-set_reserve_limit!(value::RegulationDevice, val::Float64) = value.reserve_limit = val
+set_reserve_limit_up!(value::RegulationDevice, val::Float64) = value.reserve_limit_up = val
+set_reserve_limit_dn!(value::RegulationDevice, val::Float64) = value.reserve_limit_dn = val
 set_inertia!(value::RegulationDevice, val::Float64) = value.inertia = val
