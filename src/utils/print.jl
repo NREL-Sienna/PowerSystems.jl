229502933c1392b590902305558e1d7fe9090ada
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-06 13:49:07 -0700

    use getter function through out

--- src/utils/print.jl
+++ src/utils/print.jl
@@ -1,24 +1,24 @@
 # "smart" summary and REPL printing
 
 function Base.summary(sys::System)
-    return "System (base power $(sys.basepower))"
+    return "System (base power $(get_base_power(sys)))"
 end
 
 function Base.show(io::IO, sys::System)
-    println(io, "basepower=$(sys.basepower)")
+    println(io, "basepower=$(get_base_power(sys))")
     show(io, sys.data)
 end
 
 function Base.show(io::IO, ::MIME"text/plain", sys::System)
     println(io, "System")
     println(io, "======")
-    println(io, "Base Power: $(sys.basepower)\n")
+    println(io, "Base Power: $(get_base_power(sys))\n")
     show(io, MIME"text/plain"(), sys.data)
 end
 
 function Base.show(io::IO, ::MIME"text/html", sys::System)
     println(io, "<h1>System</h1>")
-    println(io, "<p><b>Base Power</b>: $(sys.basepower)</p>")
+    println(io, "<p><b>Base Power</b>: $(get_base_power(sys))</p>")
     show(io, MIME"text/html"(), sys.data)
 end
 
