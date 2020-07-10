e1861049c9e81ae8f382df1a9ce49ce49cfa5ee4
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-09 17:20:02 -0700

    handle nothing

--- src/models/components.jl
+++ src/models/components.jl
@@ -1,12 +1,21 @@
+"""
+    Default behavior of a component. If there is no base_power field, assume is in the system's base power.
+"""
+function get_base_power(c::Component)
+    return get_internal(c).units_info.base_value
+end
+
 function _get_multiplier(c::T) where T <: Component
     setting = get_internal(c).units_info
-    if setting.unit_system == IS.DEVICE_BASE
+    if isnothing(setting)
+        return 1.0
+    elseif setting.unit_system == IS.DEVICE_BASE
         return 1.0
     elseif setting.unit_system == IS.SYSTEM_BASE
-        numerator = hasfield(T, :base_power) ? setting.base_value : get_base_power(c)
+        numerator = get_base_power(c)
         denominator = setting.base_value
     elseif setting.unit_system == IS.NATURAL_UNITS
-        numerator = hasfield(T, :base_power) ? setting.base_value : get_base_power(c)
+        numerator = get_base_power(c)
         denominator = 1.0
     else
         @assert false
