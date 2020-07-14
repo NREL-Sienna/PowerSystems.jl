dfaa650e584a8d2bc75fd0bb0520ff6c5ceba298
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-07-13 22:43:45 -0700

    remove extra get V_ref function in inverter

--- src/models/dynamic_inverter.jl
+++ src/models/dynamic_inverter.jl
@@ -129,4 +129,3 @@ get_static_injector(device::DynamicInverter) = device.static_injector
 get_internal(device::DynamicInverter) = device.internal
 get_P_ref(value::DynamicInverter) = get_P_ref(get_active_power(get_outer_control(value)))
 get_V_ref(value::DynamicInverter) = get_V_ref(get_reactive_power(get_outer_control(value)))
-get_V_ref(value::DynamicInverter) = get_V_ref(get_reactive_power(get_outer_control(value)))
