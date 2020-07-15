b3489f0d13dfc1af2f808377be89a5edca59ad03
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-14 17:33:40 -0700

    add IS.prefix

--- src/base.jl
+++ src/base.jl
@@ -353,7 +353,7 @@ function set_units_base_system!(system::System, settings::String)
     return
 end
 
-function set_units_base_system!(system::System, settings::SystemUnitsSettings)
+function set_units_base_system!(system::System, settings::IS.SystemUnitsSettings)
     system.units_settings.unit_system = settings
     @info "Unit System changed to $settings"
     return
