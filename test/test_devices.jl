3dd11b4c63992008ac9e0dbcaa6c4e53bf36336d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-16 12:08:59 -0700

    add tests

---
+++ test/test_devices.jl
@@ -0,0 +1,20 @@
+@testset "Test special accessors" begin
+
+    cdmsys = create_rts_system()
+    th = first(get_components(ThermalStandard, cdmsys))
+    re = first(get_components(RenewableDispatch, cdmsys))
+
+    @test get_max_active_power(th) == get_active_power_limits(th).max
+    @test get_max_active_power(re) <= get_rating(re)
+
+    mutable struct TestDevice <: Device
+        name::String
+    end
+
+    mutable struct TestRenDevice <: RenewableGen
+        name::String
+    end
+
+    @test_throws MethodError get_max_active_power(TestDevice("foo"))
+    @test_throws MethodError get_max_active_power(TestRenDevice("foo"))
+end
