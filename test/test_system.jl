42553c7ad71b681b617684f4ccad5af6d7bec849
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-17 13:42:45 -0700

    restore RTS labels

--- test/test_system.jl
+++ test/test_system.jl
@@ -50,19 +50,19 @@
     components = collect(get_components(HydroEnergyReservoir, sys))
     @test !isempty(components)
     component = components[1]
-    forecast = get_forecast(Deterministic, component, initial_time, "get_max_active_power")
+    forecast = get_forecast(Deterministic, component, initial_time, "get_rating")
     @test forecast isa Deterministic
 
     # Test all versions of get_forecast_values()
     values1 = get_forecast_values(component, forecast)
     values2 =
-        get_forecast_values(Deterministic, component, initial_time, "get_max_active_power")
+        get_forecast_values(Deterministic, component, initial_time, "get_rating")
     @test values1 == values2
     values3 = get_forecast_values(
         Deterministic,
         component,
         initial_time,
-        "get_max_active_power",
+        "get_rating",
         get_horizon(forecast),
     )
     @test values1 == values3
