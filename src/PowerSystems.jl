1a0163e7d7053f8fa5a68f94aa27d81e98a18242
Andrew.Rosemberg <andrew.rosemberg@invenialabs.co.uk>
2020-07-27 11:17:52 +0100

    remove reserve direction for refactoring

--- src/PowerSystems.jl
+++ src/PowerSystems.jl
@@ -145,8 +145,6 @@ export Reserve
 export ReserveDirection
 export ReserveUp
 export ReserveDown
-export ReserveSupplementaryUp
-export ReserveSupplementaryDown
 export StaticReserve
 export VariableReserve
 export AGC
--- src/models/reserves.jl
+++ src/models/reserves.jl
@@ -1,6 +1,4 @@
 abstract type ReserveDirection end
 abstract type ReserveUp <: ReserveDirection end
 abstract type ReserveDown <: ReserveDirection end
-abstract type ReserveSupplementaryUp  <: ReserveDirection end
-abstract type ReserveSupplementaryDown  <: ReserveDirection end
 abstract type Reserve{T <: ReserveDirection} <: Service end
