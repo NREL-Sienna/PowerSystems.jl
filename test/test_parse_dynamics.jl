4534aaf0566cbcbb6b1d4162be27310b379a888e
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-07-27 20:54:11 -0700

    update tests

--- test/test_parse_dynamics.jl
+++ test/test_parse_dynamics.jl
@@ -2,7 +2,7 @@
 @testset "2-area, 11-bus, 4-generator benchmark system Parsing " begin
     ger4_raw_file = joinpath(PSSE_RAW_DIR, "Benchmark_4ger_33_2015.RAW")
     ger4_dyr_file = joinpath(PSSE_DYR_DIR, "Benchmark_4ger_33_2015.dyr")
-    sys = parse_dyn_system(ger4_raw_file, ger4_dyr_file)
+    sys = System(ger4_raw_file, ger4_dyr_file)
     dyn_injectors = get_components(DynamicInjection, sys)
     @test length(dyn_injectors) == 4
     for g in dyn_injectors
@@ -15,8 +15,8 @@ end
     threebus_raw_file = joinpath(PSSE_TEST_DIR, "ThreeBusNetwork.raw")
     gencls_dyr_file = joinpath(PSSE_TEST_DIR, "TestGENCLS.dyr")
     nogencls_dyr_file = joinpath(PSSE_TEST_DIR, "Test-NoCLS.dyr")
-    sys_gencls = parse_dyn_system(threebus_raw_file, gencls_dyr_file)
-    sys_nogencls = parse_dyn_system(threebus_raw_file, nogencls_dyr_file)
+    sys_gencls = System(threebus_raw_file, gencls_dyr_file)
+    sys_nogencls = System(threebus_raw_file, nogencls_dyr_file)
 
     #Check that generator at bus 102 (H = 0) is a Source, and not ThermalStandard.
     @test isnothing(get_component(ThermalStandard, sys_gencls, "generator-102-1"))
