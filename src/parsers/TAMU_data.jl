27c4598e16e21ee50266d8c9ff7549b4161a39da
claytonpbarrows <clayton.barrows@nrel.gov>
2020-08-01 11:47:15 -0600

    formatting

--- src/parsers/TAMU_data.jl
+++ src/parsers/TAMU_data.jl
@@ -65,7 +65,10 @@ function TamuSystem(tamu_folder::AbstractString; kwargs...)
         push!(fixed_cols, "load_" * join(lsplit[2:3], "_"))
     end
 
-    loads = DataFrames.DataFrame(CSV.File(load_file, skipto = 3, header = fixed_cols), copycols = false)
+    loads = DataFrames.DataFrame(
+        CSV.File(load_file, skipto = 3, header = fixed_cols),
+        copycols = false,
+    )
 
     function parse_datetime_ampm(ds::AbstractString, fmt::Dates.DateFormat)
         m = match(r"(.*)\s(AM|PM)", ds)
