d42e4e0778464ff23bcece209c5febdad8b49740
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-14 14:36:16 -0700

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,11 @@
 using Documenter, PowerSystems
 import DataStructures: OrderedDict
 using Literate
-const PSYPATH = dirname(pathof(PowerSystems))
 
 # This is commented out because the output is not user-friendly. Deliberation on how to best
 # communicate this information to users is ongoing.
 #include(joinpath(@__DIR__, "src", "generate_validation_table.jl"))
+include(joinpath(@__DIR__, "make_model_library.jl"))
 
 pages = OrderedDict(
         "Welcome Page" => "index.md",
@@ -13,11 +13,11 @@ pages = OrderedDict(
             "user_guide/installation.md",
             "user_guide/quick_start_guide.md",
             "user_guide/system.md",
-            "user_guide/generated.md"
         ],
         "Modeler" => Any[
             "modeler/parsing.md",
             "modeler/data.md",
+            "modeler/example_dynamic_data.md"
         ],
         "Model Developer" => Any[
             "Extending Parsing" => "model_developer/extending_parsing.md",
@@ -28,10 +28,31 @@ pages = OrderedDict(
             "Testing" => "developer/testing.md",
             "Logging" => "developer/logging.md",
         ],
-        "API" => Any[
-            "DynamicStructs" => "api/dynamicsAPI.md",
-            "Internal" => "api/internal.md"
-        ]
+        "Model Library" => Any[],
+        "Internal API" => "api/internal.md"
+)
+
+pages["Model Library"] = make_model_library(
+     categories = [
+        Topology,
+        StaticInjection,
+        Service,
+        Branch,
+        PSY.DeviceParameter,
+    ],
+    exceptions = [PSY.DynamicComponent,
+                  PSY.ActivePowerControl,
+                  PSY.ReactivePowerControl,
+                  PSY.DynamicBranch
+                  ],
+    manual_additions =
+        Dict("Service" => ["Reserves" => "model_library/reserves.md"],
+        "StaticInjection" => ["Regulation Device" => "model_library/regulation_device.md"],
+        "DynamicInjection" => ["Dynamic Inverter" => "model_library/dynamic_inverter.md",
+        "Dynamic Generator" => "model_library/dynamic_generator.md",
+        ],
+        "Branch" => ["Dynamic Lines" => "model_library/dynamic_branch.md"]
+        )
 )
 
 # This code performs the automated addition of Literate - Generated Markdowns. The desired
@@ -39,6 +60,7 @@ pages = OrderedDict(
 julia_file_filter = x -> occursin(".jl", x)
 folders = Dict(
     "User Guide" => filter(julia_file_filter, readdir("docs/src/user_guide")),
+    "Model Library" => filter(julia_file_filter, readdir("docs/src/model_library")),
     "Modeler" => filter(julia_file_filter, readdir("docs/src/modeler")),
     "Model Developer" => filter(julia_file_filter, readdir("docs/src/model_developer")),
     "Developer" => filter(julia_file_filter, readdir("docs/src/developer")),
