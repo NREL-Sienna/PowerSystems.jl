bb0a1458bb49c5c0c303c9104ba6b592562469d2
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-18 09:15:54 -0700

    undo last change

--- src/utils/conversion.jl
+++ src/utils/conversion.jl
@@ -1,5 +1,7 @@
 "Convert Tuple to Min Max Named Tuple"
-Base.convert(::Type{Min_Max}, input::Tuple{Float64, Float64}) = Min_Max(input[1], input[2])
+Base.convert(::Type{Min_Max}, input::Tuple{Float64, Float64}) =
+    (min = input[1], max = input[2])
 
 "Convert Tuple to Up Down Named Tuple"
-Base.convert(::Type{Up_Down}, input::Tuple{Float64, Float64}) = Up_Down(input[1], input[2])
+Base.convert(::Type{Up_Down}, input::Tuple{Float64, Float64}) =
+    (up = input[1], down = input[2])
