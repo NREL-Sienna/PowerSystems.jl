b1e3819354e6f52b0d8eb720c1b669e687627b32
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-26 13:47:43 -0700

    add default

--- src/models/generated/MarketBidCost.jl
+++ src/models/generated/MarketBidCost.jl
@@ -5,9 +5,9 @@ This file is auto-generated. Do not edit.
     mutable struct MarketBidCost <: OperationalCost
         energy_bid::VariableCost
         no_load_cost::Float64
-        ancillary_services::Float64
         start_up::NamedTuple{(:hot, :int, :cold), NTuple{3, Float64}}
         shut_down::Float64
+        ancillary_services::Dict{String, Float64}
         forecasts::InfrastructureSystems.Forecasts
         internal::InfrastructureSystemsInternal
     end
@@ -18,9 +18,9 @@ Compatible with most US Market bidding mechanisms
 # Arguments
 - `energy_bid::VariableCost`: variable cost
 - `no_load_cost::Float64`: no load cost
-- `ancillary_services::Float64`: Bids for the ancillary services
 - `start_up::NamedTuple{(:hot, :int, :cold), NTuple{3, Float64}}`: start-up cost
 - `shut_down::Float64`: shutdown cost, validation range: `(0, nothing)`, action if invalid: `warn`
+- `ancillary_services::Dict{String, Float64}`: Bids for the ancillary services
 - `forecasts::InfrastructureSystems.Forecasts`: internal forecast storage
 - `internal::InfrastructureSystemsInternal`: power system internal reference, do not modify
 """
@@ -29,24 +29,24 @@ mutable struct MarketBidCost <: OperationalCost
     energy_bid::VariableCost
     "no load cost"
     no_load_cost::Float64
-    "Bids for the ancillary services"
-    ancillary_services::Float64
     "start-up cost"
     start_up::NamedTuple{(:hot, :int, :cold), NTuple{3, Float64}}
     "shutdown cost"
     shut_down::Float64
+    "Bids for the ancillary services"
+    ancillary_services::Dict{String, Float64}
     "internal forecast storage"
     forecasts::InfrastructureSystems.Forecasts
     "power system internal reference, do not modify"
     internal::InfrastructureSystemsInternal
 end
 
-function MarketBidCost(energy_bid, no_load_cost, ancillary_services, start_up, shut_down, forecasts=InfrastructureSystems.Forecasts(), )
-    MarketBidCost(energy_bid, no_load_cost, ancillary_services, start_up, shut_down, forecasts, InfrastructureSystemsInternal(), )
+function MarketBidCost(energy_bid, no_load_cost, start_up, shut_down, ancillary_services=Dict{String, Float}(), forecasts=InfrastructureSystems.Forecasts(), )
+    MarketBidCost(energy_bid, no_load_cost, start_up, shut_down, ancillary_services, forecasts, InfrastructureSystemsInternal(), )
 end
 
-function MarketBidCost(; energy_bid, no_load_cost, ancillary_services, start_up, shut_down, forecasts=InfrastructureSystems.Forecasts(), )
-    MarketBidCost(energy_bid, no_load_cost, ancillary_services, start_up, shut_down, forecasts, )
+function MarketBidCost(; energy_bid, no_load_cost, start_up, shut_down, ancillary_services=Dict{String, Float}(), forecasts=InfrastructureSystems.Forecasts(), )
+    MarketBidCost(energy_bid, no_load_cost, start_up, shut_down, ancillary_services, forecasts, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -54,9 +54,9 @@ function MarketBidCost(::Nothing)
     MarketBidCost(;
         energy_bid=VariableCost((0.0, 0.0)),
         no_load_cost=0.0,
-        ancillary_services=Dict{String, Float}(),
         start_up=(hot = START_COST, int = START_COST, cold = START_COST),
         shut_down=0.0,
+        ancillary_services=Dict{String, Float}(),
         forecasts=InfrastructureSystems.Forecasts(),
     )
 end
@@ -65,12 +65,12 @@ end
 get_energy_bid(value::MarketBidCost) = value.energy_bid
 """Get [`MarketBidCost`](@ref) `no_load_cost`."""
 get_no_load_cost(value::MarketBidCost) = value.no_load_cost
-"""Get [`MarketBidCost`](@ref) `ancillary_services`."""
-get_ancillary_services(value::MarketBidCost) = value.ancillary_services
 """Get [`MarketBidCost`](@ref) `start_up`."""
 get_start_up(value::MarketBidCost) = value.start_up
 """Get [`MarketBidCost`](@ref) `shut_down`."""
 get_shut_down(value::MarketBidCost) = value.shut_down
+"""Get [`MarketBidCost`](@ref) `ancillary_services`."""
+get_ancillary_services(value::MarketBidCost) = value.ancillary_services
 
 InfrastructureSystems.get_forecasts(value::MarketBidCost) = value.forecasts
 """Get [`MarketBidCost`](@ref) `internal`."""
@@ -80,12 +80,12 @@ get_internal(value::MarketBidCost) = value.internal
 set_energy_bid!(value::MarketBidCost, val) = value.energy_bid = val
 """Set [`MarketBidCost`](@ref) `no_load_cost`."""
 set_no_load_cost!(value::MarketBidCost, val) = value.no_load_cost = val
-"""Set [`MarketBidCost`](@ref) `ancillary_services`."""
-set_ancillary_services!(value::MarketBidCost, val) = value.ancillary_services = val
 """Set [`MarketBidCost`](@ref) `start_up`."""
 set_start_up!(value::MarketBidCost, val) = value.start_up = val
 """Set [`MarketBidCost`](@ref) `shut_down`."""
 set_shut_down!(value::MarketBidCost, val) = value.shut_down = val
+"""Set [`MarketBidCost`](@ref) `ancillary_services`."""
+set_ancillary_services!(value::MarketBidCost, val) = value.ancillary_services = val
 
 InfrastructureSystems.set_forecasts!(value::MarketBidCost, val) = value.forecasts = val
 """Set [`MarketBidCost`](@ref) `internal`."""
