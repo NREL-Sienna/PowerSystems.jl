bab0729c0851394bbb845a9a8e5f0e9e0ca618d3
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-10-13 15:02:35 -0600

    Removing variable cost from MarketBidCost and ReserveDemandCurve

--- src/models/generated/MarketBidCost.jl
+++ src/models/generated/MarketBidCost.jl
@@ -3,7 +3,6 @@ This file is auto-generated. Do not edit.
 =#
 """
     mutable struct MarketBidCost <: OperationalCost
-        variable::VariableCost
         no_load::Float64
         start_up::NamedTuple{(:hot, :warm, :cold), NTuple{3, Float64}}
         shut_down::Float64
@@ -14,15 +13,12 @@ Data Structure Operational Cost to reflect market bids of energy and ancilliary 
 Compatible with most US Market bidding mechanisms
 
 # Arguments
-- `variable::VariableCost`: variable cost representing the energy bid
 - `no_load::Float64`: no load cost
 - `start_up::NamedTuple{(:hot, :warm, :cold), NTuple{3, Float64}}`: start-up cost at different stages of the thermal cycle. Warm is also refered as intermediate in some markets
 - `shut_down::Float64`: shut-down cost, validation range: `(0, nothing)`, action if invalid: `warn`
 - `ancillary_services::IdDict{Any,Float64}`: Bids for the ancillary services
 """
 mutable struct MarketBidCost <: OperationalCost
-    "variable cost representing the energy bid"
-    variable::VariableCost
     "no load cost"
     no_load::Float64
     "start-up cost at different stages of the thermal cycle. Warm is also refered as intermediate in some markets"
@@ -34,14 +30,13 @@ mutable struct MarketBidCost <: OperationalCost
 end
 
 
-function MarketBidCost(; variable, no_load, start_up, shut_down, ancillary_services=IdDict{Any,Float64}(), )
-    MarketBidCost(variable, no_load, start_up, shut_down, ancillary_services, )
+function MarketBidCost(; no_load, start_up, shut_down, ancillary_services=IdDict{Any,Float64}(), )
+    MarketBidCost(no_load, start_up, shut_down, ancillary_services, )
 end
 
 # Constructor for demo purposes; non-functional.
 function MarketBidCost(::Nothing)
     MarketBidCost(;
-        variable=VariableCost((0.0, 0.0)),
         no_load=0.0,
         start_up=(hot = START_COST, warm = START_COST, cold = START_COST),
         shut_down=0.0,
@@ -49,8 +44,6 @@ function MarketBidCost(::Nothing)
     )
 end
 
-"""Get [`MarketBidCost`](@ref) `variable`."""
-get_variable(value::MarketBidCost) = value.variable
 """Get [`MarketBidCost`](@ref) `no_load`."""
 get_no_load(value::MarketBidCost) = value.no_load
 """Get [`MarketBidCost`](@ref) `start_up`."""
@@ -60,8 +53,6 @@ get_shut_down(value::MarketBidCost) = value.shut_down
 """Get [`MarketBidCost`](@ref) `ancillary_services`."""
 get_ancillary_services(value::MarketBidCost) = value.ancillary_services
 
-"""Set [`MarketBidCost`](@ref) `variable`."""
-set_variable!(value::MarketBidCost, val) = value.variable = val
 """Set [`MarketBidCost`](@ref) `no_load`."""
 set_no_load!(value::MarketBidCost, val) = value.no_load = val
 """Set [`MarketBidCost`](@ref) `start_up`."""
--- src/models/generated/ReserveDemandCurve.jl
+++ src/models/generated/ReserveDemandCurve.jl
@@ -6,7 +6,6 @@ This file is auto-generated. Do not edit.
         name::String
         available::Bool
         time_frame::Float64
-        operation_cost::TwoPartCost
         ext::Dict{String, Any}
         time_series_container::InfrastructureSystems.TimeSeriesContainer
         internal::InfrastructureSystemsInternal
@@ -18,7 +17,6 @@ Data Structure for a operating reserve with demand curve product for system simu
 - `name::String`
 - `available::Bool`
 - `time_frame::Float64`: the relative saturation time_frame, validation range: `(0, nothing)`, action if invalid: `error`
-- `operation_cost::TwoPartCost`: Cost for providing reserves  [`TwoPartCost`](@ref)
 - `ext::Dict{String, Any}`
 - `time_series_container::InfrastructureSystems.TimeSeriesContainer`: internal time_series storage
 - `internal::InfrastructureSystemsInternal`: power system internal reference, do not modify
@@ -28,8 +26,6 @@ mutable struct ReserveDemandCurve{T <: ReserveDirection} <: Reserve{T}
     available::Bool
     "the relative saturation time_frame"
     time_frame::Float64
-    "Cost for providing reserves  [`TwoPartCost`](@ref)"
-    operation_cost::TwoPartCost
     ext::Dict{String, Any}
     "internal time_series storage"
     time_series_container::InfrastructureSystems.TimeSeriesContainer
@@ -37,12 +33,12 @@ mutable struct ReserveDemandCurve{T <: ReserveDirection} <: Reserve{T}
     internal::InfrastructureSystemsInternal
 end
 
-function ReserveDemandCurve{T}(name, available, time_frame, operation_cost, ext=Dict{String, Any}(), time_series_container=InfrastructureSystems.TimeSeriesContainer(), ) where T <: ReserveDirection
-    ReserveDemandCurve{T}(name, available, time_frame, operation_cost, ext, time_series_container, InfrastructureSystemsInternal(), )
+function ReserveDemandCurve{T}(name, available, time_frame, ext=Dict{String, Any}(), time_series_container=InfrastructureSystems.TimeSeriesContainer(), ) where T <: ReserveDirection
+    ReserveDemandCurve{T}(name, available, time_frame, ext, time_series_container, InfrastructureSystemsInternal(), )
 end
 
-function ReserveDemandCurve{T}(; name, available, time_frame, operation_cost, ext=Dict{String, Any}(), time_series_container=InfrastructureSystems.TimeSeriesContainer(), internal=InfrastructureSystemsInternal(), ) where T <: ReserveDirection
-    ReserveDemandCurve{T}(name, available, time_frame, operation_cost, ext, time_series_container, internal, )
+function ReserveDemandCurve{T}(; name, available, time_frame, ext=Dict{String, Any}(), time_series_container=InfrastructureSystems.TimeSeriesContainer(), internal=InfrastructureSystemsInternal(), ) where T <: ReserveDirection
+    ReserveDemandCurve{T}(name, available, time_frame, ext, time_series_container, internal, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -51,7 +47,6 @@ function ReserveDemandCurve{T}(::Nothing) where T <: ReserveDirection
         name="init",
         available=false,
         time_frame=0.0,
-        operation_cost=TwoPartCost(nothing),
         ext=Dict{String, Any}(),
         time_series_container=InfrastructureSystems.TimeSeriesContainer(),
     )
@@ -63,8 +58,6 @@ InfrastructureSystems.get_name(value::ReserveDemandCurve) = value.name
 get_available(value::ReserveDemandCurve) = value.available
 """Get [`ReserveDemandCurve`](@ref) `time_frame`."""
 get_time_frame(value::ReserveDemandCurve) = value.time_frame
-"""Get [`ReserveDemandCurve`](@ref) `operation_cost`."""
-get_operation_cost(value::ReserveDemandCurve) = value.operation_cost
 """Get [`ReserveDemandCurve`](@ref) `ext`."""
 get_ext(value::ReserveDemandCurve) = value.ext
 
@@ -78,8 +71,6 @@ InfrastructureSystems.set_name!(value::ReserveDemandCurve, val) = value.name = v
 set_available!(value::ReserveDemandCurve, val) = value.available = val
 """Set [`ReserveDemandCurve`](@ref) `time_frame`."""
 set_time_frame!(value::ReserveDemandCurve, val) = value.time_frame = val
-"""Set [`ReserveDemandCurve`](@ref) `operation_cost`."""
-set_operation_cost!(value::ReserveDemandCurve, val) = value.operation_cost = val
 """Set [`ReserveDemandCurve`](@ref) `ext`."""
 set_ext!(value::ReserveDemandCurve, val) = value.ext = val
 
