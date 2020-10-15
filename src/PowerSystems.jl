9546531cc1c1f38db6b723585eb2e346818c3707
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-10-15 14:21:42 -0600

    updating  module file

--- src/PowerSystems.jl
+++ src/PowerSystems.jl
@@ -240,6 +240,10 @@ export get_resolution
 export get_data
 export iterate_components
 export get_time_series_multiple
+export get_variable_cost
+export get_services_bid
+export set_variable_cost!
+export set_service_bid!
 #export make_time_series
 export get_bus_numbers
 export get_name
@@ -422,6 +426,9 @@ include("base.jl")
 #Data Checks
 include("utils/IO/system_checks.jl")
 include("utils/IO/branchdata_checks.jl")
+
+# cost function TimeSeries convertion
+include("models/cost_function_timeseries.jl")
 
 # network calculations
 include("utils/network_calculations/common.jl")
