5ee80d19ff48a0d35a9e1e3a47d74a96e5872982
Jose Daniel Lara <jdlara@berkeley.edu>
2020-10-21 12:16:54 -0400

    include tests for Hybrid System

--- test/test_hybrid_system.jl
+++ test/test_hybrid_system.jl
@@ -9,5 +9,22 @@ c_sys14() = System(
     branches14(nodes_14),
 )
 
-    gen = ThermalStandard()
+test_sys = c_sys14()
+
+    h_sys = HybridSystem(
+        name = "Test H",
+        available = true,
+        status = true,
+        bus = get_component(Bus, test_sys, "Bus 1"),
+        active_power = 1.0,
+        reactive_power = 1.0,
+        thermal_unit = ThermalStandard(nothing),
+        electric_load = PowerLoad(nothing),
+        storage = GenericBattery(nothing),
+        renewable_unit = RenewableDispatch(nothing),
+        base_power = 100.0,
+        operation_cost = TwoPartCost(nothing)
+    )
+    add_component!(test_sys, h_sys)
+    to_json(test_sys, "./test_sys.json", force = true)
 end
