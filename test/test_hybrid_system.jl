f4d26c45ea5e68f5e2dd2b5b7ba36a3ac6318266
Jose Daniel Lara <jdlara@berkeley.edu>
2020-10-21 12:50:44 -0400

    add forecast addition tests

--- test/test_hybrid_system.jl
+++ test/test_hybrid_system.jl
@@ -26,5 +26,16 @@
         operation_cost = TwoPartCost(nothing),
     )
     add_component!(test_sys, h_sys)
+
+    initial_time = Dates.DateTime("2020-09-01")
+    resolution = Dates.Hour(1)
+    other_time = initial_time + resolution
+    name = "test"
+    horizon = 24
+    data = Dict(initial_time => ones(horizon), other_time => 5.0*ones(horizon))
+    forecast = Deterministic(name, data, resolution)
+    add_time_series!(test_sys, h_sys, forecast)
+    ts = get_time_series(Deterministic, h_sys, "test")
+    @test isa(ts, Deterministic)
     @test_throws ErrorException to_json(test_sys, "./test_sys.json", force = true)
 end
