7380ad0e75be244de0c296a5006297b44ed60f09
Jose Daniel Lara <jdlara@berkeley.edu>
2020-10-21 11:15:47 -0400

    WIP add test data

--- test/test_hybrid_system.jl
+++ test/test_hybrid_system.jl
@@ -1,3 +1,13 @@
 @testset "Hybrid System tests" begin
+    include(joinpath(BASE_DIR, "test", "data_14bus_pu.jl"))
+    nodes_14 = nodes14()
+c_sys14() = System(
+    100.0,
+    nodes_14,
+    thermal_generators14(nodes_14),
+    loads14(nodes_14),
+    branches14(nodes_14),
+)
 
+    gen = ThermalStandard()
 end
