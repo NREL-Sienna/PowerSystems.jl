16fbf718a2dce9bf1a8d7cc70b3e3a47337c3fe6
andrewrosemberg <andrew.rosemberg@invenialabs.co.uk>
2020-10-23 17:43:59 +0100

    add ReserveSymmetric

--- src/models/reserves.jl
+++ src/models/reserves.jl
@@ -1,6 +1,7 @@
 abstract type ReserveDirection end
 abstract type ReserveUp <: ReserveDirection end
 abstract type ReserveDown <: ReserveDirection end
+abstract type ReserveSymmetric <: ReserveDirection end
 abstract type Reserve{T <: ReserveDirection} <: Service end
 
 abstract type ReserveNonSpinning <: Service end
