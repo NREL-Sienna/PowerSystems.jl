40a851f782dd01a2f577a5ec9c63e1da56803265
Jose Daniel Lara <jdlara@berkeley.edu>
2020-10-27 16:06:03 -0400

    minor fixes

--- docs/src/modeler_guide/network_matrices.jl
+++ docs/src/modeler_guide/network_matrices.jl
@@ -1,4 +1,4 @@
-# #Network Matrices
+# # Network Matrices
 
 # `PowerSystems.jl` is able to build classic power systems modeling network matrices such as
 # [Ybus](https://en.wikipedia.org/wiki/Nodal_admittance_matrix), [PTDF](https://www.powerworld.com/WebHelp/Content/MainDocumentation_HTML/Power_Transfer_Distribution_Factors.htm) and LODF
