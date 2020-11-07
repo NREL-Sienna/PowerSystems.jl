dede4f34179472c814068585ecd66e7f4f41d10e
Raphael Saavedra <raphael.saavedra93@gmail.com>
2020-11-07 09:06:44 +0000

    Update src/models/supplemental_accessors.jl
    Co-authored-by: Jose Daniel Lara <jdlara@berkeley.edu>

--- src/models/supplemental_accessors.jl
+++ src/models/supplemental_accessors.jl
@@ -15,7 +15,11 @@ _remove_aggregration_topology!(bus::Bus, ::Area) = bus.area = nothing
 Calculate the admittance of AC branches
 """
 get_series_susceptance(b::ACBranch) = -1 / get_x(b)
-get_series_susceptance(b::TapTransformer) = -1 / (get_x(b) * get_tap(b))
+function get_series_susceptance(b::TapTransformer)
+    y = 1 / (get_r(b) + get_x(b) * 1im)
+    y_a = y / (get_tap(b))
+    return imag(y_a)
+end
 function get_series_susceptance(b::PhaseShiftingTransformer)
     y = 1 / (get_r(b) + get_x(b) * 1im)
     y_a = y / (get_tap(b) * exp(get_α(b) * 1im))
