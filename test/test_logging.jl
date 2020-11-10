b71c7b1ec35937a0165cdb4a1f3d96d965588952
Daniel Thom <daniel.thom@nrel.gov>
2020-11-10 08:05:36 -0700

    Restore global logger after test

--- test/test_logging.jl
+++ test/test_logging.jl
@@ -6,6 +6,7 @@ TEST_MSG = "test log message"
     logfile = "testlog.txt"
     isfile(logfile) && rm(logfile)
 
+    orig_logger = global_logger()
     try
         logger = configure_logging(
             console_level = Logging.Warn,
@@ -23,18 +24,23 @@ TEST_MSG = "test log message"
         @test occursin(TEST_MSG, lines[1])
     finally
         rm(logfile)
+        global_logger(orig_logger)
     end
 
     # Verify logging with no file.
-    logger = configure_logging(
-        console_level = Logging.Warn,
-        file_level = Logging.Info,
-        filename = nothing,
-    )
-    with_logger(logger) do
-        @info TEST_MSG
-    end
+    try
+        logger = configure_logging(
+            console_level = Logging.Warn,
+            file_level = Logging.Info,
+            filename = nothing,
+        )
+        with_logger(logger) do
+            @info TEST_MSG
+        end
 
-    close(logger)
-    @test !isfile(logfile)
+        close(logger)
+        @test !isfile(logfile)
+    finally
+        global_logger(orig_logger)
+    end
 end
