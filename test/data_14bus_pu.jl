23ceaa85b6c948812917564ae0df86bce50964c1
Jose Daniel Lara <jdlara@berkeley.edu>
2020-11-10 14:40:24 -0800

    update data file

--- test/data_14bus_pu.jl
+++ test/data_14bus_pu.jl
@@ -655,7 +655,7 @@ thermal_generators14(nodes14) = [
         time_limits = nothing,
         ramp_limits = nothing,
         operation_cost = ThreePartCost((430.292599, 2000.0), 0.0, 0.0, 0.0),
-        base_power = 1.0,
+        base_power = 100.0,
     ),
     ThermalStandard(
         name = "Bus2",
@@ -672,7 +672,7 @@ thermal_generators14(nodes14) = [
         time_limits = nothing,
         ramp_limits = nothing,
         operation_cost = ThreePartCost((2500.0, 2000.0), 0.0, 0.0, 0.0),
-        base_power = 1.0,
+        base_power = 100.0,
     ),
     ThermalStandard(
         name = "Bus3",
@@ -689,7 +689,7 @@ thermal_generators14(nodes14) = [
         time_limits = nothing,
         ramp_limits = nothing,
         operation_cost = ThreePartCost((100.0, 4000.0), 0.0, 0.0, 0.0),
-        base_power = 1.0,
+        base_power = 100.0,
     ),
     ThermalStandard(
         name = "Bus6",
@@ -706,7 +706,7 @@ thermal_generators14(nodes14) = [
         time_limits = nothing,
         ramp_limits = nothing,
         operation_cost = ThreePartCost((100.0, 4000.0), 0.0, 0.0, 0.0),
-        base_power = 1.0,
+        base_power = 100.0,
     ),
     ThermalStandard(
         name = "Bus8",
@@ -723,7 +723,7 @@ thermal_generators14(nodes14) = [
         time_limits = nothing,
         ramp_limits = nothing,
         operation_cost = ThreePartCost((100.0, 4000.0), 0.0, 0.0, 0.0),
-        base_power = 1.0,
+        base_power = 100.0,
     ),
 ]
 
@@ -960,5 +960,5 @@ battery14(nodes14) = [GenericBattery(
     reactive_power = 0.0,
     reactive_power_limits = (min = -50.0, max = 50.0),
     efficiency = (in = 0.80, out = 0.90),
-    base_power = 1.0,
+    base_power = 100.0,
 )]
