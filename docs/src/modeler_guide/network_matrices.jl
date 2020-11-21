14362a882a7ff9d7df49493e96e7d221563f91c6
Jose Daniel Lara <jdlara@berkeley.edu>
2020-11-21 15:19:26 -0800

    improve examples

--- docs/src/modeler_guide/network_matrices.jl
+++ docs/src/modeler_guide/network_matrices.jl
@@ -3,6 +3,26 @@
 # `PowerSystems.jl` is able to build classic power systems modeling network matrices such as
 # [Ybus](https://en.wikipedia.org/wiki/Nodal_admittance_matrix), [PTDF](https://www.powerworld.com/WebHelp/Content/MainDocumentation_HTML/Power_Transfer_Distribution_Factors.htm) and LODF
 
-# Check section [Network Matrices](@ref net_mat)
+# Check section [Network Matrices](@ref net_mat) for more details
 
-# **UNDER CONSTRUCTION**
+# ## Overview
+# Network matrices are implemented in `PowerSystems.jl` as arrays that enable using Branch or Bus
+# names a indexes to facilitate exploration and analysis. Ybus is stored as an SparseMatrix
+# and the PTDF and LODF are stored as dense matrices.
+
+# ## Ybus
+using PowerSystems
+const PSY = PowerSystems
+
+DATA_DIR = "../../../data" #hide
+system_data = System(joinpath(DATA_DIR, "matpower/case14.m"))
+
+ybus = Ybus(system_data)
+
+# ## PTDF
+
+ptdf = PTDF(system_data)
+
+# ## LODF
+
+lodf = LODF(system_data)
--- docs/src/modeler_guide/power_flow.jl
+++ docs/src/modeler_guide/power_flow.jl
@@ -14,15 +14,34 @@ using PowerSystems
 const PSY = PowerSystems
 
 DATA_DIR = "../../../data" #hide
-system_data = System(joinpath(DATA_DIR, "matpower/case5.m"))
+system_data = System(joinpath(DATA_DIR, "matpower/case14.m"))
 
 # `PowerSystems.jl` has two modes of using the power flow solver.
 
 # 1. Solving the power flow for the current operating point in the system.
 #    Takes the data in the buses, the `active_power` and `reactive_power` fields
-#    in the static injection devices. Returns a dictionary with results in a DataFrame
+#    in the static injection devices. Returns a dictionary with results in a DataFrame that
+#    can be exported or manipulated as needed.
 
 # example
 
 results = solve_powerflow(system_data)
-res["bus_results"]
+results["bus_results"]
+
+# 2. Solves the power flow and updated the devices in the system to the operating condition.
+#    This model will update the values of magnitudes and angles in the system's buses. It
+#    also updates the active and reactive power flows in the branches and devices connected
+#    to PV buses. This utility is useful to initialize systems before serializing or checking
+#    the addition of new devices is still AC feasible.
+
+for b in get_components(Bus, system_data)
+    println("Before")
+    println("$(get_name(b)) - Magnitude $(get_magnitude(b)) - Angle (rad) $(get_angle(b))")
+end
+
+solve_powerflow!(system_data)
+
+for b in get_components(Bus, system_data)
+    println("After")
+    println("$(get_name(b)) - Magnitude $(get_magnitude(b)) - Angle (rad) $(get_angle(b))")
+end
