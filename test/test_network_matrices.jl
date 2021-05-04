69473bc61a0b2a7890995f5a8553da54ff9a171a
claytonpbarrows <clayton.barrows@nrel.gov>
2021-05-03 21:50:33 -0600

    adding tests

--- test/test_network_matrices.jl
+++ test/test_network_matrices.jl
@@ -516,3 +516,19 @@ end
     remove_component!(Line, t2_sys5_re, "5")
     @test isa(Ybus(t2_sys5_re), Ybus)
 end
+
+@testset "Test connected networks" begin
+    @test validate_connectivity(sys)
+    @test(
+        @test_logs (:info, "Validating connectivity with network traversal") match_mode =
+            :any validate_connectivity(sys, method = find_connected_components)
+    )
+    @test length(collect(find_connected_components(sys))[1]) == 5
+end
+
+@testset "Test disconnected networks" begin
+    remove_components!(Line, sys)
+    @test (@test_logs (:warn, "Principal connected component does not contain:") match_mode =
+        :any validate_connectivity(sys)) == false
+    @test validate_connectivity(sys, method = find_connected_components) == false
+end
