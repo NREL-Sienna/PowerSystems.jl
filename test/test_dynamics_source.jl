b6011970a75e7fdd7312e4efc5c10c684ac750f6
Jose Daniel Lara <jdlara@berkeley.edu>
2021-07-19 16:13:10 -0700

    add serialization test

--- test/test_dynamics_source.jl
+++ test/test_dynamics_source.jl
@@ -10,3 +10,25 @@
     add_component!(sys, pvs, source)
     @test get_components(PeriodicVariableSource, sys) !== nothing
 end
+
+
+@testset "Test Dynamic Source" begin
+    sys = System(100)
+    bus = Bus(nothing)
+    add_component!(sys, bus)
+    source = Source(nothing)
+    set_bus!(source, bus)
+    add_component!(sys, source)
+    pvs = PeriodicVariableSource(nothing)
+    set_bus!(pvs, bus)
+    add_component!(sys, pvs, source)
+    @test get_components(PeriodicVariableSource, sys) !== nothing
+    retrieved_pvs = collect(get_components(PeriodicVariableSource, sys))
+    temp_dir = mktempdir()
+    orig_dir = mktempdir()
+    cd(temp_dir)
+    to_json(sys,"test.json")
+    sys2 = System("test.json")
+    serialized_pvs = collect(get_components(PeriodicVariableSource, sys2))
+    @test get_name(retrieved_pvs[1]) == get_name(serialized_pvs[1])
+end
