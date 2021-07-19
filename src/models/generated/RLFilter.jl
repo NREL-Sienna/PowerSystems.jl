4cc6c7058859c5162315d857edffb62c917a78ba
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-07-19 15:21:10 -0700

    fix RLFilter

--- src/models/generated/RLFilter.jl
+++ src/models/generated/RLFilter.jl
@@ -4,7 +4,7 @@ This file is auto-generated. Do not edit.
 """
     mutable struct RLFilter <: Filter
         rf::Float64
-        lg::Float64
+        lf::Float64
         ext::Dict{String, Any}
         states::Vector{Symbol}
         n_states::Int
@@ -14,7 +14,7 @@ Parameters of RL series filter in algebraic representation
 
 # Arguments
 - `rf::Float64`: Series resistance in p.u. of converter filter to the grid, validation range: `(0, nothing)`
-- `lg::Float64`: Series inductance in p.u. of converter filter to the grid, validation range: `(0, nothing)`
+- `lf::Float64`: Series inductance in p.u. of converter filter to the grid, validation range: `(0, nothing)`
 - `ext::Dict{String, Any}`
 - `states::Vector{Symbol}`: RLFilter has zero states
 - `n_states::Int`: RLFilter has zero states
@@ -23,7 +23,7 @@ mutable struct RLFilter <: Filter
     "Series resistance in p.u. of converter filter to the grid"
     rf::Float64
     "Series inductance in p.u. of converter filter to the grid"
-    lg::Float64
+    lf::Float64
     ext::Dict{String, Any}
     "RLFilter has zero states"
     states::Vector{Symbol}
@@ -31,27 +31,27 @@ mutable struct RLFilter <: Filter
     n_states::Int
 end
 
-function RLFilter(rf, lg, ext=Dict{String, Any}(), )
-    RLFilter(rf, lg, ext, Vector{Symbol}(), 0, )
+function RLFilter(rf, lf, ext=Dict{String, Any}(), )
+    RLFilter(rf, lf, ext, Vector{Symbol}(), 0, )
 end
 
-function RLFilter(; rf, lg, ext=Dict{String, Any}(), states=Vector{Symbol}(), n_states=0, )
-    RLFilter(rf, lg, ext, states, n_states, )
+function RLFilter(; rf, lf, ext=Dict{String, Any}(), states=Vector{Symbol}(), n_states=0, )
+    RLFilter(rf, lf, ext, states, n_states, )
 end
 
 # Constructor for demo purposes; non-functional.
 function RLFilter(::Nothing)
     RLFilter(;
         rf=0,
-        lg=0,
+        lf=0,
         ext=Dict{String, Any}(),
     )
 end
 
 """Get [`RLFilter`](@ref) `rf`."""
 get_rf(value::RLFilter) = value.rf
-"""Get [`RLFilter`](@ref) `lg`."""
-get_lg(value::RLFilter) = value.lg
+"""Get [`RLFilter`](@ref) `lf`."""
+get_lf(value::RLFilter) = value.lf
 """Get [`RLFilter`](@ref) `ext`."""
 get_ext(value::RLFilter) = value.ext
 """Get [`RLFilter`](@ref) `states`."""
@@ -61,8 +61,8 @@ get_n_states(value::RLFilter) = value.n_states
 
 """Set [`RLFilter`](@ref) `rf`."""
 set_rf!(value::RLFilter, val) = value.rf = val
-"""Set [`RLFilter`](@ref) `lg`."""
-set_lg!(value::RLFilter, val) = value.lg = val
+"""Set [`RLFilter`](@ref) `lf`."""
+set_lf!(value::RLFilter, val) = value.lf = val
 """Set [`RLFilter`](@ref) `ext`."""
 set_ext!(value::RLFilter, val) = value.ext = val
 
