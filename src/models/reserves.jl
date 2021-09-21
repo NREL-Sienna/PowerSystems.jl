62006bab72b9cedb2d7994f90765a6315c71be7c
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-09-21 12:54:38 -0600

    adding new abstract reserve type

--- src/models/reserves.jl
+++ src/models/reserves.jl
@@ -2,6 +2,6 @@ abstract type ReserveDirection end
 abstract type ReserveUp <: ReserveDirection end
 abstract type ReserveDown <: ReserveDirection end
 abstract type ReserveSymmetric <: ReserveDirection end
-abstract type Reserve{T <: ReserveDirection} <: Service end
-
-abstract type ReserveNonSpinning <: Service end
+abstract type AbstractReserve <: Service end
+abstract type Reserve{T <: ReserveDirection} <: AbstractReserve end
+abstract type ReserveNonSpinning <: AbstractReserve end
