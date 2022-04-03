58e81699a28ccbf98c8ac1065a9a432dd10d07d0
tavovalmo <tavovalmo@gmail.com>
2022-04-03 14:50:38 -0600

    add struct for GENQEC-RR

--- src/models/generated/includes.jl
+++ src/models/generated/includes.jl
@@ -63,6 +63,7 @@ include("OneDOneQMachine.jl")
 include("SimpleAFMachine.jl")
 include("SimpleFullMachine.jl")
 include("SimpleMarconatoMachine.jl")
+include("GENQECRR.jl")
 include("PSSFixed.jl")
 include("PSSSimple.jl")
 include("IEEEST.jl")
@@ -204,6 +205,7 @@ export get_Kp_q
 export get_Ks
 export get_Kt
 export get_Kv
+export get_Kw
 export get_L_1d
 export get_L_1q
 export get_L_ad
@@ -466,6 +468,7 @@ export get_requirement
 export get_rf
 export get_rg
 export get_rv
+export get_sat_flag
 export get_saturation_coeffs
 export get_services
 export get_shut_down
@@ -612,6 +615,7 @@ export set_Kp_q!
 export set_Ks!
 export set_Kt!
 export set_Kv!
+export set_Kw!
 export set_L_1d!
 export set_L_1q!
 export set_L_ad!
@@ -874,6 +878,7 @@ export set_requirement!
 export set_rf!
 export set_rg!
 export set_rv!
+export set_sat_flag!
 export set_saturation_coeffs!
 export set_services!
 export set_shut_down!
