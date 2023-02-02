f83d616edab488a8a2d7944762ee0fbea4e16d11
Jose Daniel Lara <jdlara@berkeley.edu>
2023-02-02 11:40:08 -0700

    Jd/two terminal rename (#962)
    * rename existing HVDCLines
    
    * make two terminal branches ACBranches

--- src/parsers/powerflowdata_data.jl
+++ src/parsers/powerflowdata_data.jl
@@ -846,7 +846,6 @@ function read_dcline!(
     bus_number_to_bus::Dict{Int, Bus};
     kwargs...,
 )
-
     @error "MultiTerminalDCLines parsing from PSS/e files v30 not implemented. Data will be ignored"
     return
 end
