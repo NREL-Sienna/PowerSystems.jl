f6e5114a4aba5842ed12c9caa35c8007896886b1
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-17 17:30:38 -0600

    restore connectivity methods

--- src/base.jl
+++ src/base.jl
@@ -2036,3 +2036,37 @@ Throws InfrastructureSystems.InvalidValue if any time series is inconsistent.
 function check_time_series_consistency(sys::System, ::Type{T}) where {T <: TimeSeriesData}
     return IS.check_time_series_consistency(sys.data, T)
 end
+
+"""
+Finds the set of bus numbers that belong to each connnected component in the System
+"""
+function find_connected_components(sys::PSY.System)
+    a = PNM.Adjacency(sys; check_connectivity = false)
+    return PNM.find_connected_components(a.data, a.lookup[1])
+end
+
+"""
+Checks the network connectivity of the system.
+
+# Keyword arguments
+- `connectivity_method::Function = goderya_connectivity`: Specifies the method used as Goderya's algorithm (`goderya_connectivity`) or depth first search/network traversal (`dfs_connectivity`)
+* Note that the default Goderya method is more efficient, but is resource intensive and may not scale well on large networks.
+"""
+function validate_connectivity(
+    sys::PSY.System;
+    connectivity_method::Function = goderya_connectivity,
+)
+    nodes = sort!(
+        collect(
+            PSY.get_components(x -> PSY.get_bustype(x) != BusTypes.ISOLATED, Bus, sys),
+        );
+        by = x -> PSY.get_number(x),
+    )
+    branches = PNM.get_ac_branches(sys)
+    a = PNM.Adjacency(branches, nodes; check_connectivity = false)
+
+    return PNM.validate_connectivity(
+        a;
+        connectivity_method = connectivity_method,
+    )
+end
