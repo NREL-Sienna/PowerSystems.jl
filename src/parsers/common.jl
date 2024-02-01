38fdb0f2bf5875af78c88911e5f81cf6725ba7d3
Gabriel Konar-Steenberg <23368820+GabrielKS@users.noreply.github.com>
2024-01-31 17:42:21 -0700

    Fix case sensitivity in enum parsers (#1045)
    * Fix case sensitivity in `PrimeMovers` enum parser
    
    * Fix case sensitivity in  enum parser

--- src/parsers/common.jl
+++ src/parsers/common.jl
@@ -4,43 +4,44 @@ const GENERATOR_MAPPING_FILE =
 const PSSE_DYR_MAPPING_FILE =
     joinpath(dirname(pathof(PowerSystems)), "parsers", "psse_dynamic_mapping.yaml")
 
-const STRING2FUEL = Dict((string(x) => x) for x in instances(ThermalFuels))
+const STRING2FUEL =
+    Dict((normalize(string(x); casefold = true) => x) for x in instances(ThermalFuels))
 merge!(
     STRING2FUEL,
     Dict(
-        "NG" => ThermalFuels.NATURAL_GAS,
-        "NUC" => ThermalFuels.NUCLEAR,
-        "GAS" => ThermalFuels.NATURAL_GAS,
-        "OIL" => ThermalFuels.DISTILLATE_FUEL_OIL,
-        "DFO" => ThermalFuels.DISTILLATE_FUEL_OIL,
-        "SYNC_COND" => ThermalFuels.OTHER,
-        "GEOTHERMAL " => ThermalFuels.GEOTHERMAL,
-        "AG_BIPRODUCT" => ThermalFuels.AG_BIPRODUCT,
+        "ng" => ThermalFuels.NATURAL_GAS,
+        "nuc" => ThermalFuels.NUCLEAR,
+        "gas" => ThermalFuels.NATURAL_GAS,
+        "oil" => ThermalFuels.DISTILLATE_FUEL_OIL,
+        "dfo" => ThermalFuels.DISTILLATE_FUEL_OIL,
+        "sync_cond" => ThermalFuels.OTHER,
+        "geothermal " => ThermalFuels.GEOTHERMAL,
+        "ag_biproduct" => ThermalFuels.AG_BIPRODUCT,
     ),
 )
 
-const STRING2PRIMEMOVER = Dict((string(x) => x) for x in instances(PrimeMovers))
+const STRING2PRIMEMOVER =
+    Dict((normalize(string(x); casefold = true) => x) for x in instances(PrimeMovers))
 merge!(
     STRING2PRIMEMOVER,
     Dict(
-        "W2" => PrimeMovers.WT,
-        "WIND" => PrimeMovers.WT,
-        "PV" => PrimeMovers.PVe,
-        "PVe" => PrimeMovers.PVe,
-        "SOLAR" => PrimeMovers.PVe,
-        "RTPV" => PrimeMovers.PVe,
-        "NB" => PrimeMovers.ST,
-        "STEAM" => PrimeMovers.ST,
-        "HYDRO" => PrimeMovers.HY,
-        "ROR" => PrimeMovers.HY,
-        "PUMP" => PrimeMovers.PS,
-        "PUMPED_HYDRO" => PrimeMovers.PS,
-        "NUCLEAR" => PrimeMovers.ST,
-        "SYNC_COND" => PrimeMovers.OT,
-        "CSP" => PrimeMovers.CP,
-        "UN" => PrimeMovers.OT,
-        "STORAGE" => PrimeMovers.BA,
-        "ICE" => PrimeMovers.IC,
+        "w2" => PrimeMovers.WT,
+        "wind" => PrimeMovers.WT,
+        "pv" => PrimeMovers.PVe,
+        "solar" => PrimeMovers.PVe,
+        "rtpv" => PrimeMovers.PVe,
+        "nb" => PrimeMovers.ST,
+        "steam" => PrimeMovers.ST,
+        "hydro" => PrimeMovers.HY,
+        "ror" => PrimeMovers.HY,
+        "pump" => PrimeMovers.PS,
+        "pumped_hydro" => PrimeMovers.PS,
+        "nuclear" => PrimeMovers.ST,
+        "sync_cond" => PrimeMovers.OT,
+        "csp" => PrimeMovers.CP,
+        "un" => PrimeMovers.OT,
+        "storage" => PrimeMovers.BA,
+        "ice" => PrimeMovers.IC,
     ),
 )
 
@@ -201,7 +202,7 @@ function convert_units!(
 end
 
 function parse_enum_mapping(::Type{ThermalFuels}, fuel::AbstractString)
-    return STRING2FUEL[uppercase(fuel)]
+    return STRING2FUEL[normalize(fuel; casefold = true)]
 end
 
 function parse_enum_mapping(::Type{ThermalFuels}, fuel::Symbol)
@@ -209,7 +210,7 @@ function parse_enum_mapping(::Type{ThermalFuels}, fuel::Symbol)
 end
 
 function parse_enum_mapping(::Type{PrimeMovers}, prime_mover::AbstractString)
-    return STRING2PRIMEMOVER[uppercase(prime_mover)]
+    return STRING2PRIMEMOVER[normalize(prime_mover; casefold = true)]
 end
 
 function parse_enum_mapping(::Type{PrimeMovers}, prime_mover::Symbol)
