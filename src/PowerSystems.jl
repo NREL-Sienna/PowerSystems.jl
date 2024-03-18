2038f51aae4274c47c1654edd364028a23952f30
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2024-03-18 17:49:11 -0600

    Scaffold ComponentSelector move from PA, copy over tests

--- src/PowerSystems.jl
+++ src/PowerSystems.jl
@@ -472,6 +472,8 @@ export StructDefinition
 export generate_struct_file
 export generate_struct_files
 
+# TODO export ComponentSelector stuff
+
 #################################################################################
 # Imports
 
@@ -703,6 +705,7 @@ include("outages.jl")
 # Definitions of PowerSystem
 include("base.jl")
 include("subsystems.jl")
+include("component_selector.jl")
 include("data_format_conversions.jl")
 
 #Data Checks
