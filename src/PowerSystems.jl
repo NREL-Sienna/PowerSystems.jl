93a9ffd0481f015883c09f1a400fa2a6da6bb509
Tengis Dashmunkh <tengis.dashmunkh@nrel.gov>
2024-03-24 16:33:10 -0600

    make PA tests pass

--- src/PowerSystems.jl
+++ src/PowerSystems.jl
@@ -472,7 +472,7 @@ export StructDefinition
 export generate_struct_file
 export generate_struct_files
 
-# TODO export ComponentSelector stuff
+#ComponentSelector
 export ComponentSelector
 export ComponentSelectorElement
 export ComponentSelectorSet
@@ -486,6 +486,7 @@ export component_to_qualified_string
 export select_components
 export get_components
 export get_subselectors
+export NAME_DELIMETER
 #################################################################################
 # Imports
 
