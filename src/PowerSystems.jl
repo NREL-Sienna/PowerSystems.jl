8ce7301ce80ecfa27d786d939a29d294f27295d7
Tengis Dashmunkh <tengis.dashmunkh@nrel.gov>
2024-03-25 11:57:10 -0600

    make PSY tests pass

--- src/PowerSystems.jl
+++ src/PowerSystems.jl
@@ -506,7 +506,6 @@ export component_to_qualified_string
 export select_components
 export get_components
 export get_subselectors
-export NAME_DELIMETER
 #################################################################################
 # Imports
 
