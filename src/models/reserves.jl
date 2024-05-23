6d8855b6e18477241c43f6cfb538730c474887e0
kdayday <kdayday@users.noreply.github.com>
2024-05-23 12:48:12 -0600

    Service, reserve, and direction docstrings

--- src/models/reserves.jl
+++ src/models/reserves.jl
@@ -1,6 +1,38 @@
+"""
+Used to specify if a [`Reserve`](@ref) is upwards, downwards, or symmetric
+"""
 abstract type ReserveDirection end
+
+"""
+An upwards reserve to increase generation or reduce load
+
+Upwards reserves are used when total load exceeds its expected level,
+typically due to forecast errors or contingencies.
+
+A [`Reserve`](@ref) can be specified as a `ReserveUp` when it is defined.
+"""
 abstract type ReserveUp <: ReserveDirection end
+
+"""
+A downwards reserve to decrease generation or increase load
+
+Downwards reserves are used when total load falls below its expected level,
+typically due to forecast errors or contingencies. Not work
+
+A [`Reserve`](@ref) can be specified as a `ReserveDown` when it is defined.
+"""
 abstract type ReserveDown <: ReserveDirection end
+
+"""
+A symmetric reserve, procuring the same quantity (MW) of both upwards and downwards
+reserves
+
+A symmetric reserve is a special case. [`ReserveUp`](@ref) and [`ReserveDown`](@ref)
+can be used individually to specify different quantities of upwards and downwards
+reserves, respectively.
+
+A [`Reserve`](@ref) can be specified as a `ReserveSymmetric` when it is defined.
+"""
 abstract type ReserveSymmetric <: ReserveDirection end
 abstract type AbstractReserve <: Service end
 abstract type Reserve{T <: ReserveDirection} <: AbstractReserve end
--- src/models/services.jl
+++ src/models/services.jl
@@ -1,3 +1,11 @@
+"""
+Supertype for all system services
+
+Services (or ancillary services) include additional requirements and support
+to ensure reliable electricity service to customers. Common services are
+reserve products to be able to respond quickly to unexpected disturbances,
+such as the sudden loss of a transmission line or generator.
+"""
 abstract type Service <: Component end
 
 supports_time_series(::Service) = true
