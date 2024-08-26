ef6fd504eed95bd9aa4adfcef484e14cda741349
pesap <pesapsanchez@gmail.com>
2024-08-26 16:41:58 -0600

    fix: Adding support for quadratic functions using the table data parsers
    Currently only works for heat rate and not cost curves.
    
    Issue #1177

--- src/parsers/power_system_table_data.jl
+++ src/parsers/power_system_table_data.jl
@@ -545,6 +545,7 @@ function gen_csv_parser!(sys::System, data::PowerSystemTableData)
         throw(IS.ConflictingInputsError("Heat rate and cost points are both defined"))
     elseif length(heat_rate_fields) > 0
         cost_colnames = _HeatRateColumns(zip(heat_rate_fields, output_point_fields))
+        @warn cost_colnames
     elseif length(cost_point_fields) > 0
         cost_colnames = _CostPointColumns(zip(cost_point_fields, output_point_fields))
     else
@@ -823,8 +824,17 @@ function make_cost(
 ) where {T <: ThermalGen}
     fuel_price = gen.fuel_price / 1000.0
 
-    cost_pairs = get_cost_pairs(gen, cost_colnames)
-    var_cost, fixed = create_pwinc_cost(cost_pairs)
+    # We check if there is any Quadratic or Linear Data defined. If not we fall back to create PowerLoad
+    @warn typeof(gen)
+    quadratic_fields = (gen.heat_rate_a0, gen.heat_rate_a1, gen.heat_rate_a2)
+
+    if any(field -> field != nothing, quadratic_fields)
+        var_cost, fixed =
+            create_poly_cost(gen, ["heat_rate_a0", "heat_rate_a1", "heat_rate_a2"])
+    else
+        cost_pairs = get_cost_pairs(gen, cost_colnames)
+        var_cost, fixed = create_pwinc_cost(cost_pairs)
+    end
 
     startup_cost, shutdown_cost = calculate_uc_cost(data, gen, fuel_price)
 
@@ -954,6 +964,40 @@ function create_pwl_cost(
     end
 
     return var_cost
+end
+
+function create_poly_cost(
+    gen, cost_colnames,
+)
+    vals = []
+    for coeff in cost_colnames
+        a = getfield(gen, Symbol(coeff))
+        if a != nothing
+            push!(vals, tryparse(Float64, a))
+        end
+    end
+
+    # Three supported cases,
+    #   1. If three values are passed then we have data looking like: a^2 + a + c,
+    #   2. If two then data is looking like: a + c
+    #   3. If two then data is looking like: c
+    if length(vals) > 3
+        throw(
+            DataFormatError(
+                "Higher order polynomial functions currently not supported.",
+            ),
+        )
+    elseif length(vals) == 3
+        var_cost = QuadraticCurve(vals[3], vals[2], vals[1])
+        @debug "Quadratic curve created for $(gen.name)"
+    elseif length(vals) == 2
+        var_cost = LinearCurve(vals[2], vals[1])
+    else
+        length(vals) == 1
+        var_cost = LinearCurve(vals[1])
+    end
+
+    return var_cost, 0.0
 end
 
 function create_pwinc_cost(
