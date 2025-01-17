de4ffd917d18d132591e21da65a3098d6107784d
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2025-01-17 16:51:25 -0700

    Restore lost `get_components` docstrings

--- src/get_components_interface.jl
+++ src/get_components_interface.jl
@@ -33,22 +33,25 @@
 
 # get_components
 """
-Returns an iterator of components. T can be concrete or abstract.
+Return an iterator of components of a given `Type` from a [`System`](@ref).
+
+`T` can be a concrete or abstract [`Component`](@ref) type from the [Type Tree](@ref).
 Call collect on the result if an array is desired.
 
 # Examples
 ```julia
-iter = PowerSystems.get_components(ThermalStandard, sys)
-iter = PowerSystems.get_components(Generator, sys)
-iter = PowerSystems.get_components(x -> PowerSystems.get_status(x), ThermalStandard, sys)
-thermal_gens = get_components(ThermalStandard, sys) do gen
-    get_status(gen)
-end
-generators = collect(PowerSystems.get_components(Generator, sys))
-
+iter = get_components(ThermalStandard, sys)
+iter = get_components(Generator, sys)
+generators = collect(get_components(Generator, sys))
 ```
 
-See also: [`iterate_components`](@ref)
+See also: [`iterate_components`](@ref), [`get_components` with a filter](@ref get_components(
+    filter_func::Function,
+    ::Type{T},
+    sys::System;
+    subsystem_name = nothing,
+) where {T <: Component}),
+[`get_available_components`](@ref), [`get_buses`](@ref)
 """
 get_components(::Type{T}, sys::System; subsystem_name = nothing) where {T <: Component} =
     IS.get_components(T, sys; subsystem_name = subsystem_name)
@@ -59,7 +62,27 @@ Return a vector of components that are attached to the supplemental attribute.
 get_components(sys::System, attribute::SupplementalAttribute) =
     IS.get_components(sys, attribute)
 
-"Variant of `get_components` that applies a filter function."
+"""
+Return an iterator of components of a given `Type` from a [`System`](@ref), using an
+additional filter
+
+`T` can be a concrete or abstract [`Component`](@ref) type from the [Type Tree](@ref).
+Call collect on the result if an array is desired.
+
+# Examples
+```julia
+iter_coal = get_components(x -> get_fuel(x) == ThermalFuels.COAL, Generator, sys)
+pv_gens =
+    collect(get_components(x -> get_prime_mover_type(x) == PrimeMovers.PVe, Generator, sys))
+```
+
+See also: [`get_components`](@ref get_components(
+    ::Type{T},
+    sys::System;
+    subsystem_name = nothing,
+) where {T <: Component}), [`get_available_components`](@ref),
+[`get_buses`](@ref)
+"""
 get_components(
     filter_func::Function,
     ::Type{T},
