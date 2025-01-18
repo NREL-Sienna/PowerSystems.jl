430090c06dca50643e49c4a7da6652ccfb8daf0a
rodrigomha <rodrigomha@gmail.com>
2025-01-17 16:14:50 -0800

    update psse parser tests for 3w and switched shunts
    Co-authored-by: mcllerena <marckcode@gmail.com>

--- test/test_parse_psse.jl
+++ test/test_parse_psse.jl
@@ -38,20 +38,22 @@ end
     @test get_available(get_component(ThermalStandard, sys, "generator-2438-EG")) == 1
 
     sys3 = build_system(PSSEParsingTestSystems, "psse_ACTIVSg2000_sys")
-    sys4 = build_system(PSSEParsingTestSystems, "pti_frankenstein_70_sys") 
-    
+    sys4 = build_system(PSSEParsingTestSystems, "pti_frankenstein_70_sys")
+
     base_dir = string(dirname(@__FILE__))
     file_dir = joinpath(base_dir, "test_data", "5circuit_3w.raw")
     sys5 = System(file_dir)
 
     @info "Testing  Three-Winding Transformer Parsing"
-    @test get_available(get_component(Transformer3W, sys3, "1")) == true
-    @test get_available(get_component(Transformer3W, sys5, "1")) == false
+    @test isnothing(get_component(Transformer3W, sys3, "1"))
+    @test get_available(
+        get_component(Transformer3W, sys5, "FAV SPOT 01-FAV SPOT 02-FAV SPOT 03-i_A"),
+    ) == false
     @test length(get_components(Transformer3W, sys5)) == 5
 
     @info "Testing Switched Shunt Parsing"
-    @test get_status(get_component(SwitchedAdmittance, sys3, "1")) == 0
-    @test get_status(get_component(SwitchedAdmittance, sys4, "1")) == 1
+    @test get_available(get_component(SwitchedAdmittance, sys3, "1030_9")) == false
+    @test get_available(get_component(SwitchedAdmittance, sys4, "1003_1")) == true
 
     @info "Testing Load Zone Formatter"
     PSB.clear_serialized_systems("psse_Benchmark_4ger_33_2015_sys")
