41770eade6bf0149467f5355a7ce621004d98eb0
rodrigomha <rodrigomha@gmail.com>
2025-01-28 16:17:48 -0800

    update VSC test
    Co-authored-by: mcllerena <marckcode@gmail.com>

--- test/test_parse_psse.jl
+++ test/test_parse_psse.jl
@@ -56,6 +56,11 @@ end
     @test get_available(get_component(SwitchedAdmittance, sys3, "1030_9")) == false
     @test get_available(get_component(SwitchedAdmittance, sys4, "1003_1")) == true
 
+    @info "Testing VSC Parser"
+    vsc = only(get_components(TwoTerminalVSCLine, sys4))
+    @test get_active_power_flow(vsc) == -0.2
+    @test get_dc_setpoint_to(vsc) == -20.0
+
     @info "Testing Load Zone Formatter"
     PSB.clear_serialized_systems("psse_Benchmark_4ger_33_2015_sys")
     sys3 = build_system(
