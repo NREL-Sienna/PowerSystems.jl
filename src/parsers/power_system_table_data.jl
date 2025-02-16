17381dc79ded34dab7a87fb280bd1e638c323226
Youssef Miftah <miftahyo@outlook.fr>
2025-02-16 15:47:58 +1100

    allow nothing values

--- src/parsers/power_system_table_data.jl
+++ src/parsers/power_system_table_data.jl
@@ -1619,9 +1619,10 @@ function _read_data_row(data::PowerSystemTableData, row, field_infos; na_to_noth
             value = row[field_info.custom_name]
         else
             value = field_info.default_value
-            value == "required" && throw(DataFormatError("$(field_info.name) is required"))
-            @debug "Column $(field_info.custom_name) doesn't exist in df, enabling use of default value of $(field_info.default_value)" _group =
-                IS.LOG_GROUP_PARSING maxlog = 1
+            if value == "required"
+                @warn "Value $(field_info.name) should be required, we continue with a nothing"
+                value = nothing
+            end
         end
         if ismissing(value)
             @info "$(field_info.custom_name) value missing"
