40932dbea3f431a0b22d8201c5bb03fdd8a379cc
mcllerena <marckcode@gmail.com>
2025-03-28 17:43:23 -0600

    abs() in resistance value

--- src/parsers/power_models_data.jl
+++ src/parsers/power_models_data.jl
@@ -757,7 +757,7 @@ function make_switch_breaker(name::String, d::Dict, bus_f::ACBus, bus_t::ACBus)
         active_power_flow = d["active_power_flow"],
         reactive_power_flow = d["reactive_power_flow"],
         arc = Arc(bus_f, bus_t),
-        r = d["r"],
+        r = abs(d["r"]),
         x = d["x"],
         rating = d["rating"],
         discrete_branch_type = d["discrete_branch_type"],
