a5113c8114682f6538172e4758c72d9a5690a1a6
pesap <pesap@users.noreply.github.com>
2025-04-03 14:51:18 -0600

    Revert "fix: Changing abstract type of component to `HydroGen`"
    This reverts commit fcbfc85e91bacafd98fdbed2c14d060213d00178.

--- src/models/HydroReservoir.jl
+++ src/models/HydroReservoir.jl
@@ -40,7 +40,7 @@ A hydropower reservoir that needs to have `HydroTurbine` attached to generate po
 - `ext::Dict{String, Any}`: (default: `Dict{String, Any}()`) An [*ext*ra dictionary](@ref additional_fields) for users to add metadata that are not used in simulation, such as latitude and longitude.
 - `internal::InfrastructureSystemsInternal`: (**Do not modify.**) PowerSystems.jl internal reference
 """
-mutable struct HydroReservoir <: HydroGen
+mutable struct HydroReservoir <: Component
     "Name of the component. Components of the same type (e.g., `PowerLoad`) must have unique names, but components of different types (e.g., `PowerLoad` and `ACBus`) can have the same name"
     name::String
     "Indicator of whether the component is connected and online (`true`) or disconnected, offline, or down (`false`). Unavailable components are excluded during simulations"
