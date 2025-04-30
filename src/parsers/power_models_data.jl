8f20df2e73f057b9512d29b777fb75c229f0a6b9
mcllerena <marckcode@gmail.com>
2025-04-30 16:44:53 -0600

    update system with new load field

--- src/parsers/power_models_data.jl
+++ src/parsers/power_models_data.jl
@@ -344,6 +344,7 @@ function make_power_load(d::Dict, bus::ACBus, sys_mbase::Float64; kwargs...)
         max_active_power = d["pd"],
         max_reactive_power = d["qd"],
         base_power = sys_mbase,
+        is_conforming = d["is_conforming"]
     )
 end
 
@@ -366,6 +367,7 @@ function make_standard_load(d::Dict, bus::ACBus, sys_mbase::Float64; kwargs...)
         max_impedance_active_power = d["py"],
         max_impedance_reactive_power = d["qy"],
         base_power = sys_mbase,
+        is_conforming = d["is_conforming"]
     )
 end
 
