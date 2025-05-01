019c8de9dccad5bb4eb1a71bcefc7233233661be
mcllerena <marckcode@gmail.com>
2025-05-01 11:58:41 -0600

    run formatter

--- src/parsers/power_models_data.jl
+++ src/parsers/power_models_data.jl
@@ -344,7 +344,7 @@ function make_power_load(d::Dict, bus::ACBus, sys_mbase::Float64; kwargs...)
         max_active_power = d["pd"],
         max_reactive_power = d["qd"],
         base_power = sys_mbase,
-        is_conforming = Bool(d["is_conforming"])
+        is_conforming = Bool(d["is_conforming"]),
     )
 end
 
@@ -367,7 +367,7 @@ function make_standard_load(d::Dict, bus::ACBus, sys_mbase::Float64; kwargs...)
         max_impedance_active_power = d["py"],
         max_impedance_reactive_power = d["qy"],
         base_power = sys_mbase,
-        is_conforming = Bool(d["is_conforming"])
+        is_conforming = Bool(d["is_conforming"]),
     )
 end
 
