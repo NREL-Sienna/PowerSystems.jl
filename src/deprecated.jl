da2ac0a6aaf4372b18f3d9495de86978559d866b
rodrigomha <rodrigomha@gmail.com>
2025-05-06 14:58:32 -0700

    remove 2.0 deprecations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,16 +1,2 @@
-# BEGIN 2.0.0  deprecations
-Base.@deprecate convert_component!(
-    linetype::Type{MonitoredLine},
-    line::Line,
-    sys::System;
-    kwargs...,
-) convert_component!(sys, line, linetype; kwargs...)
-Base.@deprecate convert_component!(
-    linetype::Type{Line},
-    line::MonitoredLine,
-    sys::System;
-    kwargs...,
-) convert_component!(sys, line, linetype; kwargs...)
-
 # BEGIN 4.0.0  deprecations
 export TwoTerminalHVDCLine
