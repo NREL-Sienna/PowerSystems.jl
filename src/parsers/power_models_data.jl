79b975db7146e57c9a40ed2f21986762ee1960c0
mcllerena <marckcode@gmail.com>
2025-05-06 11:35:41 -0600

    modify ext structure and propagate change

--- src/parsers/power_models_data.jl
+++ src/parsers/power_models_data.jl
@@ -801,15 +801,13 @@ function make_thermal_gen(
         shut_down = shutdn,
     )
 
-    ext = Dict{String, Any}()
-    if haskey(d, "r_source")
-        ext["z_source"] = (r = d["r_source"], x = d["x_source"])
-    end
-
-    if haskey(d, "rt_source")
-        ext["zt_source"] = (rt = d["rt_source"], xt = d["xt_source"])
-    end
-
+    ext = Dict{String, Float64}(
+        "r" => d["r_source"],
+        "x" => d["x_source"],
+        "rt" => d["rt_source"],
+        "xt" => d["xt_source"],
+    )
+ 
     if d["mbase"] != 0.0
         mbase = d["mbase"]
     else
