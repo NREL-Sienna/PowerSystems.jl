a800ce1be9ff853bae8784239bd5003d8a52b02f
rodrigomha <rodrigomha@gmail.com>
2025-05-13 15:49:42 -0700

    update get_value and set_value code

--- src/models/components.jl
+++ src/models/components.jl
@@ -40,76 +40,86 @@ _get_multiplier(
 _get_multiplier(::T, ::IS.SystemUnitsSettings, ::Val) where {T <: Component} =
     error("Undefined Conditional")
 
-function get_value(c::Component, value::Float64)
+function get_value(c::Component, value::Float64, unit::Val = Val{:mva})
     return _get_multiplier(c) * value
 end
 
-function get_value(c::Component, value::MinMax)
+function get_value(c::Component, value::MinMax, unit::Val = Val{:mva})
     m = _get_multiplier(c)
     return (min = value.min * m, max = value.max * m)
 end
 
-function get_value(c::Component, value::StartUpShutDown)
+function get_value(c::Component, value::StartUpShutDown, unit::Val = Val{:mva})
     m = _get_multiplier(c)
     return (startup = value.startup * m, shutdown = value.shutdown * m)
 end
 
-function get_value(c::Component, value::UpDown)
+function get_value(c::Component, value::UpDown, unit::Val = Val{:mva})
     m = _get_multiplier(c)
     return (up = value.up * m, down = value.down * m)
 end
 
-function get_value(c::Component, value::FromTo_ToFrom)
+function get_value(c::Component, value::FromTo_ToFrom, unit::Val = Val{:mva})
     m = _get_multiplier(c)
     return (from_to = value.from_to * m, to_from = value.to_from * m)
 end
 
-function get_value(c::Component, value::Nothing)
+function get_value(c::Component, value::FromTo, unit::Val = Val{:mva})
+    m = _get_multiplier(c)
+    return (from = value.from * m, to = value.to * m)
+end
+
+function get_value(c::Component, value::Nothing, unit::Val = Val{:mva})
     return value
 end
 
-function get_value(c::T, value::V) where {T <: Component, V}
+function get_value(c::T, value::V, ::Val) where {T <: Component, V}
     @warn("conversion not implemented for $(V) in component $(T)")
     return value::V
 end
 
-function get_value(::Nothing, _)
+function get_value(::Nothing, _, _)
     return
 end
 
-function set_value(c::Component, value::Float64)
+function set_value(c::Component, value::Float64, unit::Val = Val{:mva})
     return (1 / _get_multiplier(c)) * value
 end
 
-function set_value(c::Component, value::MinMax)
+function set_value(c::Component, value::MinMax, unit::Val = Val{:mva})
     m = 1 / _get_multiplier(c)
     return (min = value.min * m, max = value.max * m)
 end
 
-function set_value(c::Component, value::StartUpShutDown)
+function set_value(c::Component, value::StartUpShutDown, unit::Val = Val{:mva})
     m = 1 / _get_multiplier(c)
     return (startup = value.startup * m, shutdown = value.shutdown * m)
 end
 
-function set_value(c::Component, value::UpDown)
+function set_value(c::Component, value::UpDown, unit::Val = Val{:mva})
     m = 1 / _get_multiplier(c)
     return (up = value.up * m, down = value.down * m)
 end
 
-function set_value(c::Component, value::FromTo_ToFrom)
+function set_value(c::Component, value::FromTo_ToFrom, unit::Val = Val{:mva})
     m = 1 / _get_multiplier(c)
     return (from_to = value.from_to * m, to_from = value.to_from * m)
 end
 
-function set_value(c::Component, value::Nothing)
+function set_value(c::Component, value::FromTo, unit::Val = Val{:mva})
+    m = 1 / _get_multiplier(c)
+    return (from = value.from * m, to_from = value.to * m)
+end
+
+function set_value(c::Component, value::Nothing, ::Val)
     return value
 end
 
-function set_value(c::T, value::V) where {T <: Component, V}
+function set_value(c::T, value::V, ::Val = Val{:mva}) where {T <: Component, V}
     @warn("conversion not implemented for $(V) in component $(T)")
     return value::V
 end
 
-function set_value(::Nothing, _)
+function set_value(::Nothing, _, _)
     return
 end
