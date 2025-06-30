d321a30acd705358b5ae2226aa582ea651508e4e
kdayday <kdayday@users.noreply.github.com>
2025-06-30 13:42:35 -0600

    Correct file name

--- docs/make.jl
+++ docs/make.jl
@@ -55,7 +55,7 @@ pages = OrderedDict(
             Any[
             "explanation/system.md",
             "explanation/type_structure.md",
-            "explanation/bus_type_explanation.md",
+            "explanation/buses_type_explanation.md",
             "explanation/per_unit.md",
             "explanation/conforming_and_non_conforming_loads.md",
             "explanation/transformer_per_unit_models.md",
