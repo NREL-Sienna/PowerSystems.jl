4adc4414f29db78e79994907f0531d00af7e550b
Jose Daniel Lara <jdlara@berkeley.edu>
2025-09-03 19:30:42 -0400

    remove lingering @show

--- src/base.jl
+++ src/base.jl
@@ -1384,7 +1384,6 @@ function _get_contributing_devices(sys::System, service::T) where {T <: Service}
     uuid = IS.get_uuid(service)
     devices = ServiceContributingDevices(service, Vector{Device}())
     for device in get_components(Device, sys)
-        @show device
         if supports_services(device)
             for _service in get_services(device)
                 if IS.get_uuid(_service) == uuid
