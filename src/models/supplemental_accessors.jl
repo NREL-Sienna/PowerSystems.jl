a2a4bd7388577485018e47cb302eb4a8249f0375
Jose Daniel Lara <jdlara@berkeley.edu>
2025-09-15 09:34:47 -0600

    Update src/models/supplemental_accessors.jl
    Co-authored-by: kdayday <kdayday@users.noreply.github.com>

--- src/models/supplemental_accessors.jl
+++ src/models/supplemental_accessors.jl
@@ -195,7 +195,7 @@ end
 
 """
 Calculate the phase shift angle α for a [`TapTransformer`](@ref) or [`Transformer2W`](@ref) based on its winding group number.
-Returns the angle in radians, calculated as -(π/6) * winding_group_number.
+Returns the angle in radians, calculated as -(π/6) * `winding_group_number`.
 If the `winding_group_number` is `WindingGroupNumber.UNDEFINED`, returns 0.0 and issues a warning.
 """
 function get_α(t::Union{TapTransformer, Transformer2W})
