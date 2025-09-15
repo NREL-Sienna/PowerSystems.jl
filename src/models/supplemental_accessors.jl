08bb9b3161227b32641c392de18419bc152d0506
Jose Daniel Lara <jdlara@berkeley.edu>
2025-09-15 09:34:19 -0600

    Update src/models/supplemental_accessors.jl
    Co-authored-by: kdayday <kdayday@users.noreply.github.com>

--- src/models/supplemental_accessors.jl
+++ src/models/supplemental_accessors.jl
@@ -137,7 +137,7 @@ that don't implement this function.
 get_active_power_limits(::T) where {T <: Device} =
     throw(ArgumentError("get_active_power_limits not implemented for $T"))
 """
-Generic fallback function for getting reactive power limits. Throws ArgumentError for devices
+Generic fallback function for getting reactive power limits. Throws `ArgumentError` for devices
 that don't implement this function.
 """
 get_reactive_power_limits(::T) where {T <: Device} =
