c6e80d8aab41cc3fb380383b8da21625197343f0
rodrigomha <rodrigomha@gmail.com>
2025-10-01 23:38:38 -0700

    update setter methods

--- src/models/supplemental_setters.jl
+++ src/models/supplemental_setters.jl
@@ -1,4 +1,9 @@
-function set_reservoirs!(turbine::HydroTurbine, reservoir::HydroReservoir)
-    set_reservoirs!(turbine, [reservoir])
+function set_upstream_turbine!(reservoir::HydroReservoir, turbine::HydroUnit)
+    set_upstream_turbines!(reservoir, [turbine])
+    return
+end
+
+function set_downstream_turbine!(reservoir::HydroReservoir, turbine::HydroUnit)
+    set_downstream_turbines!(reservoir, [turbine])
     return
 end
