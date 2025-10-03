5fa5506b04dc623234727156ac7b979c89ac93de
rodrigomha <rodrigomha@gmail.com>
2025-10-02 17:26:22 -0700

    update setters

--- src/models/supplemental_setters.jl
+++ src/models/supplemental_setters.jl
@@ -1,9 +1,13 @@
-function set_upstream_turbine!(reservoir::HydroReservoir, turbine::HydroUnit)
+function set_upstream_turbines!(reservoir::HydroReservoir, turbine::HydroUnit)
     set_upstream_turbines!(reservoir, [turbine])
     return
 end
 
-function set_downstream_turbine!(reservoir::HydroReservoir, turbine::HydroUnit)
+function set_downstream_turbines!(reservoir::HydroReservoir, turbine::HydroUnit)
     set_downstream_turbines!(reservoir, [turbine])
     return
+end
+
+function set_head_to_volume_factor!(reservoir::HydroReservoir, val::Float64)
+    return set_head_to_volume_factor!(reservoir, LinearCurve(val))
 end
