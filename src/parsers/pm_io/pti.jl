93577216e490a973f82c73b3d9e91c455be70458
mcllerena <marckcode@gmail.com>
2025-10-13 20:09:19 -0600

    handle dynamically the start of sections in v35 files

--- src/parsers/pm_io/pti.jl
+++ src/parsers/pm_io/pti.jl
@@ -1936,8 +1936,42 @@ function _parse_pti_data(data_io::IO)
         is_v35 = true
     end
 
-    header_line_start = is_v35 ? 2 : 1
-    bus_data_start = is_v35 ? 25 : 4
+    header_line_start = is_v35 ? 2 : 1 # Start in second line due to @! 
+    # Dynamically handle the start of BUS DATA section
+    # In v35 files, BUS DATA starts in different lines due to the fields GENERAL,GAUSS,NEWTON,ADJUST,TYSL,SOLVER,RATING
+    # This fields are optional in the file and when not found, the start of the reading vary a lot
+    bus_data_start = if is_v35
+        found_start = 25  # Default of most files
+        for i in 3:min(35, length(data_lines))
+            line = strip(data_lines[i])
+
+            # Skip comments and system-wide data
+            if startswith(
+                line,
+                r"@!|GENERAL,|GAUSS,|NEWTON,|ADJUST,|TYSL,|SOLVER,|RATING,",
+            ) || isempty(line)
+                continue
+            end
+
+            # Look for section marker of BUS DATA
+            if contains(line, "END OF SYSTEM-WIDE DATA") ||
+               (
+                tryparse(Int, split(line, ',')[1] |> strip) !== nothing &&
+                contains(line, "'")
+            )
+                found_start = if contains(line, "END OF SYSTEM-WIDE DATA")
+                    (i + (startswith(strip(data_lines[i + 1]), "@!") ? 2 : 1))
+                else
+                    i
+                end
+                break
+            end
+        end
+        # New updated start section 
+        found_start
+    else
+        4 # Start for all v33 files
+    end
 
     current_dtypes = is_v35 ? _pti_dtypes_v35 : _pti_dtypes
 
