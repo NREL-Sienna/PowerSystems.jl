e07a651cfa5ce244539d95ffcc73a324dbf70cf1
Jose Daniel Lara <jdlara@berkeley.edu>
2025-10-31 17:12:21 -0600

    data format

--- src/base.jl
+++ src/base.jl
@@ -36,7 +36,7 @@ const SYSTEM_KWARGS = Set((
 ))
 
 # This will be used in the future to handle serialization changes.
-const DATA_FORMAT_VERSION = "4.0.0"
+const DATA_FORMAT_VERSION = "5.0.0"
 
 mutable struct SystemMetadata <: IS.InfrastructureSystemsType
     name::Union{Nothing, String}
