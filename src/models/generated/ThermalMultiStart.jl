c5ce9729dc72b2e8f140a30c8a2492e3b01e0eea
Jose Daniel Lara <jdlara@berkeley.edu>
2025-10-31 18:33:03 -0600

    clarify the definition of ratings

--- src/models/generated/ThermalMultiStart.jl
+++ src/models/generated/ThermalMultiStart.jl
@@ -43,7 +43,7 @@ A thermal generator, such as a fossil fuel or nuclear generator, that can start-
 - `bus::ACBus`: Bus that this component is connected to
 - `active_power::Float64`: Initial active power set point of the unit in MW. For power flow, this is the steady state operating point of the system. For production cost modeling, this may or may not be used as the initial starting point for the solver, depending on the solver used, validation range: `active_power_limits`
 - `reactive_power::Float64`: Initial reactive power set point of the unit (MVAR), validation range: `reactive_power_limits`
-- `rating::Float64`: Maximum output power rating of the unit (MVA), validation range: `(0, nothing)`
+- `rating::Float64`: Maximum AC side output power rating of the unit. Stored in per unit of the device and not to be confused with base_power, validation range: `(0, nothing)`
 - `prime_mover_type::PrimeMovers`: Prime mover technology according to EIA 923. Options are listed [here](@ref pm_list)
 - `fuel::ThermalFuels`: Prime mover fuel according to EIA 923. Options are listed [here](@ref tf_list)
 - `active_power_limits::MinMax`: Minimum and maximum stable active power levels (MW)
@@ -75,7 +75,7 @@ mutable struct ThermalMultiStart <: ThermalGen
     active_power::Float64
     "Initial reactive power set point of the unit (MVAR)"
     reactive_power::Float64
-    "Maximum output power rating of the unit (MVA)"
+    "Maximum AC side output power rating of the unit. Stored in per unit of the device and not to be confused with base_power"
     rating::Float64
     "Prime mover technology according to EIA 923. Options are listed [here](@ref pm_list)"
     prime_mover_type::PrimeMovers
