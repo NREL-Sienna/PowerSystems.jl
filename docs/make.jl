8f69d01db306ca6bd9e03f824418235198b7665c
kdayday <kdayday@users.noreply.github.com>
2025-11-03 19:35:22 -0700

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -47,7 +47,7 @@ pages = OrderedDict(
             "...add cost data" => Any[
                 "Add an Operating Cost" => "how_to/add_cost_curve.md",
                 "Add a market bid" => "how_to/market_bid_cost.md",
-                "Create a System with a Source and ImportExportCost" => "how_to/create_system_with_source_import_export_cost.md",
+                "Add costs for imported/exported power" => "how_to/create_system_with_source_import_export_cost.md",
             ],
             "...customize or add a new Type" => "how_to/add_new_types.md",
             "...define hydro generators with reservoirs" => "how_to/create_hydro_datasets.md",
