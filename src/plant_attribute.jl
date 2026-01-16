113a7cf1851acf1cbe77a5ecc3aaeb11dc31c43a
Jose Daniel Lara <jdlara@berkeley.edu>
2026-01-16 16:53:11 -0700

    refine plant definitions

--- src/plant_attribute.jl
+++ src/plant_attribute.jl
@@ -1,44 +1,157 @@
 abstract type PowerPlant <: SupplementalAttribute end
 
+"""Get `internal`."""
+get_internal(x::PowerPlant) = x.internal
+
 """
 Attribute to represent ThermalGeneration power plants with synchronous generation.
-For CombinedCyle plants consider using [`CombinedCycleBlock`](@ref) or [`CombinedCycleFractional`](@ref).
+For CombinedCycle plants consider using [`CombinedCycleBlock`](@ref).
 
 The shaft map field is used to represent shared shafts between units.
+
+# Arguments
+- `name::String`: Name of the power plant
+- `shaft_map::Dict{Int, String}`: Mapping of shaft numbers to unit names
+- `internal::InfrastructureSystemsInternal`: (**Do not modify.**) PowerSystems internal reference
 """
 struct ThermalPowerPlant <: PowerPlant
     name::String
     shaft_map::Dict{Int, String}
+    internal::InfrastructureSystemsInternal
 end
+
+"""
+    ThermalPowerPlant(; name, shaft_map, internal)
+
+Construct a [`ThermalPowerPlant`](@ref).
+
+# Arguments
+- `name::String`: Name of the power plant
+- `shaft_map::Dict{Int, String}`: (default: `Dict{Int, String}()`) Mapping of shaft numbers to unit names
+- `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystems internal reference
+"""
+function ThermalPowerPlant(;
+    name,
+    shaft_map = Dict{Int, String}(),
+    internal = InfrastructureSystemsInternal(),
+)
+    return ThermalPowerPlant(name, shaft_map, internal)
+end
+
+"""Get [`ThermalPowerPlant`](@ref) `name`."""
+get_name(value::ThermalPowerPlant) = value.name
+"""Get [`ThermalPowerPlant`](@ref) `shaft_map`."""
+get_shaft_map(value::ThermalPowerPlant) = value.shaft_map
 
 """
 Attribute to represent combined cycle generation by block configuration.
 For aggregate representations consider using [`CombinedCycleFractional`](@ref).
+
+# Arguments
+- `name::String`: Name of the combined cycle block
+- `configuration::CombinedCycleConfiguration`: Configuration type of the combined cycle
+- `heat_recovery_to_steam_factor::Float64`: Factor for heat recovery to steam conversion
+- `internal::InfrastructureSystemsInternal`: (**Do not modify.**) PowerSystems internal reference
 """
 struct CombinedCycleBlock <: PowerPlant
     name::String
     configuration::CombinedCycleConfiguration
+    heat_recovery_to_steam_factor::Float64
+    internal::InfrastructureSystemsInternal
 end
 
 """
-Attribute for aggregate representations of combined cycle plants.
+    CombinedCycleBlock(; name, configuration, heat_recovery_to_steam_factor, internal)
+
+Construct a [`CombinedCycleBlock`](@ref).
+
+# Arguments
+- `name::String`: Name of the combined cycle block
+- `configuration::CombinedCycleConfiguration`: Configuration type of the combined cycle
+- `heat_recovery_to_steam_factor::Float64`: (default: `0.0`) Factor for heat recovery to steam conversion
+- `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystems internal reference
 """
-struct CombinedCycleFractional <: PowerPlant
-    name::String
-    total_power_allocation::Dict{String, Float64}
-    operation_exclusions::Dict{String, Bool}
+function CombinedCycleBlock(;
+    name,
+    configuration,
+    heat_recovery_to_steam_factor = 0.0,
+    internal = InfrastructureSystemsInternal(),
+)
+    return CombinedCycleBlock(name, configuration, heat_recovery_to_steam_factor, internal)
 end
 
+"""Get [`CombinedCycleBlock`](@ref) `name`."""
+get_name(value::CombinedCycleBlock) = value.name
+"""Get [`CombinedCycleBlock`](@ref) `configuration`."""
+get_configuration(value::CombinedCycleBlock) = value.configuration
+"""Get [`CombinedCycleBlock`](@ref) `heat_recovery_to_steam_factor`."""
+get_heat_recovery_to_steam_factor(value::CombinedCycleBlock) =
+    value.heat_recovery_to_steam_factor
+
+"""
+Attribute to represent hydro power plants with shared penstocks.
+
+# Arguments
+- `name::String`: Name of the hydro power plant
+- `penstock_map::Dict{Int, String}`: Mapping of penstock numbers to unit names
+- `internal::InfrastructureSystemsInternal`: (**Do not modify.**) PowerSystems internal reference
+"""
 struct HydroPowerPlant <: PowerPlant
     name::String
     penstock_map::Dict{Int, String}
+    internal::InfrastructureSystemsInternal
 end
 
-struct PumpedHydroPowerPlant <: PowerPlant
-    name::String
-    penstock_map::Dict{Int, String}
+"""
+    HydroPowerPlant(; name, penstock_map, internal)
+
+Construct a [`HydroPowerPlant`](@ref).
+
+# Arguments
+- `name::String`: Name of the hydro power plant
+- `penstock_map::Dict{Int, String}`: (default: `Dict{Int, String}()`) Mapping of penstock numbers to unit names
+- `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystems internal reference
+"""
+function HydroPowerPlant(;
+    name,
+    penstock_map = Dict{Int, String}(),
+    internal = InfrastructureSystemsInternal(),
+)
+    return HydroPowerPlant(name, penstock_map, internal)
 end
 
+"""Get [`HydroPowerPlant`](@ref) `name`."""
+get_name(value::HydroPowerPlant) = value.name
+"""Get [`HydroPowerPlant`](@ref) `penstock_map`."""
+get_penstock_map(value::HydroPowerPlant) = value.penstock_map
+
+"""
+Attribute to represent renewable power plants.
+
+# Arguments
+- `name::String`: Name of the renewable power plant
+- `internal::InfrastructureSystemsInternal`: (**Do not modify.**) PowerSystems internal reference
+"""
 struct RenewablePowerPlant <: PowerPlant
     name::String
+    internal::InfrastructureSystemsInternal
 end
+
+"""
+    RenewablePowerPlant(; name, internal)
+
+Construct a [`RenewablePowerPlant`](@ref).
+
+# Arguments
+- `name::String`: Name of the renewable power plant
+- `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystems internal reference
+"""
+function RenewablePowerPlant(;
+    name,
+    internal = InfrastructureSystemsInternal(),
+)
+    return RenewablePowerPlant(name, internal)
+end
+
+"""Get [`RenewablePowerPlant`](@ref) `name`."""
+get_name(value::RenewablePowerPlant) = value.name
