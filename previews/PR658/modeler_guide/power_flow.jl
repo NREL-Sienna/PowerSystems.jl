1837758b0654323e0b2308879f5dcf3ee4a4dd3b
Documenter.jl <documenter@juliadocs.github.io>
2026-03-14 21:45:04 +0000

    delete history

---
+++ previews/PR658/modeler_guide/power_flow.jl
@@ -0,0 +1,12 @@
+# # Power Flow
+
+# `PowerSystems.jl` provides the capability to run a power flow with the intention of
+# providing a valid initial AC operating point to the system. It is not meant as an analytics
+# tool; the main issue is to determine if the system has feasible AC data.
+
+# The power flow solver uses [NLsolve.jl](https://github.com/JuliaNLSolvers/NLsolve.jl) under
+# the hood and takes any keyword argument accepted by NLsolve
+
+# Check section [Power Flow](@ref pf)
+
+# **Under Construction**
