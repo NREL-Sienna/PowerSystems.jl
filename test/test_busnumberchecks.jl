ab2662c574204818e92ccd70865a476e12f23e5f
Jose Daniel Lara <jdlara@berkeley.edu>
2026-06-10 16:14:54 -0600

    update tests

--- test/test_busnumberchecks.jl
+++ test/test_busnumberchecks.jl
@@ -1,91 +1,89 @@
-# TODO: re-enable once PowerSystemCaseBuilder no longer relies on PSY parsers
-# (PSB.build_system uses PSY.PowerModelsData internally for matpower systems).
-#
-# base_dir = dirname(dirname(pathof(PowerSystems)))
-#
-# @testset "Check bus index" begin
-#     # This signature is used to capture expected error logs from parsing matpower
-#     test_bus_index =
-#         () -> begin
-#             sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
-#             @test sort([b.number for b in collect(get_components(ACBus, sys))]) ==
-#                   [1, 2, 3, 4, 10]
-#             @test sort(
-#                 collect(
-#                     Set([b.arc.from.number for b in collect(get_components(Branch, sys))]),
-#                 ),
-#             ) == [1, 2, 3, 4]
-#             @test sort(
-#                 collect(
-#                     Set([b.arc.to.number for b in collect(get_components(Branch, sys))]),
-#                 ),
-#             ) == [2, 3, 4, 10]
-#
-#             # TODO: add test for loadzones testing MAPPING_BUSNUMBER2INDEX
-#         end
-#     @test_logs min_level = Logging.Error match_mode = :any test_bus_index()
-# end
-#
-# @testset "Test unique bus numbers" begin
-#     # This signature is used to capture expected error logs from parsing matpower
-#     test_bus_numbers =
-#         () -> begin
-#             sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
-#             number = 100
-#             bus1 = ACBus(;
-#                 number = number,
-#                 name = "bus100",
-#                 available = true,
-#                 bustype = ACBusTypes.PV,
-#                 angle = 1.0,
-#                 magnitude = 1.0,
-#                 voltage_limits = (min = -1.0, max = 1.0),
-#                 base_voltage = 1.0,
-#             )
-#             bus2 = ACBus(;
-#                 number = number,
-#                 name = "bus101",
-#                 available = true,
-#                 bustype = ACBusTypes.PV,
-#                 angle = 1.0,
-#                 magnitude = 1.0,
-#                 voltage_limits = (min = -1.0, max = 1.0),
-#                 base_voltage = 1.0,
-#                 area = nothing,
-#                 load_zone = nothing,
-#             )
-#
-#             add_component!(sys, bus1)
-#             @test_throws ArgumentError add_component!(sys, bus2)
-#         end
-#     @test_logs min_level = Logging.Error match_mode = :any test_bus_numbers()
-# end
-#
-# @testset "Test unique DCBus numbers" begin
-#     test_dcbus_numbers =
-#         () -> begin
-#             sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
-#             number = 200
-#             dcbus_defaults = (;
-#                 available = true,
-#                 magnitude = 1.0,
-#                 voltage_limits = (min = -1.0, max = 1.0),
-#                 base_voltage = 1.0,
-#             )
-#             dcbus1 = DCBus(; number = number, name = "dcbus200", dcbus_defaults...)
-#             dcbus2 = DCBus(; number = number, name = "dcbus201", dcbus_defaults...)
-#
-#             add_component!(sys, dcbus1)
-#             @test_throws ArgumentError add_component!(sys, dcbus2)
-#
-#             # Also verify that a DCBus cannot share a number with an existing ACBus
-#             existing_ac_number = first(get_bus_numbers(sys))
-#             dcbus3 = DCBus(;
-#                 number = existing_ac_number,
-#                 name = "dcbus_conflict",
-#                 dcbus_defaults...,
-#             )
-#             @test_throws ArgumentError add_component!(sys, dcbus3)
-#         end
-#     @test_logs min_level = Logging.Error match_mode = :any test_dcbus_numbers()
-# end
+
+base_dir = dirname(dirname(pathof(PowerSystems)))
+
+@testset "Check bus index" begin
+    # This signature is used to capture expected error logs from parsing matpower
+    test_bus_index =
+        () -> begin
+            sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
+            @test sort([b.number for b in collect(get_components(ACBus, sys))]) ==
+                  [1, 2, 3, 4, 10]
+            @test sort(
+                collect(
+                    Set([b.arc.from.number for b in collect(get_components(Branch, sys))]),
+                ),
+            ) == [1, 2, 3, 4]
+            @test sort(
+                collect(
+                    Set([b.arc.to.number for b in collect(get_components(Branch, sys))]),
+                ),
+            ) == [2, 3, 4, 10]
+
+            # TODO: add test for loadzones testing MAPPING_BUSNUMBER2INDEX
+        end
+    @test_logs min_level = Logging.Error match_mode = :any test_bus_index()
+end
+
+@testset "Test unique bus numbers" begin
+    # This signature is used to capture expected error logs from parsing matpower
+    test_bus_numbers =
+        () -> begin
+            sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
+            number = 100
+            bus1 = ACBus(;
+                number = number,
+                name = "bus100",
+                available = true,
+                bustype = ACBusTypes.PV,
+                angle = 1.0,
+                magnitude = 1.0,
+                voltage_limits = (min = -1.0, max = 1.0),
+                base_voltage = 1.0,
+            )
+            bus2 = ACBus(;
+                number = number,
+                name = "bus101",
+                available = true,
+                bustype = ACBusTypes.PV,
+                angle = 1.0,
+                magnitude = 1.0,
+                voltage_limits = (min = -1.0, max = 1.0),
+                base_voltage = 1.0,
+                area = nothing,
+                load_zone = nothing,
+            )
+
+            add_component!(sys, bus1)
+            @test_throws ArgumentError add_component!(sys, bus2)
+        end
+    @test_logs min_level = Logging.Error match_mode = :any test_bus_numbers()
+end
+
+@testset "Test unique DCBus numbers" begin
+    test_dcbus_numbers =
+        () -> begin
+            sys = PSB.build_system(PSB.MatpowerTestSystems, "matpower_case5_re_sys")
+            number = 200
+            dcbus_defaults = (;
+                available = true,
+                magnitude = 1.0,
+                voltage_limits = (min = -1.0, max = 1.0),
+                base_voltage = 1.0,
+            )
+            dcbus1 = DCBus(; number = number, name = "dcbus200", dcbus_defaults...)
+            dcbus2 = DCBus(; number = number, name = "dcbus201", dcbus_defaults...)
+
+            add_component!(sys, dcbus1)
+            @test_throws ArgumentError add_component!(sys, dcbus2)
+
+            # Also verify that a DCBus cannot share a number with an existing ACBus
+            existing_ac_number = first(get_bus_numbers(sys))
+            dcbus3 = DCBus(;
+                number = existing_ac_number,
+                name = "dcbus_conflict",
+                dcbus_defaults...,
+            )
+            @test_throws ArgumentError add_component!(sys, dcbus3)
+        end
+    @test_logs min_level = Logging.Error match_mode = :any test_dcbus_numbers()
+end
--- test/test_hybrid_system.jl
+++ test/test_hybrid_system.jl
@@ -29,128 +29,124 @@
     @test isa(ts, Deterministic)
 end
 
-# TODO: re-enable once PowerSystemCaseBuilder no longer relies on PSY parsers
-# (PSB.build_system uses PSY.PowerSystemTableData internally for test_RTS_GMLC_sys_with_hybrid).
-# @testset "Hybrid System from parsed files" begin
-#     sys = PSB.build_system(
-#         PSB.PSITestSystems,
-#         "test_RTS_GMLC_sys_with_hybrid";
-#         add_forecasts = true,
-#         force_build = true,
-#     )
-#     hybrids = collect(get_components(HybridSystem, sys))
-#     @test length(hybrids) == 1
-#     h_sys = hybrids[1]
-#
-#     electric_load = nothing
-#     thermal_unit = nothing
-#     subcomponents = collect(get_subcomponents(h_sys))
-#     @test length(subcomponents) == 4
-#     expected_time_series_names = Set{String}()
-#     num_time_series = 0
-#     for subcomponent in subcomponents
-#         @test !PSY.is_attached(subcomponent, sys)
-#         @test IS.is_attached(subcomponent, sys.data.masked_components)
-#         if subcomponent isa PowerLoad
-#             electric_load = subcomponent
-#         elseif subcomponent isa ThermalStandard
-#             thermal_unit = subcomponent
-#         end
-#         for ts in get_time_series_multiple(subcomponent)
-#             push!(
-#                 expected_time_series_names,
-#                 PSY.make_subsystem_time_series_name(subcomponent, ts),
-#             )
-#             num_time_series += 1
-#         end
-#     end
-#     @test electric_load !== nothing
-#     @test thermal_unit !== nothing
-#
-#     sts = collect(get_time_series_multiple(h_sys; type = SingleTimeSeries))
-#     forecasts =
-#         collect(get_time_series_multiple(h_sys; type = DeterministicSingleTimeSeries))
-#     @test length(sts) == 2
-#     @test length(forecasts) == 2
-#     @test issubset((get_name(x) for x in sts), expected_time_series_names)
-#     @test issubset((get_name(x) for x in forecasts), expected_time_series_names)
-#
-#     @test get_time_series(SingleTimeSeries, electric_load, "max_active_power") isa
-#           SingleTimeSeries
-#     @test get_time_series(Deterministic, electric_load, "max_active_power") isa
-#           DeterministicSingleTimeSeries
-#
-#     @test !has_time_series(thermal_unit)
-#     @test has_time_series(electric_load)
-#     remove_time_series!(
-#         sys,
-#         DeterministicSingleTimeSeries,
-#         electric_load,
-#         "max_active_power",
-#     )
-#     @test !has_time_series(electric_load, DeterministicSingleTimeSeries, "max_active_power")
-#
-#     # Can't set the units when the HybridSystem is attached to system.
-#     @test_throws ArgumentError PSY.set_thermal_unit!(h_sys, thermal_unit)
-# end
+@testset "Hybrid System from parsed files" begin
+    sys = PSB.build_system(
+        PSB.PSITestSystems,
+        "test_RTS_GMLC_sys_with_hybrid";
+        add_forecasts = true,
+        force_build = true,
+    )
+    hybrids = collect(get_components(HybridSystem, sys))
+    @test length(hybrids) == 1
+    h_sys = hybrids[1]
 
-# TODO: re-enable once PowerSystemCaseBuilder no longer relies on PSY parsers
-# (PSB.build_system uses PSY.PowerSystemTableData internally for test_RTS_GMLC_sys).
-# @testset "Hybrid System from unattached subcomponents" begin
-#     sys = PSB.build_system(PSB.PSITestSystems, "test_RTS_GMLC_sys"; add_forecasts = false)
-#     thermal_unit = first(get_components(ThermalStandard, sys))
-#     bus = get_bus(thermal_unit)
-#     electric_load = first(get_components(PowerLoad, sys))
-#     storage = first(get_components(EnergyReservoirStorage, sys))
-#     renewable_unit = first(get_components(RenewableDispatch, sys))
-#
-#     for subcomponent in (thermal_unit, electric_load, storage, renewable_unit)
-#         remove_component!(sys, subcomponent)
-#     end
-#
-#     name = "Test H"
-#     h_sys = HybridSystem(;
-#         name = name,
-#         available = true,
-#         status = true,
-#         bus = bus,
-#         active_power = 1.0,
-#         reactive_power = 1.0,
-#         thermal_unit = thermal_unit,
-#         electric_load = electric_load,
-#         storage = storage,
-#         renewable_unit = renewable_unit,
-#         base_power = 100.0,
-#         operation_cost = MarketBidCost(nothing),
-#     )
-#     add_component!(sys, h_sys)
-#
-#     for subcomponent in (thermal_unit, electric_load, storage, renewable_unit)
-#         @test !PSY.is_attached(subcomponent, sys)
-#         @test !has_time_series(subcomponent)
-#         @test IS.is_attached(subcomponent, sys.data.masked_components)
-#     end
-#
-#     @test length(IS.get_masked_components(Component, sys.data)) == 4
-#
-#     # Add time series for a subcomponent.
-#     initial_time = Dates.DateTime("2020-09-01")
-#     resolution = Dates.Hour(1)
-#     other_time = initial_time + resolution
-#     name = "test"
-#     horizon = 24
-#     data = Dict(initial_time => rand(horizon), other_time => 5.0 * ones(horizon))
-#
-#     forecast = Deterministic(name, data, resolution)
-#     add_time_series!(sys, thermal_unit, forecast)
-#     @test get_time_series(Deterministic, thermal_unit, name) isa Deterministic
-#     copy_subcomponent_time_series!(h_sys, thermal_unit)
-#     @test get_time_series(
-#         Deterministic,
-#         h_sys,
-#         PSY.make_subsystem_time_series_name(thermal_unit, forecast),
-#     ) isa Deterministic
-# end
+    electric_load = nothing
+    thermal_unit = nothing
+    subcomponents = collect(get_subcomponents(h_sys))
+    @test length(subcomponents) == 4
+    expected_time_series_names = Set{String}()
+    num_time_series = 0
+    for subcomponent in subcomponents
+        @test !PSY.is_attached(subcomponent, sys)
+        @test IS.is_attached(subcomponent, sys.data.masked_components)
+        if subcomponent isa PowerLoad
+            electric_load = subcomponent
+        elseif subcomponent isa ThermalStandard
+            thermal_unit = subcomponent
+        end
+        for ts in get_time_series_multiple(subcomponent)
+            push!(
+                expected_time_series_names,
+                PSY.make_subsystem_time_series_name(subcomponent, ts),
+            )
+            num_time_series += 1
+        end
+    end
+    @test electric_load !== nothing
+    @test thermal_unit !== nothing
+
+    sts = collect(get_time_series_multiple(h_sys; type = SingleTimeSeries))
+    forecasts =
+        collect(get_time_series_multiple(h_sys; type = DeterministicSingleTimeSeries))
+    @test length(sts) == 2
+    @test length(forecasts) == 2
+    @test issubset((get_name(x) for x in sts), expected_time_series_names)
+    @test issubset((get_name(x) for x in forecasts), expected_time_series_names)
+
+    @test get_time_series(SingleTimeSeries, electric_load, "max_active_power") isa
+          SingleTimeSeries
+    @test get_time_series(Deterministic, electric_load, "max_active_power") isa
+          DeterministicSingleTimeSeries
+
+    @test !has_time_series(thermal_unit)
+    @test has_time_series(electric_load)
+    remove_time_series!(
+        sys,
+        DeterministicSingleTimeSeries,
+        electric_load,
+        "max_active_power",
+    )
+    @test !has_time_series(electric_load, DeterministicSingleTimeSeries, "max_active_power")
+
+    # Can't set the units when the HybridSystem is attached to system.
+    @test_throws ArgumentError PSY.set_thermal_unit!(h_sys, thermal_unit)
+end
+
+@testset "Hybrid System from unattached subcomponents" begin
+    sys = PSB.build_system(PSB.PSITestSystems, "test_RTS_GMLC_sys"; add_forecasts = false)
+    thermal_unit = first(get_components(ThermalStandard, sys))
+    bus = get_bus(thermal_unit)
+    electric_load = first(get_components(PowerLoad, sys))
+    storage = first(get_components(EnergyReservoirStorage, sys))
+    renewable_unit = first(get_components(RenewableDispatch, sys))
+
+    for subcomponent in (thermal_unit, electric_load, storage, renewable_unit)
+        remove_component!(sys, subcomponent)
+    end
+
+    name = "Test H"
+    h_sys = HybridSystem(;
+        name = name,
+        available = true,
+        status = true,
+        bus = bus,
+        active_power = 1.0,
+        reactive_power = 1.0,
+        thermal_unit = thermal_unit,
+        electric_load = electric_load,
+        storage = storage,
+        renewable_unit = renewable_unit,
+        base_power = 100.0,
+        operation_cost = MarketBidCost(nothing),
+    )
+    add_component!(sys, h_sys)
+
+    for subcomponent in (thermal_unit, electric_load, storage, renewable_unit)
+        @test !PSY.is_attached(subcomponent, sys)
+        @test !has_time_series(subcomponent)
+        @test IS.is_attached(subcomponent, sys.data.masked_components)
+    end
+
+    @test length(IS.get_masked_components(Component, sys.data)) == 4
+
+    # Add time series for a subcomponent.
+    initial_time = Dates.DateTime("2020-09-01")
+    resolution = Dates.Hour(1)
+    other_time = initial_time + resolution
+    name = "test"
+    horizon = 24
+    data = Dict(initial_time => rand(horizon), other_time => 5.0 * ones(horizon))
+
+    forecast = Deterministic(name, data, resolution)
+    add_time_series!(sys, thermal_unit, forecast)
+    @test get_time_series(Deterministic, thermal_unit, name) isa Deterministic
+    copy_subcomponent_time_series!(h_sys, thermal_unit)
+    @test get_time_series(
+        Deterministic,
+        h_sys,
+        PSY.make_subsystem_time_series_name(thermal_unit, forecast),
+    ) isa Deterministic
+end
 
 @testset "get_fuel_cost for HybridSystem delegates to thermal subunit" begin
     # Hybrid with thermal: get_fuel_cost delegates to thermal subunit
